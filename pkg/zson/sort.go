c6facdf65158a83fd2ce0a59e7412de1b6ea12f3
Matthew Nibecker <hello@mattnibecker.com>
2019-11-13 14:22:27 -0800

    Move sorter functionality to pkg zson
    This will be useful in other places. Also added sorter.Pop/Push to
    satisfy the demands of heap.Interface.

---
+++ pkg/zson/sort.go
@@ -0,0 +1,202 @@
+package zson
+
+import (
+	"bytes"
+	"sort"
+
+	"github.com/mccanne/zq/pkg/zeek"
+)
+
+type Sorter struct {
+	records []*Record
+	fields  []string
+	sorters []sortFn
+	dir     int
+}
+
+func NewSorter(dir int, fields ...string) *Sorter {
+	return &Sorter{
+		dir:     dir,
+		fields:  fields,
+		sorters: make([]sortFn, len(fields)),
+	}
+}
+
+// SortStable performs a stable sort on the provided records.
+func (s *Sorter) SortStable(records []*Record) {
+	s.records = records
+	sort.Stable(s)
+}
+
+// Swap implements sort.Interface for *Record slices.
+func (s *Sorter) Len() int { return len(s.records) }
+
+// Swap implements sort.Interface for *Record slices.
+func (s *Sorter) Swap(i, j int) { s.records[i], s.records[j] = s.records[j], s.records[i] }
+
+// Less implements sort.Interface for *Record slices.
+func (s *Sorter) Less(i, j int) bool {
+	for k, field := range s.fields {
+		sf := s.sorters[k]
+		if sf == nil {
+			sf = lookupSorter(s.records[i], field, s.dir)
+		}
+		if sf == nil {
+			// if we can't build a sorter, then
+			// the record doesn't have a field
+			// with the corresponding name, so
+			// we return true causing these records
+			// to get sorted first
+			return true
+		}
+		s.sorters[k] = sf
+		v := sf(s.records[i], s.records[j], s.dir)
+		// If the events don't match, then return the sort
+		// info.  Otherwise, they match and we continue on
+		// on in the loop to the secondary key, etc.
+		if v != 0 {
+			return v < 0
+		}
+	}
+	// All the keys matched with equality, so arbitrarily return true.
+	return true
+}
+
+// Push adds x as element Len(). Implements heap.Interface.
+func (s *Sorter) Push(r interface{}) {
+	s.records = append(s.records, r.(*Record))
+}
+
+// Pop removes the first element in the array. Implements heap.Interface.
+func (s *Sorter) Pop() interface{} {
+	rec := s.records[len(s.records)-1]
+	s.records = s.records[:len(s.records)-1]
+	return rec
+}
+
+// Index returns the ith record.
+func (s *Sorter) Index(i int) *Record {
+	return s.records[i]
+}
+
+type sortFn func(a *Record, b *Record, dir int) int
+
+func lookupSorter(r *Record, field string, dir int) sortFn {
+	zv := r.ValueByField(field)
+	if zv == nil {
+		return nil
+	}
+	typ := zv.Type()
+	switch typ.(type) {
+	default:
+		return func(*Record, *Record, int) int { return 1 }
+	case *zeek.TypeOfBool:
+		return func(a, b *Record, dir int) int {
+			va, err := a.AccessBool(field)
+			if err != nil {
+				return -dir
+			}
+			vb, err := b.AccessBool(field)
+			if err != nil {
+				return dir
+			}
+			if va == vb {
+				return 0
+			}
+			if va {
+				return dir
+			}
+			return -dir
+		}
+
+	case *zeek.TypeOfString, *zeek.TypeOfEnum:
+		return func(a, b *Record, dir int) int {
+			va, _, err := a.Access(field)
+			if err != nil {
+				return -dir
+			}
+			vb, _, err := b.Access(field)
+			if err != nil {
+				return dir
+			}
+			delta := bytes.Compare(va, vb)
+			if delta < 0 {
+				return -dir
+			} else if delta > 0 {
+				return dir
+			}
+			return 0
+		}
+
+		//XXX note zeek port type can have "/tcp" etc suffix according
+		// to docs but we've only encountered ints in data files.
+		// need to fix this.  XXX also we should break this sorts
+		// into the different types.
+	case *zeek.TypeOfPort, *zeek.TypeOfCount, *zeek.TypeOfInt:
+		return func(a, b *Record, dir int) int {
+			va, err := a.AccessInt(field)
+			if err != nil {
+				return -dir
+			}
+			vb, err := b.AccessInt(field)
+			if err != nil {
+				return dir
+			}
+			if va < vb {
+				return -dir
+			} else if va > vb {
+				return dir
+			}
+			return 0
+		}
+
+	case *zeek.TypeOfDouble:
+		return func(a, b *Record, dir int) int {
+			va, err := a.AccessDouble(field)
+			if err != nil {
+				return -dir
+			}
+			vb, err := b.AccessDouble(field)
+			if err != nil {
+				return dir
+			}
+			if va < vb {
+				return -dir
+			} else if va > vb {
+				return dir
+			}
+			return 0
+		}
+
+	case *zeek.TypeOfTime, *zeek.TypeOfInterval:
+		return func(a, b *Record, dir int) int {
+			va, err := a.AccessTime(field)
+			if err != nil {
+				return -dir
+			}
+			vb, err := b.AccessTime(field)
+			if err != nil {
+				return dir
+			}
+			if va < vb {
+				return -dir
+			} else if va > vb {
+				return dir
+			}
+			return 0
+		}
+
+	case *zeek.TypeOfAddr:
+		return func(a, b *Record, dir int) int {
+			va, err := a.AccessIP(field)
+			if err != nil {
+				return -dir
+			}
+			vb, err := b.AccessIP(field)
+			if err != nil {
+				return dir
+			}
+			return bytes.Compare(va.To16(), vb.To16()) * dir
+		}
+	}
+}
--- proc/sort.go
+++ proc/sort.go
@@ -1,9 +1,7 @@
 package proc
 
 import (
-	"bytes"
 	"fmt"
-	"sort"
 
 	"github.com/mccanne/zq/pkg/nano"
 	"github.com/mccanne/zq/pkg/zeek"
@@ -13,11 +11,11 @@ import (
 // SortProc xxx
 type SortProc struct {
 	Base
-	dir     int
-	limit   int
-	fields  []string
-	sorters []SortFn
-	out     []*zson.Record
+	dir    int
+	limit  int
+	fields []string
+	sorter *zson.Sorter
+	out    []*zson.Record
 }
 
 // defaultSortLimit is the default limit of the number of records that
@@ -26,136 +24,10 @@ type SortProc struct {
 const defaultSortLimit = 1000000
 
 func NewSortProc(c *Context, parent Proc, limit int, fields []string, dir int) *SortProc {
-	var sorters []SortFn
-	if fields == nil {
-		sorters = make([]SortFn, 1)
-	} else {
-		sorters = make([]SortFn, len(fields))
-	}
 	if limit == 0 {
 		limit = defaultSortLimit
 	}
-	return &SortProc{Base{Context: c, Parent: parent}, dir, limit, fields, sorters, nil}
-}
-
-func lookupSorter(r *zson.Record, field string, dir int) SortFn {
-	zv := r.ValueByField(field)
-	if zv == nil {
-		return nil
-	}
-	typ := zv.Type()
-	switch typ.(type) {
-	default:
-		return func(*zson.Record, *zson.Record, int) int { return 1 }
-	case *zeek.TypeOfBool:
-		return func(a, b *zson.Record, dir int) int {
-			va, err := a.AccessBool(field)
-			if err != nil {
-				return -dir
-			}
-			vb, err := b.AccessBool(field)
-			if err != nil {
-				return dir
-			}
-			if va == vb {
-				return 0
-			}
-			if va {
-				return dir
-			}
-			return -dir
-		}
-
-	case *zeek.TypeOfString, *zeek.TypeOfEnum:
-		return func(a, b *zson.Record, dir int) int {
-			va, _, err := a.Access(field)
-			if err != nil {
-				return -dir
-			}
-			vb, _, err := b.Access(field)
-			if err != nil {
-				return dir
-			}
-			delta := bytes.Compare(va, vb)
-			if delta < 0 {
-				return -dir
-			} else if delta > 0 {
-				return dir
-			}
-			return 0
-		}
-
-		//XXX note zeek port type can have "/tcp" etc suffix according
-		// to docs but we've only encountered ints in data files.
-		// need to fix this.  XXX also we should break this sorts
-		// into the different types.
-	case *zeek.TypeOfPort, *zeek.TypeOfCount, *zeek.TypeOfInt:
-		return func(a, b *zson.Record, dir int) int {
-			va, err := a.AccessInt(field)
-			if err != nil {
-				return -dir
-			}
-			vb, err := b.AccessInt(field)
-			if err != nil {
-				return dir
-			}
-			if va < vb {
-				return -dir
-			} else if va > vb {
-				return dir
-			}
-			return 0
-		}
-
-	case *zeek.TypeOfDouble:
-		return func(a, b *zson.Record, dir int) int {
-			va, err := a.AccessDouble(field)
-			if err != nil {
-				return -dir
-			}
-			vb, err := b.AccessDouble(field)
-			if err != nil {
-				return dir
-			}
-			if va < vb {
-				return -dir
-			} else if va > vb {
-				return dir
-			}
-			return 0
-		}
-
-	case *zeek.TypeOfTime, *zeek.TypeOfInterval:
-		return func(a, b *zson.Record, dir int) int {
-			va, err := a.AccessTime(field)
-			if err != nil {
-				return -dir
-			}
-			vb, err := b.AccessTime(field)
-			if err != nil {
-				return dir
-			}
-			if va < vb {
-				return -dir
-			} else if va > vb {
-				return dir
-			}
-			return 0
-		}
-
-	case *zeek.TypeOfAddr:
-		return func(a, b *zson.Record, dir int) int {
-			va, err := a.AccessIP(field)
-			if err != nil {
-				return -dir
-			}
-			vb, err := b.AccessIP(field)
-			if err != nil {
-				return dir
-			}
-			return bytes.Compare(va.To16(), vb.To16()) * dir
-		}
-	}
+	return &SortProc{Base: Base{Context: c, Parent: parent}, dir: dir, limit: limit, fields: fields}
 }
 
 func firstOf(d *zson.Descriptor, which zeek.Type) string {
@@ -193,43 +65,6 @@ func guess(recs []*zson.Record) string {
 	return "ts"
 }
 
-type SortFn func(*zson.Record, *zson.Record, int) int
-
-func (s *SortProc) sorter(recs []*zson.Record) func(int, int) bool {
-	return func(i, j int) bool {
-		if s.fields == nil {
-			// If no sort-by fields are given, then we try to guess
-			// something that makes sense...
-			fld := guess(recs)
-			s.fields = []string{fld}
-		}
-		for k, field := range s.fields {
-			sf := s.sorters[k]
-			if sf == nil {
-				sf = lookupSorter(recs[i], field, s.dir)
-				if sf == nil {
-					// if we can't build a sorter, then
-					// the record doesn't have a field
-					// with the corresponding name, so
-					// we return true causing these records
-					// to get sorted first
-					return true
-				}
-				s.sorters[k] = sf
-			}
-			v := s.sorters[k](recs[i], recs[j], s.dir)
-			// If the events don't match, then return the sort
-			// info.  Otherwise, they match and we continue on
-			// on in the loop to the secondary key, etc.
-			if v != 0 {
-				return v < 0
-			}
-		}
-		// All the keys matched with equality, so arbitrarily return true.
-		return true
-	}
-}
-
 func (s *SortProc) Pull() (zson.Batch, error) {
 	for {
 		batch, err := s.Get()
@@ -255,19 +90,19 @@ func (s *SortProc) consume(batch zson.Batch) {
 	}
 }
 
-//XXX this is just string sorting for now on the first field.
-// also, need to sort according to types
-func (s *SortProc) sortRecords(recs []*zson.Record) {
-	sorter := s.sorter(recs)
-	sort.SliceStable(recs, sorter)
-}
-
 func (s *SortProc) sort() zson.Batch {
 	out := s.out
 	if len(out) == 0 {
 		return nil
 	}
 	s.out = nil
-	s.sortRecords(out)
+	if s.fields == nil {
+		fld := guess(out)
+		s.fields = []string{fld}
+	}
+	if s.sorter == nil {
+		s.sorter = zson.NewSorter(s.dir, s.fields...)
+	}
+	s.sorter.SortStable(out)
 	return zson.NewArray(out, nano.NewSpanTs(s.MinTs, s.MaxTs))
 }
