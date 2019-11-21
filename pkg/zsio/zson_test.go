adf4f4dc664f34f5200b6972b28273879aa939f6
Steven McCanne <steve@mccanne.com>
2019-11-21 14:48:43 -0800

    more tests

--- pkg/zsio/zson_test.go
+++ pkg/zsio/zson_test.go
@@ -43,12 +43,40 @@ const zson3 = `
 #0:record[foo:set[string]]
 0:[[-;];];`
 
+// string \x2d is "-"
 const zson4 = `
 #0:record[foo:string]
 0:[\x2d;];`
+
+// string \x5b is "[", second string is "[-]" and should pass through
+const zson5 = `
+#0:record[foo:string,bar:string]
+0:[\x5b;\x5b-];];`
+
+func repeat(c byte, n int) string {
+	b := make([]byte, n)
+	for k := 0; k < n; k++ {
+		b[k] = c
+	}
+	return string(b)
+}
+
+// generate some really big strings
+func zsonBig() string {
+	s := "#0:record[f0:string,f1:string,f2:string,f3:string]\n"
+	s += "0:["
+	s += repeat('a', 4) + ";"
+	s += repeat('b', 400) + ";"
+	s += repeat('c', 30000) + ";"
+	s += repeat('d', 2) + ";];\n"
+	return s
+}
 
 func TestZson(t *testing.T) {
 	identity(t, zson1)
 	identity(t, zson2)
 	identity(t, zson3)
+	identity(t, zson4)
+	identity(t, zson5)
+	identity(t, zsonBig())
 }
