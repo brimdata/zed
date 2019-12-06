7d2cdf3b66d26988ed05f4dcd61c736b9c375c3c
aswan <aswan@looky.cloud>
2019-12-06 13:59:52 -0600

    Move AST construction routines out of zql.peg (#102)
    This unclutters the grammar and also makes editor higlighting/formatting/etc
    on the supporting go and js code work smoothly.

--- zql/zql.js
+++ zql/zql.js
@@ -6099,119 +6099,121 @@ function peg$parse(input, options) {
   }
 
 
-      let reglob = require("../reglob/reglob")
 
-      function makeSequentialProc(procs) {
-          return { op: "SequentialProc", procs };
-      }
+  let reglob = require("../reglob/reglob")
 
-      function makeParallelProc(procs) {
-          return { op: "ParallelProc", procs };
-      }
+    function makeSequentialProc(procs) {
+      return { op: "SequentialProc", procs };
+    }
 
-      function makeTypedValue(type, value) { return { type, value }; }
-      function getValueType(v) { return v.type; }
+  function makeParallelProc(procs) {
+    return { op: "ParallelProc", procs };
+  }
 
-      function makeFieldCall(fn, field, param) {
-          return { op: "FieldCall", fn, field, param };
-      }
-      function chainFieldCalls(base, derefs) {
-          let ret = { op: "FieldRead", field: base };
-          for (let d of derefs) {
-              d.field = ret
-              ret = d
-          }
-          return ret
-      }
+  function makeTypedValue(type, value) { return { type, value }; }
+  function getValueType(v) { return v.type; }
 
-      function makeBooleanLiteral(value) {
-          return { op: "BooleanLiteral", value };
-      }
+  function makeFieldCall(fn, field, param) {
+    return { op: "FieldCall", fn, field, param };
+  }
+  function chainFieldCalls(base, derefs) {
+    let ret = { op: "FieldRead", field: base };
+    for (let d of derefs) {
+      d.field = ret
+      ret = d
+    }
+    return ret
+  }
 
-      function makeCompareField(comparator, field, value) {
-          return { op: "CompareField", comparator, field, value };
-      }
+  function makeBooleanLiteral(value) {
+    return { op: "BooleanLiteral", value };
+  }
 
-      function makeCompareAny(comparator, recursive, value) {
-          return { op: "CompareAny", comparator, recursive, value };
-      }
+  function makeCompareField(comparator, field, value) {
+    return { op: "CompareField", comparator, field, value };
+  }
 
-      function makeLogicalNot(expr) { return { op: "LogicalNot", expr }; }
+  function makeCompareAny(comparator, recursive, value) {
+    return { op: "CompareAny", comparator, recursive, value };
+  }
 
-      function makeChain(first, rest, op) {
-          if (!rest || rest.length == 0) {
-              return first;
-          }
-          let result = first;
-          for (let term of rest) {
-              result = { op, left: result, right: term };
-          }
-          return result;
-      }
+  function makeLogicalNot(expr) { return { op: "LogicalNot", expr }; }
 
-      function makeOrChain(first, rest) {
-          return makeChain(first, rest, "LogicalOr");
-      }
-      function makeAndChain(first, rest) {
-          return makeChain(first, rest, "LogicalAnd");
-      }
+  function makeChain(first, rest, op) {
+    if (!rest || rest.length == 0) {
+      return first;
+    }
+    let result = first;
+    for (let term of rest) {
+      result = { op, left: result, right: term };
+    }
+    return result;
+  }
 
-      function makeSearchString(value) {
-          return { op: "SearchString", value };
-      }
-      function resetSearchStringType(v) {
-          v.type = "string";
-      }
+  function makeOrChain(first, rest) {
+    return makeChain(first, rest, "LogicalOr");
+  }
+  function makeAndChain(first, rest) {
+    return makeChain(first, rest, "LogicalAnd");
+  }
 
-      function makeSortProc(fields, sortdir, limit) {
-          if (limit === null) { limit = undefined; }
-          return { op: "SortProc", fields, sortdir, limit };
-      }
+  function makeSearchString(value) {
+    return { op: "SearchString", value };
+  }
+  function resetSearchStringType(v) {
+    v.type = "string";
+  }
 
-      function makeTopProc(fields, limit, flush) {
-          if (limit === null) { limit = undefined; }
-          if (fields === null) { fields = undefined; }
-          flush = !!flush
-          return { op: "TopProc", fields, limit, flush};
-      }
+  function makeSortProc(fields, sortdir, limit) {
+    if (limit === null) { limit = undefined; }
+    return { op: "SortProc", fields, sortdir, limit };
+  }
 
-      function makeCutProc(fields) { return { op: "CutProc", fields }; }
-      function makeHeadProc(count) { return { op: "HeadProc", count }; }
-      function makeTailProc(count) { return { op: "TailProc", count }; }
-      function makeUniqProc(cflag) { return { op: "TailProc", cflag }; }
-      function makeFilterProc(filter) { return { op: "FilterProc", filter }; }
-      function makeReducer(op, var_, field) {
-          if (field === null) { field = undefined; }
-          return { op, var: var_, field };
-      }
-      function overrideReducerVar(reducer, v) {
-          reducer.var = v;
-          return reducer;
-      }
+  function makeTopProc(fields, limit, flush) {
+    if (limit === null) { limit = undefined; }
+    if (fields === null) { fields = undefined; }
+    flush = !!flush
+    return { op: "TopProc", fields, limit, flush};
+  }
 
-      function makeDuration(seconds) {
-          return {type: "Duration", seconds};
-      }
+  function makeCutProc(fields) { return { op: "CutProc", fields }; }
+  function makeHeadProc(count) { return { op: "HeadProc", count }; }
+  function makeTailProc(count) { return { op: "TailProc", count }; }
+  function makeUniqProc(cflag) { return { op: "TailProc", cflag }; }
+  function makeFilterProc(filter) { return { op: "FilterProc", filter }; }
+  function makeReducer(op, var_, field) {
+    if (field === null) { field = undefined; }
+    return { op, var: var_, field };
+  }
+  function overrideReducerVar(reducer, v) {
+    reducer.var = v;
+    return reducer;
+  }
 
-      function makeReducerProc(reducers) {
-          return { op: "ReducerProc", reducers };
-      }
-      function makeGroupByProc(duration, limit, keys, reducers) {
-          if (limit === null) { limit = undefined; }
-          return { op: "GroupByProc", keys, reducers, duration, limit };
-      }
+  function makeDuration(seconds) {
+    return {type: "Duration", seconds};
+  }
 
-      function joinChars(chars) {
-          return chars.join("");
-      }
+  function makeReducerProc(reducers) {
+    return { op: "ReducerProc", reducers };
+  }
+  function makeGroupByProc(duration, limit, keys, reducers) {
+    if (limit === null) { limit = undefined; }
+    return { op: "GroupByProc", keys, reducers, duration, limit };
+  }
 
-      function toLowerCase(str) {
-          return str.toLowerCase();
-      }
+  function joinChars(chars) {
+    return chars.join("");
+  }
 
-      function OR(a, b) {
-          return a || b
-      }
+  function toLowerCase(str) {
+    return str.toLowerCase();
+  }
+
+  function OR(a, b) {
+    return a || b
+  }
+
 
 
   peg$result = peg$startRuleFunction();
