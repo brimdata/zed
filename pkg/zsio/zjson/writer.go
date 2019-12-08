ac5967310bf1ad37db5c8cf4418c203bfc353f68
Steve McCanne <steve@mccanne.com>
2019-12-07 20:38:36 -0800

    refactor zjson writer into a writer and a stream (#115)
    This commit refactors the zjson writer so a stream can be created
    that transforms a sequence of zson.Records to a sequence of
    zjson.Records.  This is useful for building up arrays of zjson.Records
    and marshaling them all together.

--- pkg/zsio/zjson/writer.go
+++ pkg/zsio/zjson/writer.go
@@ -2,12 +2,10 @@ package zjson
 
 import (
 	"encoding/json"
-	"errors"
 	"io"
 
 	"github.com/mccanne/zq/pkg/zeek"
 	"github.com/mccanne/zq/pkg/zson"
-	"github.com/mccanne/zq/pkg/zson/resolver"
 	"github.com/mccanne/zq/pkg/zval"
 )
 
@@ -24,40 +22,22 @@ type Record struct {
 
 type Writer struct {
 	io.Writer
-	tracker *resolver.Tracker
+	stream *Stream
 }
 
 func NewWriter(w io.Writer) *Writer {
 	return &Writer{
-		Writer:  w,
-		tracker: resolver.NewTracker(),
+		Writer: w,
+		stream: NewStream(),
 	}
 }
 
 func (w *Writer) Write(r *zson.Record) error {
-	id := r.Descriptor.ID
-	var typ []interface{}
-	if !w.tracker.Seen(id) {
-		var err error
-		typ, err = EncodeType(r.Descriptor.Type)
-		if err != nil {
-			return err
-		}
-	}
-	v, err := w.encodeContainer(r.Raw)
+	rec, err := w.stream.Transform(r)
 	if err != nil {
 		return err
 	}
-	values, ok := v.([]interface{})
-	if !ok {
-		return errors.New("internal error: zson record body must be a container")
-	}
-	rec := Record{
-		Id:     id,
-		Type:   typ,
-		Values: values,
-	}
-	b, err := json.Marshal(&rec)
+	b, err := json.Marshal(rec)
 	if err != nil {
 		return err
 	}
@@ -106,28 +86,4 @@ func (w *Writer) encodeContainer(val []byte) (interface{}, error) {
 		}
 	}
 	return body, nil
-}
-
-// Encode a type as a resursive set of JSON objects.  We could simply encode
-// the top level type string, but then a javascript client would need to have
-// a type parser.  Instead, we encode recursive record types as a nested set
-// of objects so a javascript client can easily call JSON.parse() and have
-// the record structure present in an easy-to-navigate nested object.
-func EncodeType(typ *zeek.TypeRecord) ([]interface{}, error) {
-	var columns []interface{}
-	for _, c := range typ.Columns {
-		childRec, ok := c.Type.(*zeek.TypeRecord)
-		var typ interface{}
-		if ok {
-			var err error
-			typ, err = EncodeType(childRec)
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			typ = c.Type.String()
-		}
-		columns = append(columns, Column{Name: c.Name, Type: typ})
-	}
-	return columns, nil
 }
