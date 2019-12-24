65f3b2000ac346a9f453793927113866772c329e
Henri DF <henridf@gmail.com>
2019-12-24 10:06:15 +0100

    fix gzip detection

--- pkg/zio/detector/gzip.go
+++ pkg/zio/detector/gzip.go
@@ -8,8 +8,10 @@ import (
 func GzipReader(r io.Reader) io.Reader {
 	recorder := NewRecorder(r)
 	track := NewTrack(recorder)
-	r, err := gzip.NewReader(track)
+	_, err := gzip.NewReader(track)
 	if err == nil {
+		// create a new reader from recorder (track keeps a copy of read data)
+		r, _ := gzip.NewReader(recorder)
 		return r
 	}
 	return recorder
