a71528f415ac328d7c32924150cd0610059bf6cd
Noah Treuhaft <noah.treuhaft@gmail.com>
2020-01-13 09:43:49 -0800

    simplify zcode.Builder (#192)

--- zcode/builder.go
+++ zcode/builder.go
@@ -1,136 +1,75 @@
 package zcode
 
-const (
-	beginContainer = -1
-	endContainer   = -2
+import (
+	"encoding/binary"
 )
 
-type node struct {
-	innerLen  int
-	outerLen  int
-	dfs       int
-	container bool
-}
-
-// Builder implements an API for holding an intermediate representation
-// of a hierarchical set of values arranged in a tree, e.g., structured
-// values that can contain nested and recursive aggregate values.
-// We encode a DFS traversal in a flat data structure that can be
-// reused across invocations so we don't otherwise allocate a tree
-// data structure for every record parsed that would then be GC'd.
+// Builder provides an efficient API for constructing nested ZNG values.
 type Builder struct {
-	nodes  []node
-	leaves [][]byte
+	bytes      Bytes
+	containers []int // Stack of open containers (as body offsets within bytes).
 }
 
+// NewBuilder returns a new Builder.
 func NewBuilder() *Builder {
-	return &Builder{
-		nodes:  make([]node, 0, 64),
-		leaves: make([][]byte, 0, 64),
+	return &Builder{}
+}
+
+// Reset resets the Builder to be empty.
+func (b *Builder) Reset() {
+	b.bytes = nil
+	b.containers = b.containers[:0]
+}
+
+// BeginContainer opens a new container.
+func (b *Builder) BeginContainer() {
+	// Allocate one byte for the container tag.  When EndContainer writes
+	// the tag, it will arrange for additional space if required.
+	b.bytes = append(b.bytes, 0)
+	// Push the offset of the container body onto the stack.
+	b.containers = append(b.containers, len(b.bytes))
+}
+
+// EndContainer closes the most recently opened container.  It panics if there
+// is no open container.
+func (b *Builder) EndContainer() {
+	// Pop the container body offset off the stack.
+	bodyOff := b.containers[len(b.containers)-1]
+	b.containers = b.containers[:len(b.containers)-1]
+	tag := containerTag(len(b.bytes) - bodyOff)
+	tagSize := sizeOfUvarint(tag)
+	// BeginContainer allocated one byte for the container tag.
+	tagOff := bodyOff - 1
+	if tagSize > 1 {
+		// Need additional space for the tag, so move body over.
+		b.bytes = append(b.bytes[:tagOff+tagSize], b.bytes[bodyOff:]...)
+	}
+	if binary.PutUvarint(b.bytes[tagOff:], tag) != tagSize {
+		panic("bad container tag size")
 	}
 }
 
-func (b *Builder) Reset() {
-	b.nodes = b.nodes[:0]
-	b.leaves = b.leaves[:0]
-}
-
-func (b *Builder) BeginContainer() {
-	b.nodes = append(b.nodes, node{dfs: beginContainer})
-}
-
-func (b *Builder) EndContainer() {
-	b.nodes = append(b.nodes, node{dfs: endContainer})
-}
-
+// AppendUnsetContainer appends an unset container.
 func (b *Builder) AppendUnsetContainer() {
 	b.Append(nil, true)
 }
 
+// AppendUnsetContainer appends an unset value.
 func (b *Builder) AppendUnsetValue() {
 	b.Append(nil, false)
 }
 
+// Append appends leaf as a container if the container boolean is true or as a
+// value otherwise.
 func (b *Builder) Append(leaf []byte, container bool) {
-	k := len(b.leaves)
-	b.leaves = append(b.leaves, leaf)
-	b.nodes = append(b.nodes, node{dfs: k, container: container})
+	b.bytes = Append(b.bytes, leaf, container)
 }
 
-func (b *Builder) measure(off int) int {
-	node := &b.nodes[off]
-	dfs := node.dfs
-	if dfs == beginContainer {
-		// skip over start token
-		off++
-		for off < len(b.nodes) {
-			next := b.measure(off)
-			if next < 0 {
-				// skip over end token
-				off++
-				break
-			}
-			node.innerLen += b.nodes[off].outerLen
-			off = next
-		}
-		node.outerLen = sizeOfContainer(node.innerLen)
-		return off
-	}
-	if dfs == endContainer {
-		return -1
-	}
-	n := len(b.leaves[dfs])
-	node.innerLen = n
-	node.outerLen = sizeOfValue(n)
-	return off + 1
-}
-
-func (b *Builder) encode(dst []byte, off int) (Bytes, int) {
-	node := &b.nodes[off]
-	dfs := node.dfs
-	if dfs == beginContainer {
-		// skip over start token
-		off++
-		dst = AppendUvarint(dst, containerTag(node.innerLen))
-		for off < len(b.nodes) {
-			var next int
-			dst, next = b.encode(dst, off)
-			if next < 0 {
-				// skip over end token
-				off++
-				break
-			}
-			off = next
-		}
-		return dst, off
-	}
-	if dfs == endContainer {
-		return dst, -1
-	}
-	if node.container {
-		return AppendContainerValue(dst, b.leaves[dfs]), off + 1
-	}
-	return AppendValue(dst, b.leaves[dfs]), off + 1
-}
-
+// Encode returns the constructed value.  It panics if the Builder has an open
+// container.
 func (b *Builder) Encode() Bytes {
-	off := 0
-	for off < len(b.nodes) {
-		next := b.measure(off)
-		if next < 0 {
-			break
-		}
-		off = next
+	if len(b.containers) > 0 {
+		panic("open container")
 	}
-	off = 0
-	var zv []byte
-	for off < len(b.nodes) {
-		var next int
-		zv, next = b.encode(zv, off)
-		if next < 0 {
-			break
-		}
-		off = next
-	}
-	return zv
+	return b.bytes
 }
