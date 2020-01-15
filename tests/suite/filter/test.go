353258bfdb95c2d20c130004958dd78eaa5a5ad9
Andrew Swan <aswan@looky.cloud>
2020-01-15 10:17:24 -0600

    add test of escaped search characters

---
+++ tests/suite/filter/test.go
@@ -0,0 +1,51 @@
+package filter
+
+import (
+	"github.com/mccanne/zq/pkg/test"
+)
+
+const in = `
+#0:record[s:string]
+0:[A=B;]
+0:[A=*;]
+`
+
+const out1 = `
+#0:record[s:string]
+0:[A=B;]
+`
+
+const out2 = `
+#0:record[s:string]
+0:[A=*;]
+`
+
+const out3 = `
+#0:record[s:string]
+0:[A=B;]
+0:[A=*;]
+`
+
+var EscapedEqual = test.Internal{
+	Name:         "Filter Escaped =",
+	Query:        `A\=B`,
+	Input:        test.Trim(in),
+	OutputFormat: "zng",
+	Expected:     test.Trim(out1),
+}
+
+var EscapedAsterisk = test.Internal{
+	Name:         "Filter Escaped =",
+	Query:        `A\=\*`,
+	Input:        test.Trim(in),
+	OutputFormat: "zng",
+	Expected:     test.Trim(out2),
+}
+
+var UnescapedAsterisk = test.Internal{
+	Name:         "Filter Escaped =",
+	Query:        `A\=*`,
+	Input:        test.Trim(in),
+	OutputFormat: "zng",
+	Expected:     test.Trim(out3),
+}
