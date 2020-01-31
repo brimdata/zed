1e9dfd62cd07e7c05162a0ed5d2867089085d5a4
Noah Treuhaft <noah.treuhaft@gmail.com>
2020-01-31 13:45:53 -0800

    simplify proc/top_test.go (#277)

--- proc/top_test.go
+++ proc/top_test.go
@@ -3,39 +3,24 @@ package proc_test
 import (
 	"testing"
 
-	"github.com/mccanne/zq/pkg/nano"
 	"github.com/mccanne/zq/proc"
-	"github.com/mccanne/zq/zbuf"
-	"github.com/mccanne/zq/zng"
-	"github.com/mccanne/zq/zng/resolver"
-	"github.com/stretchr/testify/require"
 )
 
 func TestTop(t *testing.T) {
-	zctx := resolver.NewContext()
-
-	fooDesc := zctx.LookupTypeRecord([]zng.Column{zng.NewColumn("foo", zng.TypeCount)})
-	r0, _ := zbuf.NewRecordZeekStrings(fooDesc, "-")
-	r1, _ := zbuf.NewRecordZeekStrings(fooDesc, "1")
-	r2, _ := zbuf.NewRecordZeekStrings(fooDesc, "2")
-	r3, _ := zbuf.NewRecordZeekStrings(fooDesc, "3")
-	r4, _ := zbuf.NewRecordZeekStrings(fooDesc, "4")
-	r5, _ := zbuf.NewRecordZeekStrings(fooDesc, "5")
-	fooBatch := zbuf.NewArray([]*zng.Record{r0, r1, r2, r3, r4, r5}, nano.MaxSpan)
-
-	test, err := proc.NewProcTestFromSource("top 3 foo", zctx, []zbuf.Batch{fooBatch})
-	require.NoError(t, err)
-
-	res, err := test.Pull()
-	require.NoError(t, err)
-	require.NoError(t, test.ExpectEOS())
-	require.NoError(t, test.Finish())
-	require.Equal(t, 3, res.Length())
-	var ints []int64
-	for i := 0; i < 3; i++ {
-		foo, err := res.Index(i).AccessInt("foo")
-		require.NoError(t, err)
-		ints = append(ints, foo)
-	}
-	require.Equal(t, []int64{5, 4, 3}, ints)
+	const in = `
+#0:record[foo:count]
+0:[-;]
+0:[1;]
+0:[2;]
+0:[3;]
+0:[4;]
+0:[5;]
+`
+	const out = `
+#0:record[foo:count]
+0:[5;]
+0:[4;]
+0:[3;]
+`
+	proc.TestOneProc(t, in, out, "top 3 foo")
 }
