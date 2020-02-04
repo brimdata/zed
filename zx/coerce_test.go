b881612a53a016a8027675b51f1617599e2c13f1
Matthew Nibecker <hello@mattnibecker.com>
2020-02-03 20:27:57 -0800

    Coerce ints to durations as well (#285)

---
+++ zx/coerce_test.go
@@ -0,0 +1,35 @@
+package zx_test
+
+import (
+	"testing"
+
+	"github.com/mccanne/zq/zng"
+	"github.com/mccanne/zq/zx"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCoerceDuration(t *testing.T) {
+	interval := zng.NewInterval(60 * 1e9)
+	runcase(t, "Int64", zng.NewInt(60), interval)
+	runcase(t, "Uint64", zng.NewCount(60), interval)
+	runcase(t, "Float64", zng.NewDouble(60), interval)
+	runcase(t, "Duration", zng.NewInterval(60*1e9), interval)
+
+	// can't coerce
+	notcase(t, "NotPort", zng.NewPort(60), zng.TypeInterval)
+}
+
+func runcase(t *testing.T, name string, in zng.Value, expected zng.Value) {
+	t.Run(name, func(t *testing.T) {
+		val, ok := zx.Coerce(in, expected.Type)
+		require.Equal(t, true, ok)
+		require.Equal(t, expected, val)
+	})
+}
+
+func notcase(t *testing.T, name string, in zng.Value, typ zng.Type) {
+	t.Run(name, func(t *testing.T) {
+		_, ok := zx.Coerce(in, typ)
+		require.Equal(t, false, ok)
+	})
+}
