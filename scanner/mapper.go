bf377393b27180a3d13618cbe24732559a100a03
Steven McCanne <steve@mccanne.com>
2020-02-05 09:26:53 -0800

    fix bug with type context in zqd search end point
    This commit fixes a bug where the reader's type context was used
    during the search instead of a blank context causing a clash between
    dynamic results and the input file types.  We fix this by allocating
    a context mapper in the search path.

---
+++ scanner/mapper.go
@@ -0,0 +1,36 @@
+package scanner
+
+import (
+	"github.com/mccanne/zq/zbuf"
+	"github.com/mccanne/zq/zng"
+	"github.com/mccanne/zq/zng/resolver"
+)
+
+type Mapper struct {
+	zbuf.Reader
+	mapper *resolver.Mapper
+}
+
+func NewMapper(reader zbuf.Reader, zctx *resolver.Context) *Mapper {
+	return &Mapper{
+		Reader: reader,
+		mapper: resolver.NewMapper(zctx),
+	}
+}
+
+func (m *Mapper) Read() (*zng.Record, error) {
+	rec, err := m.Reader.Read()
+	if err != nil {
+		return nil, err
+	}
+	if rec == nil {
+		return nil, nil
+	}
+	id := rec.Type.ID()
+	sharedType := m.mapper.Map(id)
+	if sharedType == nil {
+		sharedType = m.mapper.Enter(id, rec.Type)
+	}
+	rec.Type = sharedType
+	return rec, nil
+}
