3d4a9ffeadbef0ecdd0f9b6025d2071cd194d1fd
aswan <aswan@looky.cloud>
2020-02-10 15:40:55 -0600

    Map Zeek enum to ZNG string (#312)
    * Map Zeek enum to ZNG string
    
    * feedback

--- zio/zeekio/types.go
+++ zio/zeekio/types.go
@@ -7,31 +7,44 @@ import (
 	"github.com/brimsec/zq/zng"
 )
 
+// Compatibility between the Zeek and ZNG type systems has a few rough
+// edges.  Several types have to be rewritten before we get into ZNG:
+//  - Zeek "vector" is ZNG "array", since this is a container and not
+//    a fully-specific type we have to rewrite it here.
+//  - Zeek "string" corresponds to ZNG "bstring".  Since "string" already
+//    exists in ZNG, we can't use an alias and just rewrite the name directly.
+//  - Zeek "enum" corresponds to ZNG "string".  There is a desire to
+//    eventually add "enum" to ZNG so we don't use an alias but rewrite
+//    "enum" to "zenum" which is aliased to "string" (using the alias lets
+//    us recover the original type when writing Zeek output.
+//
+// The function zeekTypeToZng() is used when reading Zeek logs to rewrite
+// types before looking up the proper Zeek type.  zngTypeToZeek() is used
+// when writing Zeek logs, it should always be the inverse of zeekTypeToZng().
+
 func zeekTypeToZng(typ string) string {
 	// As zng types diverge from zeek types, we'll probably want to
 	// re-do this but lets keep it simple for now.
 	typ = strings.ReplaceAll(typ, "string", "bstring")
-	return strings.ReplaceAll(typ, "vector", "array")
+	typ = strings.ReplaceAll(typ, "enum", "zenum")
+	typ = strings.ReplaceAll(typ, "vector", "array")
+	return typ
 }
 
-type typeContainer struct {
-	label string
-	inner fmt.Stringer
-}
-
-func (v typeContainer) String() string {
-	return fmt.Sprintf("%s[%s]", v.label, v.inner.String())
-}
-
-func zngTypeToZeek(typ zng.Type) fmt.Stringer {
+func zngTypeToZeek(typ zng.Type) string {
 	switch typ := typ.(type) {
 	case *zng.TypeArray:
-		return typeContainer{"vector", zngTypeToZeek(typ.Type)}
+		return fmt.Sprintf("vector[%s]", zngTypeToZeek(typ.Type))
 	case *zng.TypeSet:
-		return typeContainer{"set", zngTypeToZeek(typ.InnerType)}
+		return fmt.Sprintf("set[%s]", zngTypeToZeek(typ.InnerType))
 	case *zng.TypeOfBstring:
-		return zng.TypeString
+		return "string"
+	case *zng.TypeAlias:
+		if typ.Name == "zenum" {
+			return "enum"
+		}
+		return typ.String()
 	default:
-		return typ
+		return typ.String()
 	}
 }
