35e6c5d196deab089c809d6140e089611a07801f
Michael Brown <mikeb@apache.org>
2020-02-18 10:02:45 -0800

    support truncated output in markdown examples
    Extend the markdown info string for zq-output to include an optional
    'head:N' directive, where only the first N lines of output of a
    zq-command are verified against its zq-output.
    
    To find these we must extend our AST walk function. Rename ZQExamplePair
    to ZQExampleInfo; add a new struct member to hold the number of lines to
    compare. Do the same for ZQExampleTest.
    
    Move the responsibility of actually running the zq-command to a
    ZQExampleTest Run() method. This also handles any truncation.

--- tests/heavy_test.go
+++ tests/heavy_test.go
@@ -3,7 +3,6 @@
 package tests
 
 import (
-	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,10 +17,9 @@ func TestMarkdownExamples(t *testing.T) {
 		exampletest := exampletest
 		t.Run(exampletest.Name, func(t *testing.T) {
 			t.Parallel()
-			c := exec.Command(exampletest.Command[0], exampletest.Command[1:]...)
-			cmdOutput, err := c.CombinedOutput()
+			cmdOutput, err := exampletest.Run()
 			require.Equal(t, nil, err, "error running command %v: %v", exampletest.Command, err)
-			assert.Equal(t, exampletest.Expected, string(cmdOutput), "output mismatch with command %v", exampletest.Command)
+			assert.Equal(t, exampletest.Expected, cmdOutput, "output mismatch with command %v", exampletest.Command)
 		})
 	}
 }
