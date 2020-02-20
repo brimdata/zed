97d9ed6e4abc6153e1f39a7e8399f49fe9850fbc
Noah Treuhaft <noah.treuhaft@gmail.com>
2020-02-19 23:22:08 -0800

    convert most tests in tests/ to YAML ztests
    * Add package ztest to run formulaic tests defined in YAML files that
      look like this:
    
        zql: count()
    
        input: |
          #0:record[i:int64]
          0:[1;]
          0:[2;]
    
        output: |
          #0:record[count:uint64]
          0:[2;]
    
    * Convert tests in tests/formats and most of those in tests/suite to
      YAML.  As a side effect, both "make test-unit" and "make test-system"
      now run all of the converted tests.  (test-unit uses
      driver.(*Driver).Run, while test-system uses the dist/zq executable).

--- go.mod
+++ go.mod
@@ -9,8 +9,10 @@ require (
 	github.com/mccanne/charm v0.0.3-0.20191224190439-b05e1b7b1be3
 	github.com/mccanne/joe v0.0.0-20181124064909-25770742c256
 	github.com/peterh/liner v1.1.0
+	github.com/pmezard/go-difflib v1.0.0
 	github.com/stretchr/testify v1.4.0
 	github.com/yuin/goldmark v1.1.22
 	go.uber.org/zap v1.12.0
 	golang.org/x/text v0.3.0
+	gopkg.in/yaml.v3 v3.0.0-20200121175148-a6ecf24a6d71
 )
--- tests/suite.go
+++ tests/suite.go
@@ -2,30 +2,14 @@ package tests
 
 import (
 	"github.com/brimsec/zq/pkg/test"
-	"github.com/brimsec/zq/tests/suite/count"
-	"github.com/brimsec/zq/tests/suite/cut"
 	"github.com/brimsec/zq/tests/suite/diropt"
 	"github.com/brimsec/zq/tests/suite/errors"
-	"github.com/brimsec/zq/tests/suite/filter"
-	"github.com/brimsec/zq/tests/suite/format"
-	"github.com/brimsec/zq/tests/suite/input"
-	"github.com/brimsec/zq/tests/suite/reducer"
-	"github.com/brimsec/zq/tests/suite/regexp"
-	"github.com/brimsec/zq/tests/suite/sort"
-	"github.com/brimsec/zq/tests/suite/time"
 	"github.com/brimsec/zq/tests/suite/utf8"
-	"github.com/brimsec/zq/tests/suite/zeek"
 )
 
 var RootDir = "./test-root"
 
 var internals = []test.Internal{
-	count.Internal,
-	cut.Internal,
-	format.Internal,
-	format.JsonTypes,
-	input.JSON,
-	input.Backslash,
 	errors.DuplicateFields,
 	errors.ErrNotPrimitive,
 	errors.ErrNotPrimitiveZJSON,
@@ -37,34 +21,9 @@ var internals = []test.Internal{
 	errors.EmptySetType,
 	errors.EmptyUnionType,
 	errors.RedefineAlias,
-	regexp.Internal,
-	filter.EscapedEqual,
-	filter.EscapedAsterisk,
-	filter.UnescapedAsterisk,
-	filter.NullWithNonexistentField,
-	filter.NullWithUnsetField,
-	reducer.UnsetAvg,
-	reducer.UnsetCountDistinct,
-	reducer.UnsetCount,
-	reducer.UnsetFirst,
-	reducer.UnsetLast,
-	reducer.UnsetMax,
-	reducer.UnsetMin,
-	reducer.UnsetSum,
-	sort.Internal1,
-	sort.Internal2,
-	sort.Internal3,
-	sort.Internal4_1,
-	sort.Internal4_2,
-	sort.Internal4_3,
-	sort.Internal4_4,
-	time.Internal,
-	zeek.Test,
 }
 
 var commands = []test.Exec{
-	cut.Exec,
-	errors.Exec,
 	utf8.Exec,
 	// this test doesn't work in circleCI apparently because it does
 	// a pipeline...
