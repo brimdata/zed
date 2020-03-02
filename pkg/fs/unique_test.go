78d95f2fa7b5f368aba344d1cd29bf8e2c2be23d
Alfred Landrum <alfred@leakybucket.org>
2020-03-01 19:26:14 -0800

    return error if uniquedir exhausts tries

---
+++ pkg/fs/unique_test.go
@@ -0,0 +1,22 @@
+package fs
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUniqueDir(t *testing.T) {
+	tdir, err := ioutil.TempDir("", "")
+	require.NoError(t, err)
+
+	err = os.Mkdir(path.Join(tdir, "foo"), 0700)
+	require.NoError(t, err)
+
+	_, err = uniqueDir(tdir, "foo", 1)
+	require.Error(t, err)
+	require.True(t, os.IsExist(err))
+}
