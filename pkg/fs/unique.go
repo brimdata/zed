f77ded6847b15d1240ef1939d4146ec41fd4e031
Alfred Landrum <alfred@leakybucket.org>
2020-03-02 10:10:10 -0800

    return error if uniquedir exhausts tries (#367)

--- pkg/fs/unique.go
+++ pkg/fs/unique.go
@@ -12,17 +12,25 @@ import (
 // exists, the dir /usr/mydir_01 is returned (and so on).  Path extensions are
 // respected and the unique number is appended before any extensions
 // (e.g. mydir.brim -> mydir_01.txt).
-func UniqueDir(parent, name string) (string, error) {
+func UniqueDir(parent, name string) (dir string, err error) {
+	return uniqueDir(parent, name, 1000)
+}
+
+func uniqueDir(parent, name string, tries int) (dir string, err error) {
+	if tries <= 0 {
+		panic("bad tries for uniqueDir")
+	}
 	var ext string
 	if n := strings.Index(name, "."); n != -1 {
 		ext = name[n:]
 	}
 	base := strings.TrimSuffix(name, ext)
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < tries; i++ {
 		if i != 0 {
 			name = fmt.Sprintf("%s_%02d%s", base, i, ext)
 		}
-		err := os.Mkdir(filepath.Join(parent, name), 0700)
+		dir = filepath.Join(parent, name)
+		err = os.Mkdir(dir, 0700)
 		if os.IsExist(err) {
 			continue
 		} else if err != nil {
@@ -30,5 +38,5 @@ func UniqueDir(parent, name string) (string, error) {
 		}
 		break
 	}
-	return filepath.Join(parent, name), nil
+	return dir, err
 }
