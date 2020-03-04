b2b1a83ccca4f7151e0572c77788d87efec70b9d
Henri DF <henridf@gmail.com>
2020-03-04 15:50:13 +0100

    Add driver.NewDemuxed()
    driver.NewDemuxed() takes a slice of zbuf.Writers which must be of
    same length as the number of leafs in the flowgraph that is then run
    on it. This allows clients to distinguish leaves whereas driver.New()
    multiplexes all records into a same zbuf.Writer.

---
+++ driver/driver_test.go
@@ -0,0 +1,64 @@
+package driver
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/brimsec/zq/scanner"
+	"github.com/brimsec/zq/zbuf"
+	"github.com/brimsec/zq/zio/zngio"
+	"github.com/brimsec/zq/zng"
+	"github.com/brimsec/zq/zng/resolver"
+	"github.com/brimsec/zq/zql"
+	"github.com/stretchr/testify/assert"
+)
+
+type counter struct {
+	n int
+}
+
+func (c *counter) Write(*zng.Record) error {
+	c.n++
+	return nil
+}
+
+func TestMuxDriver(t *testing.T) {
+	input := `#0:record[_path:string,ts:time]
+0:[conn;1425565514.419939;]`
+
+	zctx := resolver.NewContext()
+	query, err := zql.ParseProc("(tail 1; tail 1)")
+	assert.NoError(t, err)
+
+	t.Run("muxed into one writer", func(t *testing.T) {
+		reader := zngio.NewReader(strings.NewReader(input), zctx)
+		flowgraph, err := Compile(query, scanner.NewScanner(reader, nil))
+		assert.NoError(t, err)
+		c := counter{}
+		d := New(&c)
+		err = d.Run(flowgraph)
+		assert.NoError(t, err)
+		assert.Equal(t, 2, c.n)
+	})
+
+	t.Run("muxed into individual writers", func(t *testing.T) {
+		reader := zngio.NewReader(strings.NewReader(input), zctx)
+		flowgraph, err := Compile(query, scanner.NewScanner(reader, nil))
+		assert.NoError(t, err)
+		cs := []zbuf.Writer{&counter{}, &counter{}}
+		d := NewDemuxed(cs)
+		err = d.Run(flowgraph)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, cs[0].(*counter).n)
+		assert.Equal(t, 1, cs[0].(*counter).n)
+	})
+
+	t.Run("Mismatched channels and writer counts", func(t *testing.T) {
+		flowgraph, err := Compile(query, nil)
+		assert.NoError(t, err)
+		cs := []zbuf.Writer{&counter{}, &counter{}, &counter{}}
+		d := NewDemuxed(cs)
+		err = d.Run(flowgraph)
+		assert.Error(t, err)
+	})
+}
--- driver/run.go
+++ driver/run.go
@@ -10,13 +10,21 @@ import (
 )
 
 type Driver struct {
-	writer   zbuf.Writer
+	writers  []zbuf.Writer
 	warnings io.Writer
+	demux    bool
 }
 
 func New(w zbuf.Writer) *Driver {
 	return &Driver{
-		writer: w,
+		writers: []zbuf.Writer{w},
+	}
+}
+
+func NewDemuxed(ws []zbuf.Writer) *Driver {
+	return &Driver{
+		writers: ws,
+		demux:   true,
 	}
 }
 
@@ -25,8 +33,11 @@ func (d *Driver) SetWarningsWriter(w io.Writer) {
 }
 
 func (d *Driver) Write(cid int, arr zbuf.Batch) error {
+	if !d.demux {
+		cid = 0
+	}
 	for _, r := range arr.Records() {
-		if err := d.writer.Write(r); err != nil {
+		if err := d.writers[cid].Write(r); err != nil {
 			return err
 		}
 	}
@@ -44,6 +55,10 @@ func (d *Driver) WriteWarning(msg string) error {
 }
 
 func (d *Driver) Run(out *proc.MuxOutput) error {
+	if d.demux && len(d.writers) != out.N() {
+		return fmt.Errorf("Driver.Run(): Mismatched channels and writer counts")
+	}
+
 	for !out.Complete() {
 		res := out.Pull(time.After(time.Second * 10))
 		if res.Err == proc.ErrTimeout {
