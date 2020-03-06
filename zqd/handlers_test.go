82db18b2a3ffb757a9148fa863b6c8ea45deb82e
Matthew Nibecker <hello@mattnibecker.com>
2020-03-06 15:31:40 -0800

    space get endpoint: handle spaces w no data (#395)
    Currently querying GET /space/:space results in a 500 error:
    all.bzng does not exist. Make endpoint more robust and return
    a 200 with a space info response with the "size" field set to 0.

--- zqd/handlers_test.go
+++ zqd/handlers_test.go
@@ -86,6 +86,22 @@ func TestSpaceInfo(t *testing.T) {
 	require.Equal(t, expected, info)
 }
 
+func TestSpaceInfoNoData(t *testing.T) {
+	const space = "test"
+	c := newCore(t)
+	createSpace(t, c, space, "")
+	u := fmt.Sprintf("http://localhost:9867/space/%s", space)
+	body := httpSuccess(t, zqd.NewHandler(c), "GET", u, nil)
+	expected := api.SpaceInfo{
+		Name:          space,
+		Size:          0,
+		PacketSupport: false,
+	}
+	var info api.SpaceInfo
+	require.NoError(t, json.NewDecoder(body).Decode(&info))
+	require.Equal(t, expected, info)
+}
+
 func TestSpacePostNameOnly(t *testing.T) {
 	c := newCore(t)
 	defer os.RemoveAll(c.Root)
