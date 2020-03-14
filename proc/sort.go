49ddc501e556a4c8281c2cf49a22cc691e998428
Noah Treuhaft <noah.treuhaft@gmail.com>
2020-03-13 17:06:59 -0700

    Warn about unmatched sort field expressions (#417)

--- proc/sort.go
+++ proc/sort.go
@@ -3,6 +3,7 @@ package proc
 import (
 	"fmt"
 
+	"github.com/brimsec/zq/ast"
 	"github.com/brimsec/zq/expr"
 	"github.com/brimsec/zq/pkg/nano"
 	"github.com/brimsec/zq/zbuf"
@@ -20,7 +21,8 @@ type Sort struct {
 	dir        int
 	limit      int
 	nullsFirst bool
-	fields     []expr.FieldExprResolver
+	fields     []ast.FieldExpr
+	resolvers  []expr.FieldExprResolver
 	out        []*zng.Record
 }
 
@@ -29,11 +31,23 @@ type Sort struct {
 // The value can be overridden by setting the limit param on the SortProc.
 const defaultSortLimit = 1000000
 
-func NewSort(c *Context, parent Proc, limit int, fields []expr.FieldExprResolver, dir int, nullsFirst bool) *Sort {
+func CompileSortProc(c *Context, parent Proc, node *ast.SortProc) (*Sort, error) {
+	limit := node.Limit
 	if limit == 0 {
 		limit = defaultSortLimit
 	}
-	return &Sort{Base: Base{Context: c, Parent: parent}, dir: dir, limit: limit, nullsFirst: nullsFirst, fields: fields}
+	resolvers, err := expr.CompileFieldExprs(node.Fields)
+	if err != nil {
+		return nil, err
+	}
+	return &Sort{
+		Base:       Base{Context: c, Parent: parent},
+		dir:        node.SortDir,
+		limit:      limit,
+		nullsFirst: node.NullsFirst,
+		fields:     node.Fields,
+		resolvers:  resolvers,
+	}, nil
 }
 
 func firstOf(typ *zng.TypeRecord, which []zng.Type) string {
@@ -111,7 +125,7 @@ func (s *Sort) sort() zbuf.Batch {
 		return nil
 	}
 	s.out = nil
-	if s.fields == nil {
+	if s.resolvers == nil {
 		fld := guessSortField(out[0])
 		resolver := func(r *zng.Record) zng.Value {
 			e, err := r.Access(fld)
@@ -120,16 +134,44 @@ func (s *Sort) sort() zbuf.Batch {
 			}
 			return e
 		}
-		s.fields = []expr.FieldExprResolver{resolver}
+		s.resolvers = []expr.FieldExprResolver{resolver}
+	} else {
+		s.warnAboutUnseenFields(out)
 	}
 	nullsMax := !s.nullsFirst
 	if s.dir < 0 {
 		nullsMax = !nullsMax
 	}
-	sorter := expr.NewSortFn(nullsMax, s.fields...)
+	sorter := expr.NewSortFn(nullsMax, s.resolvers...)
 	sortWithDir := func(a, b *zng.Record) int {
 		return s.dir * sorter(a, b)
 	}
 	expr.SortStable(out, sortWithDir)
 	return zbuf.NewArray(out, nano.NewSpanTs(s.MinTs, s.MaxTs))
+}
+
+func (s *Sort) warnAboutUnseenFields(records []*zng.Record) {
+	unseenFields := make(map[ast.FieldExpr]expr.FieldExprResolver)
+	for i, r := range s.resolvers {
+		unseenFields[s.fields[i]] = r
+	}
+	sawType := make(map[*zng.TypeRecord]bool)
+	for _, rec := range records {
+		if !sawType[rec.Type] {
+			sawType[rec.Type] = true
+			for field, res := range unseenFields {
+				if !res(rec).IsNil() {
+					delete(unseenFields, field)
+				}
+			}
+			if len(unseenFields) == 0 {
+				break
+			}
+		}
+	}
+	for _, f := range s.fields {
+		if _, ok := unseenFields[f]; ok {
+			s.Warnings <- fmt.Sprintf("Sort field %s not present in input", expr.FieldExprToString(f))
+		}
+	}
 }
