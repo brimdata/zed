255fbac526750512ab5a039bfb8d5cc4b7d04466
Andrew Swan <aswan@brimsecurity.com>
2020-03-24 17:24:10 -0600

    Add additional Math.* functions

--- expr/functions.go
+++ expr/functions.go
@@ -20,9 +20,77 @@ var allFns = map[string]struct {
 	maxArgs int
 	impl    Function
 }{
-	"Math.max":  {1, -1, mathMax},
-	"Math.min":  {1, -1, mathMin},
-	"Math.sqrt": {1, 1, mathSqrt},
+	"Math.abs":   {1, 1, mathAbs},
+	"Math.ceil":  {1, 1, mathCeil},
+	"Math.floor": {1, 1, mathFloor},
+	"Math.log":   {1, 1, mathLog},
+	"Math.max":   {1, -1, mathMax},
+	"Math.min":   {1, -1, mathMin},
+	"Math.mod":   {2, 2, mathMod},
+	"Math.round": {1, 1, mathRound},
+	"Math.pow":   {2, 2, mathPow},
+	"Math.sqrt":  {1, 1, mathSqrt},
+}
+
+func err(fn string, err error) (zngnative.Value, error) {
+	return zngnative.Value{}, fmt.Errorf("%s: %w", fn, err)
+}
+
+func mathAbs(args []zngnative.Value) (zngnative.Value, error) {
+	switch args[0].Type.ID() {
+	case zng.IdInt16, zng.IdInt32, zng.IdInt64:
+		x := args[0].Value.(int64)
+		if x < 0 {
+			x = -x
+		}
+		return zngnative.Value{zng.TypeInt64, x}, nil
+
+	case zng.IdByte, zng.IdUint16, zng.IdUint32, zng.IdUint64:
+		return args[0], nil
+
+	case zng.IdFloat64:
+		return zngnative.Value{zng.TypeFloat64, math.Abs(args[0].Value.(float64))}, nil
+
+	default:
+		return err("Math.abs", ErrBadArgument)
+	}
+}
+
+func mathCeil(args []zngnative.Value) (zngnative.Value, error) {
+	switch args[0].Type.ID() {
+	case zng.IdInt16, zng.IdInt32, zng.IdInt64, zng.IdByte, zng.IdUint16, zng.IdUint32, zng.IdUint64:
+		return args[0], nil
+
+	case zng.IdFloat64:
+		return zngnative.Value{zng.TypeFloat64, math.Ceil(args[0].Value.(float64))}, nil
+
+	default:
+		return err("Math.Ceil", ErrBadArgument)
+	}
+}
+
+func mathFloor(args []zngnative.Value) (zngnative.Value, error) {
+	switch args[0].Type.ID() {
+	case zng.IdInt16, zng.IdInt32, zng.IdInt64, zng.IdByte, zng.IdUint16, zng.IdUint32, zng.IdUint64:
+		return args[0], nil
+
+	case zng.IdFloat64:
+		return zngnative.Value{zng.TypeFloat64, math.Floor(args[0].Value.(float64))}, nil
+
+	default:
+		return err("Math.Floor", ErrBadArgument)
+	}
+}
+
+func mathLog(args []zngnative.Value) (zngnative.Value, error) {
+	x, ok := zngnative.CoerceNativeToFloat64(args[0])
+	if !ok {
+		return err("Math.log", ErrBadArgument)
+	}
+	if x <= 0 {
+		return err("Math.log", ErrBadArgument)
+	}
+	return zngnative.Value{zng.TypeFloat64, math.Log(x)}, nil
 }
 
 func mathMax(args []zngnative.Value) (zngnative.Value, error) {
@@ -58,7 +126,7 @@ func mathMax(args []zngnative.Value) (zngnative.Value, error) {
 		return zngnative.Value{zng.TypeFloat64, ret}, nil
 
 	default:
-		return zngnative.Value{}, fmt.Errorf("Math.max: %w", ErrBadArgument)
+		return err("Math.max", ErrBadArgument)
 	}
 }
 
@@ -95,8 +163,57 @@ func mathMin(args []zngnative.Value) (zngnative.Value, error) {
 		return zngnative.Value{zng.TypeFloat64, ret}, nil
 
 	default:
-		return zngnative.Value{}, fmt.Errorf("Math.min: %w", ErrBadArgument)
+		return err("Math.min", ErrBadArgument)
 	}
+}
+
+func mathMod(args []zngnative.Value) (zngnative.Value, error) {
+	y, ok := zngnative.CoerceNativeToUint(args[1])
+	if !ok {
+		return err("Math.mod", ErrBadArgument)
+	}
+
+	switch args[0].Type.ID() {
+	case zng.IdInt16, zng.IdInt32, zng.IdInt64:
+		x := args[0].Value.(int64)
+		return zngnative.Value{zng.TypeInt64, x % int64(y)}, nil
+
+	case zng.IdByte, zng.IdUint16, zng.IdUint32, zng.IdUint64:
+		x := args[0].Value.(uint64)
+		return zngnative.Value{zng.TypeInt64, x % y}, nil
+
+	default:
+		return err("Math.mod", ErrBadArgument)
+	}
+}
+
+func mathRound(args []zngnative.Value) (zngnative.Value, error) {
+	switch args[0].Type.ID() {
+	case zng.IdInt16, zng.IdInt32, zng.IdInt64, zng.IdByte, zng.IdUint16, zng.IdUint32, zng.IdUint64:
+		return args[0], nil
+
+	case zng.IdFloat64:
+		return zngnative.Value{zng.TypeFloat64, math.Round(args[0].Value.(float64))}, nil
+
+	default:
+		return err("Math.round", ErrBadArgument)
+	}
+}
+
+func mathPow(args []zngnative.Value) (zngnative.Value, error) {
+	x, ok := zngnative.CoerceNativeToFloat64(args[0])
+	if !ok {
+		return err("Math.pow", ErrBadArgument)
+	}
+	y, ok := zngnative.CoerceNativeToFloat64(args[1])
+	if !ok {
+		return err("Math.pow", ErrBadArgument)
+	}
+	r := math.Pow(x, y)
+	if math.IsNaN(r) {
+		return err("Math.pow", ErrBadArgument)
+	}
+	return zngnative.Value{zng.TypeFloat64, r}, nil
 }
 
 func mathSqrt(args []zngnative.Value) (zngnative.Value, error) {
@@ -109,7 +226,7 @@ func mathSqrt(args []zngnative.Value) (zngnative.Value, error) {
 	case zng.IdByte, zng.IdUint16, zng.IdUint32, zng.IdUint64:
 		x = float64(args[0].Value.(uint64))
 	default:
-		return zngnative.Value{}, fmt.Errorf("Math.sqrt: %w", ErrBadArgument)
+		return err("Math.sqrt", ErrBadArgument)
 	}
 
 	r := math.Sqrt(x)
@@ -117,7 +234,7 @@ func mathSqrt(args []zngnative.Value) (zngnative.Value, error) {
 		// For now we can't represent non-numeric values in a float64,
 		// we will revisit this but it has implications for file
 		// formats, zql, etc.
-		return zngnative.Value{}, fmt.Errorf("Math.sqrt: %w", ErrBadArgument)
+		return err("Math.sqrt", ErrBadArgument)
 	}
 
 	return zngnative.Value{zng.TypeFloat64, r}, nil
