5ab6f9eef754a3435718aa993af8261289c89b47
Henri DF <henridf@gmail.com>
2020-03-24 11:53:11 +0100

    Wire up _path inference for use with json types

--- cmd/zq/zq.go
+++ cmd/zq/zq.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 
 	"github.com/brimsec/zq/ast"
 	"github.com/brimsec/zq/driver"
@@ -27,9 +28,10 @@ import (
 var version = "unknown"
 
 var Zq = &charm.Spec{
-	Name:  "zq",
-	Usage: "zq [ options ] [ zql ] file [ file ... ]",
-	Short: "command line logs processor",
+	Name:        "zq",
+	Usage:       "zq [ options ] [ zql ] file [ file ... ]",
+	Short:       "command line logs processor",
+	HiddenFlags: "pathregexp",
 	Long: `
 zq is a command-line tool for processing logs.  It applies boolean logic
 to filter each log value, optionally computes analytics and transformations,
@@ -75,29 +77,38 @@ func init() {
 }
 
 type Command struct {
-	zctx         *resolver.Context
-	ifmt         string
-	ofmt         string
-	dir          string
-	path         string
-	jsonTypePath string
-	outputFile   string
-	verbose      bool
-	stats        bool
-	quiet        bool
-	showVersion  bool
+	zctx           *resolver.Context
+	ifmt           string
+	ofmt           string
+	dir            string
+	path           string
+	jsonTypePath   string
+	jsonInferPath  bool
+	jsonPathRegexp string
+	outputFile     string
+	verbose        bool
+	stats          bool
+	quiet          bool
+	showVersion    bool
 	zio.Flags
 }
 
 func New(f *flag.FlagSet) (charm.Command, error) {
 	cwd, _ := os.Getwd()
 	c := &Command{zctx: resolver.NewContext()}
+
+	// x509.14:00:00-15:00:00.log.gz (open source zeek)
+	// x509_20191101_14:00:00-15:00:00+0000.log.gz (corelight)
+	c.jsonPathRegexp = `([a-zA-Z0-9_]+)(?:\.|_\d{8}_)\d\d:\d\d:\d\d\-\d\d:\d\d:\d\d(?:[+\-]\d{4})?\.log(?:$|\.gz)`
+
 	f.StringVar(&c.ifmt, "i", "auto", "format of input data [auto,bzng,ndjson,zeek,zjson,zng]")
 	f.StringVar(&c.ofmt, "f", "zng", "format for output data [bzng,ndjson,table,text,types,zeek,zjson,zng]")
 	f.StringVar(&c.path, "p", cwd, "path for input")
 	f.StringVar(&c.dir, "d", "", "directory for output data files")
 	f.StringVar(&c.outputFile, "o", "", "write data to output file")
 	f.StringVar(&c.jsonTypePath, "j", "", "path to json types file")
+	f.BoolVar(&c.jsonInferPath, "inferpath", false, "infer zeek _path from file name (for ndjson objects without _path)")
+	f.StringVar(&c.jsonPathRegexp, "pathregexp", c.jsonPathRegexp, "regexp for extracting _path from json log name (when -inferpath=true)")
 	f.BoolVar(&c.verbose, "v", false, "show verbose details")
 	f.BoolVar(&c.stats, "S", false, "display search stats on stderr")
 	f.BoolVar(&c.quiet, "q", false, "don't display zql warnings")
@@ -188,6 +199,9 @@ func (c *Command) Run(args []string) error {
 	if len(args) == 0 {
 		return Zq.Exec(c, []string{"help"})
 	}
+	if _, err := regexp.Compile(c.jsonPathRegexp); err != nil {
+		return err
+	}
 	paths := args
 	var query ast.Proc
 	var err error
@@ -225,9 +239,18 @@ func (c *Command) Run(args []string) error {
 		if err != nil {
 			return err
 		}
-		for _, r := range readers {
+		for i, r := range readers {
 			if ndjr, ok := r.(*ndjsonio.Reader); ok {
-				if err := ndjr.SetTypeConfig(*tc); err != nil {
+				var path string
+				if c.jsonInferPath {
+					re := regexp.MustCompile(c.jsonPathRegexp)
+					match := re.FindStringSubmatch(paths[i])
+					if len(match) != 2 {
+						return fmt.Errorf("could not infer path from filename %s", paths[i])
+					}
+					path = match[1]
+				}
+				if err := ndjr.ConfigureTypes(*tc, path); err != nil {
 					return err
 				}
 			}
