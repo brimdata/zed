c6149c3d18fb845fed85d0a5e81b95c84e66caee
Henri DF <henridf@gmail.com>
2020-03-26 16:29:08 +0100

    Add support for checking stderr output in test.Shell (#464)
    * Clean up failure handling in Script system tests
    
    * test.Shell: Add support for checking stderr output

--- tests/system_test.go
+++ tests/system_test.go
@@ -5,6 +5,7 @@ package tests
 import (
 	"errors"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"testing"
 
@@ -52,24 +53,18 @@ func TestScripts(t *testing.T) {
 	require.NoError(t, err)
 	for _, script := range scripts {
 		t.Run(script.Name, func(t *testing.T) {
-			var fail bool
 			shell := test.NewShellTest(script)
-			_, _, err := shell.Run(RootDir, path)
-			if err != nil {
-				fail = true
+			_, stderr, err := shell.Run(RootDir, path)
+			assert.NoError(t, err)
+			if script.ExpectedStderrRE != "" {
+				assert.Regexp(t, regexp.MustCompile(script.ExpectedStderrRE), stderr)
 			}
-			require.NoError(t, err)
 			for _, file := range script.Expected {
 				actual, err := shell.Read(file.Name)
-				if err != nil {
-					fail = true
-				}
-				require.NoError(t, err)
-				if !assert.Exactly(t, file.Data, actual, "Wrong shell script results") {
-					fail = true
-				}
+				assert.NoError(t, err)
+				assert.Exactly(t, file.Data, actual, "Wrong shell script results")
 			}
-			if !fail {
+			if !t.Failed() {
 				// Remove the testdir on success.  If test fails,  we
 				// leave it behind in testroot for debugging.  These
 				// failed test directories have to be manually removed.
