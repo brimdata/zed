stevesmoot/zq

make space deletion cancel and await ingest stop
When a space delete API call is made, signal to active ingests to
cancel, and wait for them to do so, preventing any further ingest
calls until the deletion is done.

This uses zqd.Core as the holder of the necessary state. Eventually, I
think this would better live in a dedicated space manager, but that's a
much larger change, and I think this code will move there easily.

This is a part of addressing ingest cancellation during applicaion
shutdown on Windows, as we currently see "file in use" errors there when
a delete is called concurrently with ingest.

I'm putting this up now for comments, and will add unit tests in a later
commit in this branch.

zqd/handlers.go
@@ -211,6 +211,9 @@ func handleSpaceDelete(c *Core, w http.ResponseWriter, r *http.Request) {
 	if s == nil {
 		return
 	}
+	deleteDone := c.startSpaceDelete(s.Name())
+	defer deleteDone()
+
 	if err := s.Delete(); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -224,16 +227,26 @@ func handlePacketPost(c *Core, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	logger := c.requestLogger(r)
+
 	s := extractSpace(c, w, r)
 	if s == nil {
 		return
 	}
+
+	ctx, ingestDone, ok := c.startSpaceIngest(r.Context(), s.Name())
+	if !ok {
+		http.Error(w, "space is awaiting deletion", http.StatusConflict)
+		return
+	}
+	defer ingestDone()
+
 	var req api.PacketPostRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	proc, err := packet.IngestFile(r.Context(), s, req.Path, c.ZeekLauncher, c.SortLimit)
+
+	proc, err := packet.IngestFile(ctx, s, req.Path, c.ZeekLauncher, c.SortLimit)
 	if err != nil {
 		if errors.Is(err, pcapio.ErrCorruptPcap) {
 			http.Error(w, err.Error(), http.StatusBadRequest)

brimdata/zed

zqd/handlers.go
@@ -220,6 +220,9 @@ func handleSpaceDelete(c *Core, w http.ResponseWriter, r *http.Request) {
 	if s == nil {
 		return
 	}
+	deleteDone := c.startSpaceDelete(s.Name())
+	defer deleteDone()
+
 	if err := s.Delete(); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -233,16 +236,26 @@ func handlePacketPost(c *Core, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	logger := c.requestLogger(r)
+
 	s := extractSpace(c, w, r)
 	if s == nil {
 		return
 	}
+
+	ctx, ingestDone, ok := c.startSpaceIngest(r.Context(), s.Name())
+	if !ok {
+		http.Error(w, "space is awaiting deletion", http.StatusConflict)
+		return
+	}
+	defer ingestDone()
+
 	var req api.PacketPostRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	proc, err := ingest.Pcap(r.Context(), s, req.Path, c.ZeekLauncher, c.SortLimit)
+
+	proc, err := ingest.Pcap(ctx, s, req.Path, c.ZeekLauncher, c.SortLimit)
 	if err != nil {
 		if errors.Is(err, pcapio.ErrCorruptPcap) {
 			http.Error(w, err.Error(), http.StatusBadRequest)