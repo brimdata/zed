bda484b5704fd75d93fa16e6d4d571a839b78caa
Matthew Nibecker <hello@mattnibecker.com>
2020-04-03 12:09:13 -0700

    Graceful http shutdown (#500)
    Add pkg httpd. httpd.Server is an http Server that provides user-friendly
    graceful http shutdown. The Server instance's Run and Start method both take
    a context that will be used as the base context for all requests issued
    initiated by the server. If the passed context is cancelled,  the Server
    closes the active listener and waits for all active connections to become
    idle. Because the passed context is used base context for all requests,
    when the context is cancelled all active requests in turn receive a
    cancellation signal. If the wait for active connections to idle exceeds
    the duration set in httpd.ShutdownTimeout the Server forcefully closes all
    active connections.
    
    It is up to the implmentor of each handler route to determine whether to
    listen to a request's ctx.Done() signal. It is in this pr author's opinion
    that short-lived requests (e.g. space info, space list, status, etc) should
    ignore ctx.Done() and all long running requests (e.g. searches and data posts
    to spaces) should start to exit on ctx.Done(). This will the effect that during
    graceful shutdown short-lived requests will be allowed to finish to completion,
    while long running requests will terminate and prevent a forced shutdown.
    
    Additional changes:
    - A pkg catcher: a user friendly interface for catching signals in
    a user friend manner.
    - Have zqd listen utilize pkg catcher and httpd to trigger a graceful
    shutdown when an interrupt signal is received.

---
+++ pkg/httpd/server.go
@@ -0,0 +1,95 @@
+package httpd
+
+import (
+	"context"
+	"errors"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var ShutdownTimeout = time.Second * 5
+
+type Server struct {
+	addr   string
+	lnAddr string
+	logger *zap.Logger
+	srv    *http.Server
+	done   sync.WaitGroup
+	err    error
+}
+
+func New(addr string, h http.Handler) *Server {
+	return &Server{addr: addr, srv: &http.Server{Handler: h}, logger: zap.NewNop()}
+}
+
+func (s *Server) SetLogger(l *zap.Logger) {
+	s.logger = l
+}
+
+func (s *Server) Addr() string {
+	return s.lnAddr
+}
+
+func (s *Server) Start(ctx context.Context) error {
+	s.done.Add(1)
+	s.srv.BaseContext = func(l net.Listener) context.Context { return ctx }
+	ln, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		return err
+	}
+	s.lnAddr = ln.Addr().String()
+	s.logger.Info("Listening", zap.String("addr", s.lnAddr))
+	go s.serve(ctx, ln)
+	return nil
+}
+
+func (s *Server) Run(ctx context.Context) error {
+	if err := s.Start(ctx); err != nil {
+		return err
+	}
+	return s.Wait()
+}
+
+func (s *Server) Wait() error {
+	s.done.Wait()
+	return s.err
+}
+
+func (s *Server) serve(ctx context.Context, ln net.Listener) {
+	defer s.done.Done()
+	errCh := make(chan error)
+	go func() {
+		if err := s.srv.Serve(ln); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errCh <- err
+		}
+		close(errCh)
+	}()
+	// Wait for either the context to cancel or for the server to exit.
+	var reason string
+	var srvErr error
+	select {
+	case srvErr = <-errCh:
+		reason = "server error"
+	case <-ctx.Done():
+		reason = "context closed"
+	}
+
+	s.logger.Info("Shutting down", zap.String("reason", reason), zap.Error(srvErr))
+	timeout, cancel := context.WithTimeout(context.Background(), ShutdownTimeout)
+	defer cancel()
+	err := s.srv.Shutdown(timeout)
+	if errors.Is(err, http.ErrServerClosed) {
+		err = srvErr
+	} else if errors.Is(err, context.DeadlineExceeded) {
+		s.logger.Warn("Server shutdown deadline exceeded", zap.Duration("duration", ShutdownTimeout))
+		if clsErr := s.srv.Close(); clsErr != nil {
+			err = clsErr
+		}
+	}
+	s.logger.Info("Closed", zap.Error(err))
+	s.err = err
+}
