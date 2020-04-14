a5f8fce0205e9d1f0b71163d048241daf52231b2
Steven McCanne <steve@mccanne.com>
2020-04-13 19:38:59 -0700

    add zbuf.Counter to wrap zbuf.Reader and count zng.Records
    This code came from the port of sst to zng. It is not currently
    used anywhere in the zq repo but is a useful, general functionality
    for the framework.

---
+++ zbuf/counter_test.go
@@ -0,0 +1,46 @@
+package zbuf_test
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/brimsec/zq/zbuf"
+	"github.com/brimsec/zq/zng"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type Sink struct{}
+
+func (n *Sink) Write(rec *zng.Record) error {
+	return nil
+}
+
+func (n *Sink) Flush() error {
+	return nil
+}
+
+func TestCounter(t *testing.T) {
+	var count int64
+	var wg sync.WaitGroup
+	var sink Sink
+	wg.Add(2)
+	go func() {
+		for i := 0; i < 22; i++ {
+			stream := newTextReader(input)
+			counter := zbuf.NewCounter(stream, &count)
+			require.NoError(t, zbuf.Copy(&sink, counter))
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 17; i++ {
+			stream := newTextReader(input)
+			counter := zbuf.NewCounter(stream, &count)
+			require.NoError(t, zbuf.Copy(&sink, counter))
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	assert.Equal(t, int64((22+17)*6), count)
+}
