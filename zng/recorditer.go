71ff54324e9a7c872a703eab65ef18faf68aeb71
Andrew Swan <aswan@brimsecurity.com>
2020-04-20 12:04:42 -0500

    Add zng.Record iterator
    This iterator iterates over all the fields inside a record, including
    fields nested inside other records.

---
+++ zng/recorditer.go
@@ -0,0 +1,104 @@
+package zng
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/brimsec/zq/zcode"
+)
+
+var (
+	ErrExhausted = errors.New("called Next() on iterator after last record")
+	ErrMismatch  = errors.New("mismatch between record type and value")
+)
+
+type RecordIter interface {
+	Done() bool
+	Next() (name string, value Value, err error)
+}
+
+type iterInfo struct {
+	iter     zcode.Iter
+	typ      *TypeRecord
+	offset   int
+	fullname string
+}
+
+type recordIter struct {
+	stack []iterInfo
+}
+
+func (r *Record) NewFieldIterator() RecordIter {
+	return &recordIter{
+		stack: []iterInfo{iterInfo{
+			iter:     r.ZvalIter(),
+			typ:      r.Type,
+			offset:   0,
+			fullname: "",
+		}},
+	}
+}
+
+func (r *recordIter) Done() bool {
+	return len(r.stack) == 0
+}
+
+func (r *recordIter) Next() (name string, value Value, err error) {
+	if len(r.stack) == 0 {
+		return "", Value{}, ErrExhausted
+	}
+	info := &r.stack[len(r.stack)-1]
+
+	zv, container, err := info.iter.Next()
+	if err != nil {
+		return "", Value{}, err
+	}
+
+	col := info.typ.Columns[info.offset]
+	name = col.Name
+	if len(info.fullname) > 0 {
+		name = fmt.Sprintf("%s.%s", info.fullname, col.Name)
+	}
+
+	// step into records as necessary
+	for {
+		recType, isRecord := AliasedType(col.Type).(*TypeRecord)
+		if !isRecord {
+			break
+		}
+
+		if !container {
+			return "", Value{}, ErrMismatch
+		}
+
+		r.stack = append(r.stack, iterInfo{zcode.Iter(zv), recType, 0, name})
+		info = &r.stack[len(r.stack)-1]
+
+		zv, container, err = info.iter.Next()
+		if err != nil {
+			return "", Value{}, err
+		}
+
+		col = recType.Columns[0]
+		name = fmt.Sprintf("%s.%s", name, col.Name)
+	}
+
+	// we're at a leaf value, assemble it
+	val := Value{col.Type, zv}
+
+	// and advance our position, stepping out of records as needed.
+	info.offset++
+	for info.offset >= len(info.typ.Columns) {
+		if !info.iter.Done() {
+			return "", Value{}, ErrMismatch
+		}
+		r.stack = r.stack[:len(r.stack)-1]
+		if len(r.stack) == 0 {
+			break
+		}
+		info = &r.stack[len(r.stack)-1]
+		info.offset++
+	}
+
+	return name, val, nil
+}
