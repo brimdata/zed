bf33758d58596e99efb842ca4f714463574df395
Henri DF <henridf@gmail.com>
2020-04-21 16:34:41 +0200

    populate ErrorType

--- zqd/handlers.go
+++ zqd/handlers.go
@@ -41,7 +41,7 @@ func errorResponse(e error) (status int, ae *api.Error) {
 		status = http.StatusConflict
 	}
 
-	ae.Type = ze.Kind.String()
+	ae.ErrorType = ze.Kind.String()
 	ae.Message = ze.Message()
 	return
 }
