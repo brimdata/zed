foxhack/zq

adapt zdx to use zng instead of ad hoc sorted tables
This commit adapts zar and zdx to use zng files as the underlying
representation of sorted index keys.  Now that we have stream
boundaries within zng files, we can use these boundaries to create
hierarchical indexes.

This commit also generalizes the zar command funcationality to handle
arbitrary indexing patterns of types or fields.  Once the zng indexing
machinery was in place, this change was relatively straightforward.

cmd/zar/find/command.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"net"
+	"strings"
 
 	"github.com/brimsec/zq/archive"
 	"github.com/brimsec/zq/cmd/zar/root"
@@ -13,13 +13,22 @@ import (
 
 var Find = &charm.Spec{
 	Name:  "find",
-	Usage: "find [-d dir] <ip>",
+	Usage: "find [-d <dir>] <pattern>",
 	Short: "look through zar index files and displays matches",
 	Long: `
-"zar find" descends the directory given by the argument looking for bzng files that have
-a corresponding zar index and if that index contains the <ip> argument,
-then the path of the zng file is printed.
-The current version supports only IP address, but this will soon change.
+"zar find" descends the directory given by the -d option looking for bzng files
+that have a corresponding zar index conforming to the indicated <pattern>.
+The <pattern> argument has the form "field=value" (for field searches)
+or ":type=value" (for type searches).  For example, if type "ip" has been
+indexed then the IP 10.0.1.2 can be searched by saying
+
+	zar find -d /path/to/logs :ip=10.0.1.2
+
+Or if the field "uri" has been indexed, you might say
+
+	zar find -d /path/to/logs uri=/x/y/z
+
+The path of each bnzng file that matches the pattern is printed.
 `,
 	New: New,
 }
@@ -41,25 +50,23 @@ func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 
 func (c *Command) Run(args []string) error {
 	if len(args) != 1 {
-		return errors.New("zar find: no search pattern provided")
+		return errors.New("zar find: exactly one search pattern must be provided")
 	}
-	//XXX presume ip address
-	pattern := net.ParseIP(args[0])
-	if pattern == nil {
-		//XXX
-		return errors.New("zar find: invalid IP address: " + args[0])
+	v := strings.Split(args[0], "=")
+	if len(v) != 2 {
+		return errors.New("zar find: syntax error: " + args[0])
 	}
-	// Convert IP to 4-byte version as this is how IP keys are stored
-	ip := pattern.To4()
-	if ip != nil {
-		pattern = ip
+	fieldOrType := v[0]
+	pattern := v[1]
+	rule, err := archive.NewRule(fieldOrType)
+	if err != nil {
+		return errors.New("zar find: error parsing pattern: " + err.Error())
 	}
-	hits, err := archive.Find(c.dir, pattern)
+	hits, err := archive.Find(c.dir, rule, pattern)
 	if err != nil {
 		return err
 	}
-	//XX should stream hits as they are found instead of collecting them
-	// all up them dumping the slice to stdout
+	//XX we should stream the hits here instead of collecting them all up
 	for _, hit := range hits {
 		fmt.Println(hit)
 	}

cmd/zar/index/command.go
@@ -11,15 +11,23 @@ import (
 
 var Index = &charm.Spec{
 	Name:  "index",
-	Usage: "index dir",
-	Short: "creates index files for bzng files",
+	Usage: "index -d dir pattern [ pattern ...]",
+	Short: "create zdx index bundles for bzng files",
 	Long: `
-zar find descends the directory argument looking for bzng files and creates an index
-file for IP addresses for each bzng file encountered.  An index is written to
-a sub-directory of the directory containing each encountered bzng file, where the
-name of the sub-directory is a concatenation of the bzng file name and the suffix
-".zar".
-The current version supports only IP address, but this will soon change.
+zar index descends the directory argument starting at dir and looks
+for bzng files.  Each bzng file fund is indexed according to the one or
+more indexing rules provided.
+
+A pattern is either a field name or a ":" followed by a zng type name.
+For example, to index the all fields of type ports and the field id.orig_h,
+you would run
+
+	zar index -d /path/to/logs id.orig_h :port
+
+Each pattern results a separate zdx bundle for each bzng file found.  The zdx
+bundles foor a given bzng file are written to a sub-directory of the directory
+containing that file, where the name of the sub-directory is a concatenation
+of the bzng file name and the suffix ".zar".
 `,
 	New: New,
 }
@@ -30,17 +38,29 @@ func init() {
 
 type Command struct {
 	*root.Command
+	dir string
 }
 
 func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 	c := &Command{Command: parent.(*root.Command)}
+	f.StringVar(&c.dir, "d", "", "directory to descend")
 	return c, nil
 }
 
 func (c *Command) Run(args []string) error {
-	if len(args) != 1 {
-		return errors.New("zar index: exactly one directory must be specified")
+	if len(args) == 0 {
+		return errors.New("zar index: one or more indexing patterns must be specified")
 	}
-	dir := args[0]
-	return archive.CreateIndexes(dir)
+	if c.dir == "" {
+		return errors.New("zar index: a directory must be specified with -d")
+	}
+	var rules []archive.Rule
+	for _, pattern := range args {
+		rule, err := archive.NewRule(pattern)
+		if err != nil {
+			return errors.New("zar index: " + err.Error())
+		}
+		rules = append(rules, rule)
+	}
+	return archive.IndexDirTree(c.dir, rules)
 }

brimdata/zed

cmd/zar/find/command.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"net"
+	"strings"
 
 	"github.com/brimsec/zq/archive"
 	"github.com/brimsec/zq/cmd/zar/root"
@@ -13,13 +13,22 @@ import (
 
 var Find = &charm.Spec{
 	Name:  "find",
-	Usage: "find [-d dir] <ip>",
+	Usage: "find [-d <dir>] <pattern>",
 	Short: "look through zar index files and displays matches",
 	Long: `
-"zar find" descends the directory given by the argument looking for zng files that have
-a corresponding zar index and if that index contains the <ip> argument,
-then the path of the zng file is printed.
-The current version supports only IP address, but this will soon change.
+"zar find" descends the directory given by the -d option looking for zng files
+that have a corresponding zar index conforming to the indicated <pattern>.
+The <pattern> argument has the form "field=value" (for field searches)
+or ":type=value" (for type searches).  For example, if type "ip" has been
+indexed then the IP 10.0.1.2 can be searched by saying
+
+	zar find -d /path/to/logs :ip=10.0.1.2
+
+Or if the field "uri" has been indexed, you might say
+
+	zar find -d /path/to/logs uri=/x/y/z
+
+The path of each zng file that matches the pattern is printed.
 `,
 	New: New,
 }
@@ -41,25 +50,23 @@ func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 
 func (c *Command) Run(args []string) error {
 	if len(args) != 1 {
-		return errors.New("zar find: no search pattern provided")
+		return errors.New("zar find: exactly one search pattern must be provided")
 	}
-	//XXX presume ip address
-	pattern := net.ParseIP(args[0])
-	if pattern == nil {
-		//XXX
-		return errors.New("zar find: invalid IP address: " + args[0])
+	v := strings.Split(args[0], "=")
+	if len(v) != 2 {
+		return errors.New("zar find: syntax error: " + args[0])
 	}
-	// Convert IP to 4-byte version as this is how IP keys are stored
-	ip := pattern.To4()
-	if ip != nil {
-		pattern = ip
+	fieldOrType := v[0]
+	pattern := v[1]
+	rule, err := archive.NewRule(fieldOrType)
+	if err != nil {
+		return errors.New("zar find: error parsing pattern: " + err.Error())
 	}
-	hits, err := archive.Find(c.dir, pattern)
+	hits, err := archive.Find(c.dir, rule, pattern)
 	if err != nil {
 		return err
 	}
-	//XX should stream hits as they are found instead of collecting them
-	// all up them dumping the slice to stdout
+	//XX we should stream the hits here instead of collecting them all up
 	for _, hit := range hits {
 		fmt.Println(hit)
 	}

cmd/zar/index/command.go
@@ -11,15 +11,23 @@ import (
 
 var Index = &charm.Spec{
 	Name:  "index",
-	Usage: "index dir",
-	Short: "creates index files for zng files",
+	Usage: "index -d dir pattern [ pattern ...]",
+	Short: "create index files for zng files",
 	Long: `
-zar find descends the directory argument looking for zng files and creates an index
-file for IP addresses for each zng file encountered.  An index is written to
-a sub-directory of the directory containing each encountered zng file, where the
-name of the sub-directory is a concatenation of the zng file name and the suffix
-".zar".
-The current version supports only IP address, but this will soon change.
+zar index descends the directory argument starting at dir and looks
+for zng files.  Each zng file fund is indexed according to the one or
+more indexing rules provided.
+
+A pattern is either a field name or a ":" followed by a zng type name.
+For example, to index the all fields of type ports and the field id.orig_h,
+you would run
+
+	zar index -d /path/to/logs id.orig_h :port
+
+Each pattern results a separate zdx index file for each zng file found.  The zdx
+files for a given zng file are written to a sub-directory of the directory
+containing that file, where the name of the sub-directory is a concatenation
+of the zng file name and the suffix ".zar".
 `,
 	New: New,
 }
@@ -30,17 +38,29 @@ func init() {
 
 type Command struct {
 	*root.Command
+	dir string
 }
 
 func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 	c := &Command{Command: parent.(*root.Command)}
+	f.StringVar(&c.dir, "d", "", "directory to descend")
 	return c, nil
 }
 
 func (c *Command) Run(args []string) error {
-	if len(args) != 1 {
-		return errors.New("zar index: exactly one directory must be specified")
+	if len(args) == 0 {
+		return errors.New("zar index: one or more indexing patterns must be specified")
 	}
-	dir := args[0]
-	return archive.CreateIndexes(dir)
+	if c.dir == "" {
+		return errors.New("zar index: a directory must be specified with -d")
+	}
+	var rules []archive.Rule
+	for _, pattern := range args {
+		rule, err := archive.NewRule(pattern)
+		if err != nil {
+			return errors.New("zar index: " + err.Error())
+		}
+		rules = append(rules, rule)
+	}
+	return archive.IndexDirTree(c.dir, rules)
 }