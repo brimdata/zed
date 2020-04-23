901b86543a8cd9a4e535e2bf83d5be89e0783ce3
Steven McCanne <steve@mccanne.com>
2020-04-22 17:09:04 -0700

    fix bug where files for writing should be truncated on open
    This commit fixes a bug where the zng.Writer output files
    were open without truncation.  This would cause corrupt
    files to be created when overlaying new but shorter output
    on an existing file.

--- emitter/file.go
+++ emitter/file.go
@@ -25,7 +25,7 @@ func NewFile(path string, flags *zio.WriterFlags) (*zio.Writer, error) {
 		f = &noClose{os.Stdout}
 	} else {
 		var err error
-		flags := os.O_WRONLY | os.O_CREATE
+		flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
 		file, err := os.OpenFile(path, flags, 0600)
 		if err != nil {
 			return nil, err
