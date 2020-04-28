7188eae796163108e78effd58e8ed00e14038ec3
Steve McCanne <steve@mccanne.com>
2020-04-28 16:38:58 -0700

    Make zar ls better (#693)

--- cmd/zar/ls/command.go
+++ cmd/zar/ls/command.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/brimsec/zq/archive"
 	"github.com/brimsec/zq/cmd/zar/root"
@@ -29,25 +32,71 @@ func init() {
 
 type Command struct {
 	*root.Command
+	root  string
+	lflag bool
 }
 
 func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 	c := &Command{Command: parent.(*root.Command)}
+	f.StringVar(&c.root, "R", os.Getenv("ZAR_ROOT"), "root directory of zar archive to walk")
+	f.BoolVar(&c.lflag, "l", false, "long form")
 	return c, nil
 }
 
 func (c *Command) Run(args []string) error {
-	if len(args) == 0 {
-		return errors.New("zar ls: no directory specified")
+	if c.root == "" {
+		return errors.New("zar ls: no root specified with -R or ZAR_ROOT")
 	}
-	for _, dir := range args {
-		err := archive.Walk(dir, func(zardir string) error {
-			fmt.Println(zardir)
-			return nil
-		})
-		if err != nil {
-			return err
+	if len(args) > 1 {
+		return errors.New("zar ls: too many arguments")
+	}
+	var pattern string
+	if len(args) == 1 {
+		pattern = args[0]
+	}
+	return archive.Walk(c.root, func(zardir string) error {
+		printDir(zardir, pattern, c.lflag)
+		return nil
+	})
+}
+
+func fileExists(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func printDir(dir, pattern string, lflag bool) {
+	if pattern != "" {
+		path := filepath.Join(dir, pattern)
+		if fileExists(path) {
+			fmt.Println(path)
+		}
+		return
+	}
+	fmt.Println(dir)
+	if lflag {
+		files := ls(dir)
+		for _, f := range files {
+			fmt.Printf("\t%s\n", f)
 		}
 	}
-	return nil
+}
+
+func ls(dir string) []string {
+	var out []string
+	infos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil
+	}
+	for _, info := range infos {
+		name := info.Name()
+		if info.IsDir() {
+			name += "/"
+		}
+		out = append(out, name)
+	}
+	return out
 }
