3a7ec106507fd83314f7d3a54b16dfb5cd891da9
Henri DF <henridf@gmail.com>
2020-05-05 18:07:46 +0200

    zngio: don't swallow errors when reading

--- zio/zngio/reader.go
+++ zio/zngio/reader.go
@@ -95,8 +95,11 @@ func (r *Reader) LastSOS() int64 {
 func (r *Reader) ReadPayload() (*zng.Record, []byte, error) {
 again:
 	b, err := r.read(1)
-	if err == io.EOF || len(b) == 0 {
+	if err == io.EOF || err == peeker.ErrTruncated {
 		return nil, nil, nil
+	}
+	if err != nil {
+		return nil, nil, err
 	}
 	code := b[0]
 	if code&0x80 != 0 {
