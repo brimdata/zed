c5ca1b9a1c11f46fa18f7f52bcb71632ec7e10ad
Steven McCanne <steve@mccanne.com>
2020-05-14 14:12:02 -0700

    This commit reworks things so microindexes are now a single file.

---
+++ cmd/zdx/section/command.go
@@ -0,0 +1,106 @@
+package section
+
+import (
+	"errors"
+	"flag"
+	"os"
+
+	"github.com/brimsec/zq/cmd/zdx/root"
+	"github.com/brimsec/zq/emitter"
+	"github.com/brimsec/zq/zbuf"
+	"github.com/brimsec/zq/zdx"
+	"github.com/brimsec/zq/zio"
+	"github.com/brimsec/zq/zng/resolver"
+	"github.com/mccanne/charm"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+var Section = &charm.Spec{
+	Name:  "section",
+	Usage: "section [flags] path",
+	Short: "extract a section of a zdx file",
+	Long: `
+The section command extracts a section from a microindex file and
+writes it to the output.  The -trailer option writes
+the microindex trailer to the output in addition to the section if the section
+number was specified.
+
+See the zdx command help for a description of a microindex.`,
+	New: newCommand,
+}
+
+func init() {
+	root.Zdx.Add(Section)
+}
+
+type Command struct {
+	*root.Command
+	outputFile   string
+	WriterFlags  zio.WriterFlags
+	textShortcut bool
+	forceBinary  bool
+	trailer      bool
+	section      int
+}
+
+func newCommand(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
+	c := &Command{Command: parent.(*root.Command)}
+	f.BoolVar(&c.textShortcut, "t", false, "use format tzng independent of -f option")
+	f.BoolVar(&c.forceBinary, "B", false, "allow binary zng be sent to a terminal output")
+	f.BoolVar(&c.trailer, "trailer", false, "include the micro-index trailer in the output")
+	f.IntVar(&c.section, "s", -1, "include the indicated section in the output")
+	c.WriterFlags.SetFlags(f)
+	return c, nil
+}
+
+func isTerminal(f *os.File) bool {
+	return terminal.IsTerminal(int(f.Fd()))
+}
+
+func (c *Command) Run(args []string) error {
+	if len(args) != 1 {
+		return errors.New("zdx section: must be run with a single path argument")
+	}
+	if c.textShortcut {
+		c.WriterFlags.Format = "tzng"
+	}
+	if c.WriterFlags.Format == "zng" && isTerminal(os.Stdout) && !c.forceBinary {
+		return errors.New("zq: writing binary zng data to terminal; override with -B or use -t for text.")
+	}
+	path := args[0]
+	reader, err := zdx.NewReader(resolver.NewContext(), path)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+	writer, err := emitter.NewFile(c.outputFile, &c.WriterFlags)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if writer != nil {
+			writer.Close()
+		}
+	}()
+	if c.section >= 0 {
+		r, err := reader.NewSectionReader(c.section)
+		if err != nil {
+			return err
+		}
+		if err := zbuf.Copy(writer, r); err != nil {
+			return err
+		}
+	}
+	if c.trailer {
+		r, err := reader.NewTrailerReader()
+		if err != nil {
+			return err
+		}
+		if err := zbuf.Copy(writer, r); err != nil {
+			return err
+		}
+	}
+	err = writer.Close()
+	writer = nil
+	return err
+}
--- zdx/zdx.go
+++ zdx/zdx.go
@@ -1,23 +1,8 @@
-// Package zdx provides an API for creating, merging, indexing, and querying zng
-// index files, a la SST in LSM trees, where a zdx "bundle" comprises a sequence of
-// key,value zng records and the records are sorted by key.
+// Package zdx provides an API for creating, merging, indexing, and querying
+// microindexes.
 //
-// A table on disk consists of the base table with zero or more b-tree files.
-// These files are all named with the same path prefix, e.g., "zdx.zng", where the
-// base table is zdx.zng and the b-tree index files, if any, are zdx.1.zng,
-// zdx.2.zng, and so forth.  The index files can always be reconstructed from
-// the base table.
-//
-// Typically when building such a table, a client starts out with the table
-// in memory.  Then it is written to storage using a Writer.  Tables can be
-// combined with a Combiner and are merged in an efficient LSM like fashion.
-//
-// The zdx base file and index files are all organized as a sequence of
-// zng streams.
-//
-// Each b-tree index file contains a key,value pair for each stream in the file
-// below in the hiearchy where the key is the first key found in that stream and
-// the value is the offset or the stream in the file below.
+// A microindex comprises a base index section followed by zero or more parent
+// section indexes.
 //
 // zdx.Reader implements zbuf.Reader and zdx.Writer implements zbuf.Writer so
 // generic zng functionality applies, e.g., a Reader can be copied to a Writer
@@ -26,20 +11,8 @@ package zdx
 
 import (
 	"errors"
-	"fmt"
-
-	"github.com/brimsec/zq/pkg/iosrc"
 )
 
 var (
 	ErrCorruptFile = errors.New("corrupt zdx file")
 )
-
-func filename(uri iosrc.URI, level int) iosrc.URI {
-	if level == 0 {
-		uri.Path += ".zng"
-	} else {
-		uri.Path += fmt.Sprintf(".%d.zng", level)
-	}
-	return uri
-}
