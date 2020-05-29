7da521041abd9c809c99b251a88b8d1194aa0ddf
Matthew Nibecker <hello@mattnibecker.com>
2020-05-29 16:31:04 -0700

    fix package colw (#838)
    Was not working at all. Fix to work like columns in the bash ls
    command.

--- pkg/colw/colw.go
+++ pkg/colw/colw.go
@@ -12,48 +12,13 @@ import (
 // width less padding.  Otherwise, you would get one column taking up all the width.
 var ErrDoesNotFit = errors.New("items do not fit")
 
-func slen(in []string, pad int) []int {
-	out := make([]int, 0, len(in))
+func colwidth(in []string, pad int) (longest int) {
 	for _, s := range in {
-		out = append(out, len(s)+pad)
-	}
-	return out
-}
-
-func build(lens []int, ncol int) []int {
-	widths := make([]int, ncol)
-	for k, v := range lens {
-		colno := k % ncol
-		if v > widths[colno] {
-			widths[colno] = v
+		if l := len(s) + pad; l > longest {
+			longest = l
 		}
 	}
-	return widths
-}
-
-func fit(trial []int, budget int) bool {
-	var n int
-	for _, v := range trial {
-		n += v
-	}
-	return n <= budget
-}
-
-// Layout takes a list of strings, a pad for each column, and a total
-// width of each row and computes the layout with the largest number of
-// columns that will fit in the given row width if the strings are displayed
-// in left to right order a row a time.
-func Layout(in []string, width, pad int) (widths []int) {
-	lens := slen(in, pad)
-	var result []int
-	for ncol := 1; ncol < width; ncol++ {
-		next := build(lens, ncol)
-		if !fit(next, width) {
-			return result
-		}
-		result = next
-	}
-	return nil
+	return
 }
 
 func makePadding(n int, padchar byte) []byte {
@@ -69,11 +34,11 @@ var newline = []byte{'\n'}
 // Write writes the list of strings to the given writer with padding and
 // newlines according to Layout
 func Write(w io.Writer, in []string, width, pad int) error {
-	widths := Layout(in, width, pad)
-	if widths == nil {
+	colw := colwidth(in, pad)
+	if colw > width {
 		return ErrDoesNotFit
 	}
-	ncol := len(widths)
+	ncol := width / colw
 	padding := makePadding(width, ' ')
 	for k, s := range in {
 		if _, err := w.Write([]byte(s)); err != nil {
@@ -89,7 +54,7 @@ func Write(w io.Writer, in []string, width, pad int) error {
 				return err
 			}
 		} else {
-			pad := widths[colno] - len(s)
+			pad := colw - len(s)
 			if pad > 0 {
 				if _, err := w.Write(padding[0:pad]); err != nil {
 					return err
