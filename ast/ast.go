16abdefcd39e2c8b4dfcdf74010bc6dad4854110
Henri DF <henridf@gmail.com>
2020-06-01 13:26:05 +0200

    Support grouping by computed keys

--- ast/ast.go
+++ ast/ast.go
@@ -176,6 +176,7 @@ func (*FunctionCall) exprNode()          {}
 func (*CastExpression) exprNode()        {}
 func (*Literal) exprNode()               {}
 func (*FieldRead) exprNode()             {}
+func (*FieldCall) exprNode()             {}
 
 // ----------------------------------------------------------------------------
 // Procs
@@ -264,10 +265,10 @@ type (
 	// be aggregated over. When absent, the runtime defaults to an appropriate value.
 	GroupByProc struct {
 		Node
-		Duration Duration  `json:"duration"`
-		Limit    int       `json:"limit,omitempty"`
-		Keys     []string  `json:"keys"`
-		Reducers []Reducer `json:"reducers"`
+		Duration Duration     `json:"duration"`
+		Limit    int          `json:"limit,omitempty"`
+		Keys     []Assignment `json:"keys"`
+		Reducers []Reducer    `json:"reducers"`
 	}
 	// TopProc is similar to proc.SortProc with a few key differences:
 	// - It only sorts in descending order.
