4604e158ac7dc42688b1f8eb7119999324aecde8
Phil Rzewski <phil@brimsecurity.com>
2020-06-10 08:53:59 -0700

    Make go fmt happy

--- tests/zq_example.go
+++ tests/zq_example.go
@@ -317,7 +317,7 @@ func DocMarkdownFiles() ([]string, error) {
 		"zql/docs/search-syntax/README.md",
 		"zql/docs/aggregate-functions/README.md",
 		"zql/docs/expressions/README.md",
-                "zql/docs/data-types/README.md",
+		"zql/docs/data-types/README.md",
 	}
 	repopath, err := RepoAbsPath()
 	if err != nil {
