49216468061784212fdc4e62a98725a94485a537
Matthew Nibecker <hello@mattnibecker.com>
2020-06-12 12:17:23 -0700

    zq: return error of writer.Close
    Because of the way s3manager is written, we may only find out about
    an s3 error until after Writer.Close is called. The zq command was
    ignoring writer.Close errors. Do not do this and add comment as to
    why we are deviating from convention.
    
    Closes #900

--- cmd/zq/zq.go
+++ cmd/zq/zq.go
@@ -249,16 +249,22 @@ func (c *Command) Run(args []string) error {
 	if err != nil {
 		return err
 	}
-	defer writer.Close()
 	mux, err := driver.CompileWarningsCh(context.Background(), query, reader, false, nano.MaxSpan, zap.NewNop(), wch)
 	if err != nil {
+		writer.Close()
 		return err
 	}
 	d := driver.NewCLI(writer)
 	if !c.quiet {
 		d.SetWarningsWriter(os.Stderr)
 	}
-	return driver.Run(mux, d, nil)
+	if err := driver.Run(mux, d, nil); err != nil {
+		return err
+	}
+	// If writer is an s3 error it could have important information as to
+	// whether the request was written successfully. Cannot do a simple defer
+	// writer.Close() here.
+	return writer.Close()
 }
 
 func (c *Command) errorf(format string, args ...interface{}) {
