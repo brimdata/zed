87f55e847a91b8c8740938c403f42525591a0e05
Alfred Landrum <alfred@leakybucket.org>
2020-06-18 07:49:16 -0700

    add zapi newsubspace command (#895)

--- cmd/zapi/cmd/new/new.go
+++ cmd/zapi/cmd/new/new.go
@@ -4,7 +4,10 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"strconv"
 
+	"github.com/alecthomas/units"
+	"github.com/brimsec/zq/archive"
 	"github.com/brimsec/zq/cmd/zapi/cmd"
 	"github.com/brimsec/zq/zqd/api"
 	"github.com/brimsec/zq/zqd/storage"
@@ -24,19 +27,35 @@ func init() {
 	cmd.CLI.Add(NewSpec)
 }
 
+// bytesValue implements flag.Value.
+type bytesValue int64
+
+func (b *bytesValue) Set(s string) error {
+	bytes, err := units.ParseStrictBytes(s)
+	*b = bytesValue(bytes)
+	return err
+}
+
+func (b bytesValue) String() string {
+	return strconv.FormatInt(int64(b), 10)
+}
+
 type Command struct {
 	*cmd.Command
 	kind     storage.Kind
 	datapath string
+	thresh   bytesValue
 }
 
 func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 	c := &Command{
 		Command: parent.(*cmd.Command),
 		kind:    storage.FileStore,
+		thresh:  bytesValue(units.Base2Bytes(archive.DefaultLogSizeThreshold)),
 	}
 	f.Var(&c.kind, "k", "kind of storage for this space")
 	f.StringVar(&c.datapath, "d", "", "specific directory for storage data")
+	f.Var(&c.thresh, "thresh", "target size of chopped files, as '10MB', '4GiB', etc.")
 	return c, nil
 }
 
@@ -47,18 +66,23 @@ func (c *Command) Run(args []string) error {
 	if len(args) != 1 {
 		return errors.New("must specify a space name")
 	}
+
 	client := c.Client()
-	name := args[0]
 	req := api.SpacePostRequest{
-		Name:     name,
+		Name:     args[0],
 		DataPath: c.datapath,
 		Storage: &storage.Config{
-			Kind: storage.Kind(c.kind),
+			Kind: c.kind,
+			Archive: &storage.ArchiveConfig{
+				CreateOptions: &storage.ArchiveCreateOptions{
+					LogSizeThreshold: (*int64)(&c.thresh),
+				},
+			},
 		},
 	}
 	if _, err := client.SpacePost(c.Context(), req); err != nil {
-		return fmt.Errorf("couldn't create new space %s: %v", name, err)
+		return fmt.Errorf("couldn't create new space %s: %v", req.Name, err)
 	}
-	fmt.Printf("%s: space created\n", name)
+	fmt.Printf("%s: space created\n", req.Name)
 	return nil
 }
---
+++ cmd/zapi/cmd/newsubspace/newsubspace.go
@@ -0,0 +1,55 @@
+package newsubspace
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	"github.com/brimsec/zq/cmd/zapi/cmd"
+	"github.com/brimsec/zq/zqd/api"
+	"github.com/brimsec/zq/zqd/storage"
+	"github.com/mccanne/charm"
+)
+
+var NewSubspace = &charm.Spec{
+	Name:  "newsubspace",
+	Usage: "newsubspace -p parent_space_id -n subspace_name log1 [log2 ...]",
+	Short: "create a new subspace",
+	Long:  `Creates a subspace of the given logs from the parent space.`,
+	New:   New,
+}
+
+func init() {
+	cmd.CLI.Add(NewSubspace)
+}
+
+type Command struct {
+	*cmd.Command
+	parentID string
+	name     string
+}
+
+func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
+	c := &Command{Command: parent.(*cmd.Command)}
+	f.StringVar(&c.parentID, "p", "", "id of parent space")
+	f.StringVar(&c.name, "n", "", "name for subspace")
+	return c, nil
+}
+
+func (c *Command) Run(args []string) error {
+	if len(args) == 0 {
+		return errors.New("must specify at least one log from parent")
+	}
+	client := c.Client()
+	req := api.SubspacePostRequest{
+		Name: c.name,
+		OpenOptions: storage.ArchiveOpenOptions{
+			LogFilter: args,
+		},
+	}
+	if _, err := client.SubspacePost(c.Context(), api.SpaceID(c.parentID), req); err != nil {
+		return fmt.Errorf("couldn't create subspace %s: %w", c.name, err)
+	}
+	fmt.Printf("%s: subspace created\n", c.name)
+	return nil
+}
