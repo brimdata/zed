56fd8465e14024b3f28b2465b5a128f426532190
Matthew Nibecker <hello@mattnibecker.com>
2020-08-13 12:09:24 -0700

    zqd listen: add -loglevel flag (#1088)
    Add loglevel flag to set the level of printed logs. If a logging
    is specified zqd config, this flag will be ignored.

--- tests/suite/zqd/services.sh
+++ tests/suite/zqd/services.sh
@@ -33,7 +33,7 @@ export AWS_ACCESS_KEY_ID=minioadmin
 export AWS_SECRET_ACCESS_KEY=minioadmin
 export AWS_S3_ENDPOINT=http://localhost:$(cat $portdir/minio)
 
-zqd listen -l=localhost:0 -portfile="$portdir/zqd" -data="$zqdroot" 2> zqd.error &
+zqd listen -l=localhost:0 -portfile="$portdir/zqd" -data="$zqdroot" -loglevel=warn &
 zqdpid=$!
 trap "rm -rf $portdir; kill -9 $miniopid $zqdpid" EXIT
 
