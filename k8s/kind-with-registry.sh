e91c9661b5e3f080c69a3e29b8038adf76e3a645
Mark Wallace <mark.t.wallace@gmail.com>
2020-08-14 18:20:22 -0700

    Update k8s/kind-with-registry.sh
    Co-authored-by: Noah Treuhaft <noah.treuhaft@gmail.com>

--- k8s/kind-with-registry.sh
+++ k8s/kind-with-registry.sh
@@ -3,9 +3,9 @@ set -x #echo on
 set -o errexit
 
 # create registry container unless it already exists
-reg_name='kind-registry'
-reg_port='5000'
-running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+reg_name=kind-registry
+reg_port=5000
+running=$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)
 if [ "${running}" != 'true' ]; then
   docker run \
     -d --restart=always -p "${reg_port}:5000" --name "${reg_name}" \
