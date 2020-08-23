0a02e80682d4787c81e9d1678d61f3aafed9bfe4
Steven McCanne <steve@mccanne.com>
2020-08-23 16:04:30 -0700

    make proposal cleaner

--- tests/ztest_test.go
+++ tests/ztest_test.go
@@ -14,19 +14,12 @@ import (
 func TestZTest(t *testing.T) {
 	t.Parallel()
 	dirs := map[string]bool{}
-	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if strings.HasSuffix(path, ".yaml") {
-			dirs[filepath.Dir(path)] = true
-		}
-		return err
-	})
-	require.NoError(t, err)
 	re, _ := regexp.Compile("/test/.*\\.yaml")
-	err = filepath.Walk("..", func(path string, info os.FileInfo, err error) error {
-		if strings.HasPrefix(path, "../tests/") || !re.MatchString(path) {
+	err := filepath.Walk("..", func(path string, info os.FileInfo, err error) error {
+		if !strings.HasSuffix(path, ".yaml") {
 			return err
 		}
-		if strings.HasSuffix(path, ".yaml") {
+		if strings.HasPrefix(path, "../tests/") || re.MatchString(path) {
 			dirs[filepath.Dir(path)] = true
 		}
 		return err
