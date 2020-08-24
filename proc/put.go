cdcf37e9dd6417ac57d60a2912090c22070d080c
Steve McCanne <steve@mccanne.com>
2020-08-24 16:14:03 -0700

    fix bug in put proc and refactor (#1158)
    The put proc had a bug where it attempted to append a clause result
    after all of the clauses had already replaced fields in the original
    input.  Put.put() was doing many different things with some confusing
    variable and type names so a refactor was warranted.
    
    This commit fixes the bug and refactors the logic into bit-sized chunks
    with hopefully more helpful names.  The resulting code is smaller.

--- proc/put.go
+++ proc/put.go
@@ -1,8 +1,6 @@
 package proc
 
 import (
-	"sort"
-
 	"github.com/brimsec/zq/ast"
 	"github.com/brimsec/zq/expr"
 	"github.com/brimsec/zq/zbuf"
@@ -10,30 +8,44 @@ import (
 	"github.com/brimsec/zq/zng"
 )
 
-// The cached information we keep for generating an output record.
-// For a given input descriptor + computed type for the put expression
-// includes the descriptor for output records (outType) plus information
-// about where (position) and how (container) to write the computed value
-// into the output record for each clause.
-type descinfo struct {
-	typ    *zng.TypeRecord
-	fields []fieldinfo
-	order  []int
-}
-
-type fieldinfo struct {
-	valType   zng.Type
-	position  int
-	container bool
-}
-
+// Put is a proc that modifies the record stream with computed values.
+// Each new value is called a clause and consists of a field name and
+// an expression.  Currently, the field name must be a top-level record
+// name, i.e., it cannot be a dotted record access or array reference
+// (XXX this will change when we add more comprehensive expression support).
+// Each put clause either replaces an existing value in the column specified
+// or appends a value as a new column.  Appended values appear as new
+// columns in the order that the clause appears in the put expression.
 type Put struct {
 	Base
 	clauses []clause
 	// vals is a fixed array to avoid re-allocating for every record
 	vals   []zng.Value
-	outmap map[int]descinfo
+	rules  map[int]*putRule
 	warned map[string]struct{}
+}
+
+// A putRule describes how a given record type is modified by describing
+// which input columns should be replaced with which clause expression and
+// which clauses should be appended.  The type of each clause expression
+// is recorded since a new rule must be created if any of the types change.
+// Such changes aren't typically expected but are possible in the expression
+// language.
+type putRule struct {
+	typ         *zng.TypeRecord
+	clauseTypes []clauseType
+	// The clause numbers indexed by input column number that should be
+	// replaced, where -1 indicates no replacement.  Nil if there are no
+	// replacements.
+	replace []int
+	// The clause numbers that should be appeneded to the output record
+	// ordered left to right.  Nil if nothing need be appended.
+	append []int
+}
+
+type clauseType struct {
+	zng.Type
+	container bool
 }
 
 type clause struct {
@@ -51,12 +63,11 @@ func CompilePutProc(c *Context, parent Proc, node *ast.PutProc) (*Put, error) {
 			return nil, err
 		}
 	}
-
 	return &Put{
 		Base:    Base{Context: c, Parent: parent},
 		clauses: clauses,
 		vals:    make([]zng.Value, len(node.Clauses)),
-		outmap:  make(map[int]descinfo),
+		rules:   make(map[int]*putRule),
 		warned:  make(map[string]struct{}),
 	}, nil
 }
@@ -70,125 +81,107 @@ func (p *Put) maybeWarn(err error) {
 	}
 }
 
-func (p *Put) put(in *zng.Record) *zng.Record {
-	// Figure out the output descriptor.  If we don't have one for
-	// this input descriptor or if any values have different types,
-	// we'll need to recompute it below.
-	descInfo, haveDescriptor := p.outmap[in.Type.ID()]
-
+func (p *Put) eval(in *zng.Record) ([]zng.Value, error) {
+	vals := p.vals
 	for k, cl := range p.clauses {
 		var err error
-		p.vals[k], err = cl.eval(in)
+		vals[k], err = cl.eval(in)
 		if err != nil {
-			p.maybeWarn(err)
-			return in
-		}
-
-		if haveDescriptor && p.vals[k].Type != descInfo.fields[k].valType {
-			haveDescriptor = false
+			return nil, err
 		}
 	}
+	return vals, nil
+}
 
-	// Figure out the output descriptor.  We cache it in outmap
-	// but if we haven't seen this input descriptor before or if
-	// the computed type of the expression has changed, recompute it.
-	if !haveDescriptor {
-		origCols := len(in.Type.Columns)
-		cols := make([]zng.Column, origCols, origCols+len(p.clauses))
-		for i, c := range in.Type.Columns {
-			cols[i] = c
-		}
-
-		fields := make([]fieldinfo, len(p.clauses))
-		for k, cl := range p.clauses {
-			newcolumn := zng.Column{
-				Name: cl.target,
-				Type: p.vals[k].Type,
-			}
-
-			position, hasCol := in.Type.ColumnOfField(cl.target)
-			if hasCol {
-				cols[position] = newcolumn
-			} else {
-				position = -1
-				cols = append(cols, newcolumn)
-			}
-			fields[k].valType = p.vals[k].Type
-			fields[k].position = position
-			fields[k].container = p.vals[k].IsContainer()
-		}
-
-		typ, err := p.TypeContext.LookupTypeRecord(cols)
-		if err != nil {
-			p.maybeWarn(err)
-			return in
-		}
-
-		// Compute the order in which to write fields (i.e., if
-		// we're overriding existing fields, write them in the
-		// order they appear in the output record).
-		order := make([]int, len(p.clauses))
-		for k := range order {
-			order[k] = k
-		}
-		sort.Slice(order, func(a, b int) bool {
-			if fields[a].position == -1 {
-				return false
-			}
-			if fields[b].position == -1 {
-				return true
-			}
-			return fields[a].position < fields[b].position
-		})
-
-		newinfo := descinfo{
-			typ:    typ,
-			fields: fields,
-			order:  order,
-		}
-
-		p.outmap[in.Type.ID()] = newinfo
-		descInfo = newinfo
+func (p *Put) buildRule(inType *zng.TypeRecord, vals []zng.Value) (*putRule, error) {
+	n := len(inType.Columns)
+	cols := make([]zng.Column, n, n+len(p.clauses))
+	copy(cols, inType.Columns)
+	clauseTypes := make([]clauseType, len(p.clauses))
+	var nreplace int
+	replace := make([]int, n)
+	for k := range replace {
+		replace[k] = -1
 	}
+	var tail []int
+	for k, cl := range p.clauses {
+		typ := vals[k].Type
+		clauseTypes[k] = clauseType{typ, zng.IsContainerType(typ)}
+		col := zng.Column{Name: cl.target, Type: typ}
+		position, hasCol := inType.ColumnOfField(cl.target)
+		if hasCol {
+			nreplace++
+			replace[position] = k
+			cols[position] = col
+		} else {
+			tail = append(tail, k)
+			cols = append(cols, col)
+		}
+	}
+	if nreplace == 0 {
+		replace = nil
+	}
+	typ, err := p.TypeContext.LookupTypeRecord(cols)
+	if err != nil {
+		return nil, err
+	}
+	return &putRule{
+		typ:         typ,
+		clauseTypes: clauseTypes,
+		replace:     replace,
+		append:      tail,
+	}, nil
+}
 
-	// Build the new output value
+func clauseTypesMatch(types []clauseType, vals []zng.Value) bool {
+	for k, typ := range types {
+		if vals[k].Type != typ.Type {
+			return false
+		}
+	}
+	return true
+}
+
+func (p *Put) lookupRule(inType *zng.TypeRecord, vals []zng.Value) (*putRule, error) {
+	rule := p.rules[inType.ID()]
+	if rule != nil && clauseTypesMatch(rule.clauseTypes, vals) {
+		return rule, nil
+	}
+	rule, err := p.buildRule(inType, vals)
+	p.rules[inType.ID()] = rule
+	return rule, err
+}
+
+func (p *Put) put(in *zng.Record) *zng.Record {
+	vals, err := p.eval(in)
+	if err != nil {
+		p.maybeWarn(err)
+		return in
+	}
+	rule, err := p.lookupRule(in.Type, vals)
+	if err != nil {
+		p.maybeWarn(err)
+		return in
+	}
+	// Start the new output value by either copying or replacing the input values.
 	var bytes zcode.Bytes
-	i := 0
-	if descInfo.fields[descInfo.order[i]].position == -1 {
-		// all fields are being appended...
+	if rule.replace == nil {
+		// All fields are being appended.
 		bytes = in.Raw
 	} else {
-		// we're overwriting one or more fields.  descInfo.order
-		// has the column numbers we need, sorted.
-		origCols := len(in.Type.Columns)
+		// We're overwriting one or more fields.  Travese the
+		// replacement vector to determine whether each value should
+		// be copied from the input or replaced with a clause result.
 		iter := in.ZvalIter()
-		vali := descInfo.order[i]
-		nextcol := descInfo.fields[vali].position
-		for col := range descInfo.typ.Columns {
-			if col == origCols {
-				break
-			}
+		for _, clause := range rule.replace {
 			item, isContainer, err := iter.Next()
 			if err != nil {
 				panic(err)
 			}
-			if col == nextcol {
-				item = p.vals[vali].Bytes
-				isContainer = descInfo.fields[vali].container
-
-				// Advance to the next column we need to
-				// overwrite.  If we're done overwriting
-				// columns, just set nextcol to -1 so this
-				// loop finishes copying existing fields.
-				i++
-				if i < len(descInfo.fields) {
-					vali = descInfo.order[i]
-					nextcol = descInfo.fields[vali].position
-				} else {
-					nextcol = -1
-				}
+			if clause >= 0 {
+				item = vals[clause].Bytes
+				isContainer = rule.clauseTypes[clause].container
 			}
-
 			if isContainer {
 				bytes = zcode.AppendContainer(bytes, item)
 			} else {
@@ -196,19 +189,16 @@ func (p *Put) put(in *zng.Record) *zng.Record {
 			}
 		}
 	}
-
-	// any remaining fields are to be appended
-	for i < len(descInfo.fields) {
-		vali := descInfo.order[i]
-		i++
-		if descInfo.fields[vali].container {
-			bytes = zcode.AppendContainer(bytes, p.vals[vali].Bytes)
+	// Finish building the output by appending the remaining clauses if any.
+	for _, clause := range rule.append {
+		item := vals[clause].Bytes
+		if rule.clauseTypes[clause].container {
+			bytes = zcode.AppendContainer(bytes, item)
 		} else {
-			bytes = zcode.AppendPrimitive(bytes, p.vals[vali].Bytes)
+			bytes = zcode.AppendPrimitive(bytes, item)
 		}
 	}
-
-	return zng.NewRecord(descInfo.typ, bytes)
+	return zng.NewRecord(rule.typ, bytes)
 }
 
 func (p *Put) Pull() (zbuf.Batch, error) {
@@ -216,7 +206,6 @@ func (p *Put) Pull() (zbuf.Batch, error) {
 	if EOS(batch, err) {
 		return nil, err
 	}
-
 	recs := make([]*zng.Record, 0, batch.Length())
 	for k := 0; k < batch.Length(); k++ {
 		in := batch.Index(k)
