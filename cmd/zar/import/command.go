8b8e2e049e8022d7f73c65a5aac0a116c5da6826
Henri DF <henridf@gmail.com>
2020-09-02 20:49:28 +0200

    Fix zar import -zngcheck (#1205)

--- cmd/zar/import/command.go
+++ cmd/zar/import/command.go
@@ -87,7 +87,7 @@ func (c *Command) Run(args []string) error {
 		Format: c.ReaderFlags.Format,
 		//JSONTypeConfig: c.jsonTypeConfig,
 		//JSONPathRegex:  c.jsonPathRegexp,
-		ZngCheck: true,
+		ZngCheck: c.ReaderFlags.ZngCheck,
 	}
 	reader, err := detector.OpenFile(zctx, path, cfg)
 	if err != nil {
