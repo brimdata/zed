ddecb33ecc048d4793479c5488c7fbef4bcb4021
Alfred Landrum <alfred@leakybucket.org>
2020-09-10 12:56:33 -0700

    handle file paths with uri escape codes (#1238)

--- pkg/iosrc/uri_unix.go
+++ pkg/iosrc/uri_unix.go
@@ -6,16 +6,16 @@ import (
 	"path/filepath"
 )
 
-func normalizeFilepaths(path string) (string, error) {
+func parseBarePath(path string) (URI, bool, error) {
 	scheme, err := getscheme(path)
 	if err != nil || scheme != "" {
-		return path, err
+		return URI{}, false, err
 	}
 	path, err = filepath.Abs(path)
 	if err != nil {
-		return "", err
+		return URI{}, false, err
 	}
-	return "file://" + path, nil
+	return URI{Scheme: FileScheme, Path: path}, true, nil
 }
 
 func (p URI) Filepath() string {
