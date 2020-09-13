f92d55172b52dcac8f85b0085c4e481ec6c2689d
Steven McCanne <steve@mccanne.com>
2020-09-12 18:02:29 -0700

    reorder cli.go source

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -18,73 +18,27 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-type OutputFlags struct {
-	Dir          string
-	OutputFile   string
-	forceBinary  bool
-	textShortcut bool
-}
-
-func (f *OutputFlags) SetFlags(fs *flag.FlagSet) {
-	fs.StringVar(&f.Dir, "d", "", "directory for output data files")
-	fs.StringVar(&f.OutputFile, "o", "", "write data to output file")
-	fs.BoolVar(&f.textShortcut, "t", false, "use format tzng independent of -f option")
-	fs.BoolVar(&f.forceBinary, "B", false, "allow binary zng be sent to a terminal output")
-}
-
-func (f *OutputFlags) Init(opts *options.Writer) error {
-	if f.textShortcut {
-		if opts.Format != "zng" {
-			return errors.New("cannot use -t with -f")
-		}
-		opts.Format = "tzng"
-	}
-	if f.OutputFile == "" && opts.Format == "zng" && IsTerminal(os.Stdout) && !f.forceBinary {
-		return errors.New("writing binary zng data to terminal; override with -B or use -t for text.")
-	}
-	return nil
-}
-
-func (o *OutputFlags) Open(opts options.Writer) (zbuf.WriteCloser, error) {
-	if o.Dir != "" {
-		d, err := emitter.NewDir(o.Dir, o.OutputFile, os.Stderr, opts)
-		if err != nil {
-			return nil, err
-		}
-		return d, nil
-	}
-	w, err := emitter.NewFile(o.OutputFile, opts)
-	if err != nil {
-		return nil, err
-	}
-	return w, nil
-}
-
 type Flags struct {
-	sortMemMaxMiB int
+	sortMemMaxMiB float64
 	cpuprofile    string
 	memprofile    string
 }
 
 func (f *Flags) SetFlags(fs *flag.FlagSet) {
-	fs.IntVar(&f.sortMemMaxMiB, "sortmem", sort.MemMaxBytes/(1024*1024), "maximum memory used by sort, in MiB")
+	fs.Float64Var(&f.sortMemMaxMiB, "sortmem", float64(sort.MemMaxBytes)/(1024*1024), "maximum memory used by sort, in MiB")
 	fs.StringVar(&f.cpuprofile, "cpuprofile", "", "write cpu profile to `file`")
 	fs.StringVar(&f.memprofile, "memprofile", "", "write memory profile to `file`")
 }
 
-func FileExists(path string) bool {
-	if path == "-" {
-		return true
+func (f *Flags) Init() error {
+	if f.cpuprofile != "" {
+		runCPUProfile(f.cpuprofile)
 	}
-	info, err := os.Stat(path)
-	if err != nil {
-		return false
+	if f.sortMemMaxMiB <= 0 {
+		return errors.New("sortmem value must be greater than zero")
 	}
-	return !info.IsDir()
-}
-
-func IsTerminal(f *os.File) bool {
-	return terminal.IsTerminal(int(f.Fd()))
+	sort.MemMaxBytes = int(f.sortMemMaxMiB * 1024 * 1024)
+	return nil
 }
 
 func (f *Flags) Cleanup() {
@@ -96,15 +50,46 @@ func (f *Flags) Cleanup() {
 	}
 }
 
-func (f *Flags) Init() error {
-	if f.cpuprofile != "" {
-		runCPUProfile(f.cpuprofile)
+type OutputFlags struct {
+	dir          string
+	outputFile   string
+	forceBinary  bool
+	textShortcut bool
+}
+
+func (f *OutputFlags) SetFlags(fs *flag.FlagSet) {
+	fs.StringVar(&f.dir, "d", "", "directory for output data files")
+	fs.StringVar(&f.outputFile, "o", "", "write data to output file")
+	fs.BoolVar(&f.textShortcut, "t", false, "use format tzng independent of -f option")
+	fs.BoolVar(&f.forceBinary, "B", false, "allow binary zng be sent to a terminal output")
+}
+
+func (f *OutputFlags) Init(opts *options.Writer) error {
+	if f.textShortcut {
+		if opts.Format != "zng" {
+			return errors.New("cannot use -t with -f")
+		}
+		opts.Format = "tzng"
 	}
-	if f.sortMemMaxMiB <= 0 {
-		return errors.New("sortmem value must be greater than zero")
+	if f.outputFile == "" && opts.Format == "zng" && IsTerminal(os.Stdout) && !f.forceBinary {
+		return errors.New("writing binary zng data to terminal; override with -B or use -t for text.")
 	}
-	sort.MemMaxBytes = f.sortMemMaxMiB * 1024 * 1024
 	return nil
+}
+
+func (o *OutputFlags) Open(opts options.Writer) (zbuf.WriteCloser, error) {
+	if o.dir != "" {
+		d, err := emitter.NewDir(o.dir, o.outputFile, os.Stderr, opts)
+		if err != nil {
+			return nil, err
+		}
+		return d, nil
+	}
+	w, err := emitter.NewFile(o.outputFile, opts)
+	if err != nil {
+		return nil, err
+	}
+	return w, nil
 }
 
 func OpenInputs(zctx *resolver.Context, opts options.Reader, paths []string, stopOnErr bool) ([]zbuf.Reader, error) {
@@ -125,6 +110,21 @@ func OpenInputs(zctx *resolver.Context, opts options.Reader, paths []string, sto
 		readers = append(readers, file)
 	}
 	return readers, nil
+}
+
+func FileExists(path string) bool {
+	if path == "-" {
+		return true
+	}
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func IsTerminal(f *os.File) bool {
+	return terminal.IsTerminal(int(f.Fd()))
 }
 
 func runCPUProfile(path string) {
