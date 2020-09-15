cb53ebb043637e01206073e1f9abc9960b21e2b0
Steven McCanne <steve@mccanne.com>
2020-09-14 19:23:39 -0700

    add ndjson option to search endpoint
    This commit adds an ndjson option to the search endpoint enabled by
    setting the "format" query parameter on the search to "ndjson".

--- zqd/handlers.go
+++ zqd/handlers.go
@@ -120,7 +120,9 @@ func getSearchOutput(w http.ResponseWriter, r *http.Request) (search.Output, err
 	switch format {
 	case "csv":
 		return search.NewCSVOutput(w, ctrl), nil
-	case "zjson", "ndjson":
+	case "ndjson":
+		return search.NewNDJSONOutput(w), nil
+	case "zjson":
 		return search.NewJSONOutput(w, search.DefaultMTU, ctrl), nil
 	case "zng":
 		return search.NewZngOutput(w, ctrl), nil
