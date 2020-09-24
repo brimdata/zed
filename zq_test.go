df7b0cd8f47e973183972ebb88296da9553a90d4
Steven McCanne <steve@mccanne.com>
2020-09-23 23:28:27 -0700

    avoid conflict with charts/zqd/templates/tests/*.yaml

--- zq_test.go
+++ zq_test.go
@@ -14,9 +14,9 @@ import (
 func TestZq(t *testing.T) {
 	t.Parallel()
 	dirs := map[string]struct{}{}
-	re, _ := regexp.Compile(".*tests/.*\\.yaml$")
+	re, _ := regexp.Compile(".*ztests/.*\\.yaml$")
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() && strings.HasSuffix(path, ".yaml") && re.MatchString(path) && !strings.Contains(path, "zqd/templates") {
+		if !info.IsDir() && strings.HasSuffix(path, ".yaml") && (re.MatchString(path) || strings.HasPrefix(path, "tests/")) {
 			dirs[filepath.Dir(path)] = struct{}{}
 		}
 		return err
