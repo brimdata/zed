brimdata/zed

commit 0382a0f8e31485232f033bda8e41f243f160aaa6

create spill package (#1345)
This commit refactors the spill code used by sort and groupby so
its interdependencies are cleanly delineated and it can be reused
by other packages without reaching into the sort package.

This also spill.File into a spilled file and a peeker so single-file
spilling can be implemented without necessarily needing to sort
each chunk (e.g., as will be used by the fuse proc).

proc/spill/peeker.go
@@ -1,0 +1,44 @@
+package spill
+
+import (
+	"github.com/brimsec/zq/zng"
+	"github.com/brimsec/zq/zng/resolver"
+)
+
+type peeker struct {
+	*File
+	nextRecord *zng.Record
+}
+
+func newPeeker(filename string, recs []*zng.Record, zctx *resolver.Context) (*peeker, error) {
+	f, err := NewFileWithPath(filename, zctx)
+	if err != nil {
+		return nil, err
+	}
+	for _, rec := range recs {
+		if err := f.Write(rec); err != nil {
+			f.closeAndRemove()
+			return nil, err
+		}
+	}
+	if err := f.Rewind(zctx); err != nil {
+		f.closeAndRemove()
+		return nil, err
+	}
+	first, err := f.Read()
+	if err != nil {
+		f.closeAndRemove()
+		return nil, err
+	}
+	return &peeker{f, first}, nil
+}
+
+// read is like Read but returns eof at the last record so a MergeSort can
+// do its heap management a bit more easily.
+func (p *peeker) read() (*zng.Record, bool, error) {
+	rec := p.nextRecord
+	var err error
+	p.nextRecord, err = p.Read()
+	eof := p.nextRecord == nil && err == nil
+	return rec, eof, err
+}

proc/groupby/groupby.go
@@ -9,7 +9,7 @@ import (
 	"github.com/brimsec/zq/ast"
 	"github.com/brimsec/zq/expr"
 	"github.com/brimsec/zq/proc"
-	"github.com/brimsec/zq/proc/sort"
+	"github.com/brimsec/zq/proc/spill"
 	"github.com/brimsec/zq/reducer"
 	"github.com/brimsec/zq/reducer/compile"
 	"github.com/brimsec/zq/zbuf"
@@ -150,7 +150,7 @@ type Aggregator struct {
 	maxTableKey  *zng.Value
 	maxSpillKey  *zng.Value
 	inputSortDir int
-	runManager   *sort.RunManager
+	spiller      *spill.MergeSort
 	consumePart  bool
 	emitPart     bool
 }
@@ -251,8 +251,8 @@ func (p *Proc) Done() {
 func (p *Proc) run() {
 	defer func() {
 		close(p.resultCh)
-		if p.agg.runManager != nil {
-			p.agg.runManager.Cleanup()
+		if p.agg.spiller != nil {
+			p.agg.spiller.Cleanup()
 		}
 	}()
 	for {
@@ -431,15 +431,15 @@ func (a *Aggregator) spillTable(eof bool) error {
 	if err != nil || batch == nil {
 		return err
 	}
-	if a.runManager == nil {
-		a.runManager, err = sort.NewRunManager(a.keysCompare)
+	if a.spiller == nil {
+		a.spiller, err = spill.NewMergeSort(a.keysCompare)
 		if err != nil {
 			return err
 		}
 	}
 	recs := batch.Records()
 	// Note that this will sort recs according to g.keysCompare.
-	if err := a.runManager.CreateRun(recs); err != nil {
+	if err := a.spiller.Spill(recs); err != nil {
 		return err
 	}
 	if !eof && a.inputSortDir != 0 {
@@ -477,7 +477,7 @@ func (a *Aggregator) updateMaxSpillKey(v zng.Value) {
 // the input is sorted in the primary key, Results can be called
 // before eof, and keys that are completed will returned.
 func (a *Aggregator) Results(eof bool) (zbuf.Batch, error) {
-	if a.runManager == nil {
+	if a.spiller == nil {
 		return a.readTable(eof, a.emitPart)
 	}
 	if eof {
@@ -496,7 +496,7 @@ func (a *Aggregator) readSpills(eof bool) (zbuf.Batch, error) {
 	}
 	for len(recs) < proc.BatchLen {
 		if !eof && a.inputSortDir != 0 {
-			rec, err := a.runManager.Peek()
+			rec, err := a.spiller.Peek()
 			if err != nil {
 				return nil, err
 			}
@@ -531,7 +531,7 @@ func (a *Aggregator) nextResultFromSpills() (*zng.Record, error) {
 	row := compile.NewRow(a.reducerDefs)
 	var firstRec *zng.Record
 	for {
-		rec, err := a.runManager.Peek()
+		rec, err := a.spiller.Peek()
 		if err != nil {
 			return nil, err
 		}
@@ -546,7 +546,7 @@ func (a *Aggregator) nextResultFromSpills() (*zng.Record, error) {
 		if err := row.ConsumePart(rec); err != nil {
 			return nil, err
 		}
-		if _, err := a.runManager.Read(); err != nil {
+		if _, err := a.spiller.Read(); err != nil {
 			return nil, err
 		}
 	}

proc/sort/manager.go
@@ -1,114 +1,0 @@
-package sort
-
-import (
-	"container/heap"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strconv"
-
-	"github.com/brimsec/zq/expr"
-	"github.com/brimsec/zq/zng"
-	"github.com/brimsec/zq/zng/resolver"
-)
-
-// runManager manages runs (files of sorted records).
-type RunManager struct {
-	runs       []*runFile
-	runIndices map[*runFile]int
-	compareFn  expr.CompareFn
-	tempDir    string
-	zctx       *resolver.Context
-}
-
-// NewRunManager creates a temporary directory.  Call Cleanup to remove it.
-func NewRunManager(compareFn expr.CompareFn) (*RunManager, error) {
-	tempDir, err := ioutil.TempDir("", "zq-sort-")
-	if err != nil {
-		return nil, err
-	}
-	return &RunManager{
-		runIndices: make(map[*runFile]int),
-		compareFn:  compareFn,
-		tempDir:    tempDir,
-		zctx:       resolver.NewContext(),
-	}, nil
-}
-
-func (r *RunManager) Cleanup() {
-	for _, run := range r.runs {
-		run.closeAndRemove()
-	}
-	os.RemoveAll(r.tempDir)
-}
-
-// CreateRun creates a new run containing the records in recs.
-func (r *RunManager) CreateRun(recs []*zng.Record) error {
-	expr.SortStable(recs, r.compareFn)
-	index := len(r.runIndices)
-	filename := filepath.Join(r.tempDir, strconv.Itoa(index))
-	runFile, err := newRunFile(filename, recs, r.zctx)
-	if err != nil {
-		return err
-	}
-	r.runIndices[runFile] = index
-	heap.Push(r, runFile)
-	return nil
-}
-
-// Peek returns the next record without advancing the reader.  The record stops
-// being valid at the next read call.
-func (r *RunManager) Peek() (*zng.Record, error) {
-	if r.Len() == 0 {
-		return nil, nil
-	}
-	return r.runs[0].nextRecord, nil
-}
-
-// Read returns the smallest record (per Less) from among the next records in
-// each run.  It implements the merge operation for an external merge sort.
-func (r *RunManager) Read() (*zng.Record, error) {
-	for {
-		if r.Len() == 0 {
-			return nil, nil
-		}
-		rec, eof, err := r.runs[0].read()
-		if err != nil {
-			return nil, err
-		}
-		if eof {
-			r.runs[0].closeAndRemove()
-			heap.Pop(r)
-		} else {
-			heap.Fix(r, 0)
-		}
-		if rec != nil {
-			return rec, nil
-		}
-	}
-}
-
-func (r *RunManager) Len() int { return len(r.runs) }
-
-func (r *RunManager) Less(i, j int) bool {
-	v := r.compareFn(r.runs[i].nextRecord, r.runs[j].nextRecord)
-	switch {
-	case v < 0:
-		return true
-	case v == 0:
-		// Maintain stability.
-		return r.runIndices[r.runs[i]] < r.runIndices[r.runs[j]]
-	default:
-		return false
-	}
-}
-
-func (r *RunManager) Swap(i, j int) { r.runs[i], r.runs[j] = r.runs[j], r.runs[i] }
-
-func (r *RunManager) Push(x interface{}) { r.runs = append(r.runs, x.(*runFile)) }
-
-func (r *RunManager) Pop() interface{} {
-	x := r.runs[len(r.runs)-1]
-	r.runs = r.runs[:len(r.runs)-1]
-	return x
-}

proc/sort/sort.go
@@ -7,6 +7,7 @@ import (
 	"github.com/brimsec/zq/ast"
 	"github.com/brimsec/zq/expr"
 	"github.com/brimsec/zq/proc"
+	"github.com/brimsec/zq/proc/spill"
 	"github.com/brimsec/zq/zbuf"
 	"github.com/brimsec/zq/zng"
 )
@@ -124,12 +125,12 @@ func (p *Proc) recordsForOneRun() ([]*zng.Record, bool, error) {
 	}
 }
 
-func (p *Proc) createRuns(firstRunRecs []*zng.Record) (*RunManager, error) {
-	rm, err := NewRunManager(p.compareFn)
+func (p *Proc) createRuns(firstRunRecs []*zng.Record) (*spill.MergeSort, error) {
+	rm, err := spill.NewMergeSort(p.compareFn)
 	if err != nil {
 		return nil, err
 	}
-	if err := rm.CreateRun(firstRunRecs); err != nil {
+	if err := rm.Spill(firstRunRecs); err != nil {
 		rm.Cleanup()
 		return nil, err
 	}
@@ -140,7 +141,7 @@ func (p *Proc) createRuns(firstRunRecs []*zng.Record) (*RunManager, error) {
 			return nil, err
 		}
 		if recs != nil {
-			if err := rm.CreateRun(recs); err != nil {
+			if err := rm.Spill(recs); err != nil {
 				rm.Cleanup()
 				return nil, err
 			}

proc/sort/spill.go
@@ -1,81 +1,0 @@
-package sort
-
-import (
-	"bufio"
-	"io"
-	"os"
-
-	"github.com/brimsec/zq/pkg/bufwriter"
-	"github.com/brimsec/zq/pkg/fs"
-	"github.com/brimsec/zq/zbuf"
-	"github.com/brimsec/zq/zio"
-	"github.com/brimsec/zq/zio/zngio"
-	"github.com/brimsec/zq/zng"
-	"github.com/brimsec/zq/zng/resolver"
-)
-
-// runFile represents a run as as a readable file containing a sorted sequence
-// of records.
-type runFile struct {
-	file       *os.File
-	nextRecord *zng.Record
-	zr         zbuf.Reader
-}
-
-// newRunFile writes sorted to filename and returns a runFile that reads the
-// file using zctx.
-func newRunFile(filename string, sorted []*zng.Record, zctx *resolver.Context) (*runFile, error) {
-	f, err := fs.Create(filename)
-	if err != nil {
-		return nil, err
-	}
-	r := &runFile{file: f}
-	if err := writeZng(f, sorted); err != nil {
-		r.closeAndRemove()
-		return nil, err
-	}
-	if _, err := f.Seek(0, 0); err != nil {
-		r.closeAndRemove()
-		return nil, err
-	}
-	zr := zngio.NewReader(bufio.NewReader(f), zctx)
-	rec, err := zr.Read()
-	if err != nil {
-		r.closeAndRemove()
-		return nil, err
-	}
-	return &runFile{
-		file:       f,
-		nextRecord: rec,
-		zr:         zr,
-	}, nil
-}
-
-// closeAndRemove closes and removes the underlying file.
-func (r *runFile) closeAndRemove() {
-	r.file.Close()
-	os.Remove(r.file.Name())
-}
-
-// read returns the next record along with a boolean that is true at EOF.
-func (r *runFile) read() (*zng.Record, bool, error) {
-	rec := r.nextRecord
-	if rec != nil {
-		rec = rec.Keep()
-	}
-	var err error
-	r.nextRecord, err = r.zr.Read()
-	eof := r.nextRecord == nil && err == nil
-	return rec, eof, err
-}
-
-// writeZng writes records to w as a zng stream.
-func writeZng(w io.Writer, records []*zng.Record) error {
-	zw := zngio.NewWriter(bufwriter.New(zio.NopCloser(w)), zngio.WriterOpts{})
-	for _, rec := range records {
-		if err := zw.Write(rec); err != nil {
-			return err
-		}
-	}
-	return zw.Close()
-}

proc/spill/file.go
@@ -1,0 +1,71 @@
+package spill
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/brimsec/zq/pkg/bufwriter"
+	"github.com/brimsec/zq/pkg/fs"
+	"github.com/brimsec/zq/zio"
+	"github.com/brimsec/zq/zio/zngio"
+	"github.com/brimsec/zq/zng/resolver"
+)
+
+// File provides a means to write a sequence of zng records to temporary
+// storage then read them back.  This is used for processing large batches of
+// data that do not fit in memory and/or cannot be shuffled to a peer worker,
+// but can be processed in multiple passes.  File implements zbuf.Reader and
+// zbuf.Writer.
+type File struct {
+	*zngio.Reader
+	*zngio.Writer
+	file *os.File
+}
+
+// NewFile returns a File.  Records should be written to File via the zbuf.Writer
+// interface, followed by a call to the Rewind method, followed by reading
+// records via the zbuf.Reader interface.
+func NewFile(f *os.File) *File {
+	return &File{
+		Writer: zngio.NewWriter(bufwriter.New(zio.NopCloser(f)), zngio.WriterOpts{}),
+		file:   f,
+	}
+}
+
+func NewTempFile() (*File, error) {
+	f, err := TempFile()
+	if err != nil {
+		return nil, err
+	}
+	return NewFile(f), nil
+}
+
+func NewFileWithPath(path string, zctx *resolver.Context) (*File, error) {
+	f, err := fs.Create(path)
+	if err != nil {
+		return nil, err
+	}
+	return NewFile(f), nil
+}
+
+func (f *File) Rewind(zctx *resolver.Context) error {
+	// Close the writer to flush any pending output but since we
+	// wrapped the file in a zio.NopCloser, the file will stay open.
+	if err := f.Writer.Close(); err != nil {
+		return err
+	}
+	f.Writer = nil
+	if _, err := f.file.Seek(0, 0); err != nil {
+		f.closeAndRemove()
+		return err
+	}
+	f.Reader = zngio.NewReader(bufio.NewReader(f.file), zctx)
+	return nil
+}
+
+// closeAndRemove closes and removes the underlying file.
+// Errors are ignored.
+func (r *File) closeAndRemove() {
+	r.file.Close()
+	os.Remove(r.file.Name())
+}

proc/spill/merge.go
@@ -1,0 +1,132 @@
+package spill
+
+import (
+	"container/heap"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/brimsec/zq/expr"
+	"github.com/brimsec/zq/zng"
+	"github.com/brimsec/zq/zng/resolver"
+)
+
+// MergeSort manages "runs" (files of sorted zng records) that are spilled to
+// disk a chunk at a time, then read back and merged in sorted order, effectively
+// implementing an external merge sort.
+type MergeSort struct {
+	runs       []*peeker
+	runIndices map[*peeker]int
+	compareFn  expr.CompareFn
+	tempDir    string
+	zctx       *resolver.Context
+}
+
+const TempPrefix = "zq-spill-"
+
+func TempDir() (string, error) {
+	return ioutil.TempDir("", TempPrefix)
+}
+
+func TempFile() (*os.File, error) {
+	return ioutil.TempFile("", TempPrefix)
+}
+
+// NewMergeSort returns a MergeSort to implement external merge sorts of a large
+// zng record stream.  It creates a temporary directory to hold the collection
+// of spilled chunks.  Call Cleanup to remove it.
+func NewMergeSort(compareFn expr.CompareFn) (*MergeSort, error) {
+	tempDir, err := TempDir()
+	if err != nil {
+		return nil, err
+	}
+	return &MergeSort{
+		runIndices: make(map[*peeker]int),
+		compareFn:  compareFn,
+		tempDir:    tempDir,
+		zctx:       resolver.NewContext(),
+	}, nil
+}
+
+func (r *MergeSort) Cleanup() {
+	for _, run := range r.runs {
+		run.closeAndRemove()
+	}
+	os.RemoveAll(r.tempDir)
+}
+
+// Spill sorts and spills a new run of records to a file in the MergeSort's
+// temp directory.  Since we sort each chunk in memory before spilling, the
+// different chunks can be easily merged into sorted order when reading back
+// the chunks sequentially.
+func (r *MergeSort) Spill(recs []*zng.Record) error {
+	expr.SortStable(recs, r.compareFn)
+	index := len(r.runIndices)
+	filename := filepath.Join(r.tempDir, strconv.Itoa(index))
+	runFile, err := newPeeker(filename, recs, r.zctx)
+	if err != nil {
+		return err
+	}
+	r.runIndices[runFile] = index
+	heap.Push(r, runFile)
+	return nil
+}
+
+// Peek returns the next record without advancing the reader.  The record stops
+// being valid at the next read call.
+func (r *MergeSort) Peek() (*zng.Record, error) {
+	if r.Len() == 0 {
+		return nil, nil
+	}
+	return r.runs[0].nextRecord, nil
+}
+
+// Read returns the smallest record (per the comparison function provided to MewMergeSort)
+// from among the next records in the spilled chunks.  It implements the merge operation
+// for an external merge sort.
+func (r *MergeSort) Read() (*zng.Record, error) {
+	for {
+		if r.Len() == 0 {
+			return nil, nil
+		}
+		rec, eof, err := r.runs[0].read()
+		if err != nil {
+			return nil, err
+		}
+		if eof {
+			r.runs[0].closeAndRemove()
+			heap.Pop(r)
+		} else {
+			heap.Fix(r, 0)
+		}
+		if rec != nil {
+			return rec, nil
+		}
+	}
+}
+
+func (r *MergeSort) Len() int { return len(r.runs) }
+
+func (r *MergeSort) Less(i, j int) bool {
+	v := r.compareFn(r.runs[i].nextRecord, r.runs[j].nextRecord)
+	switch {
+	case v < 0:
+		return true
+	case v == 0:
+		// Maintain stability.
+		return r.runIndices[r.runs[i]] < r.runIndices[r.runs[j]]
+	default:
+		return false
+	}
+}
+
+func (r *MergeSort) Swap(i, j int) { r.runs[i], r.runs[j] = r.runs[j], r.runs[i] }
+
+func (r *MergeSort) Push(x interface{}) { r.runs = append(r.runs, x.(*peeker)) }
+
+func (r *MergeSort) Pop() interface{} {
+	x := r.runs[len(r.runs)-1]
+	r.runs = r.runs[:len(r.runs)-1]
+	return x
+}

commit 6004eb363ef685131b27685c6e74b4200a126831

fix record volatility bug introduced in commit 0382a0f
This commit fixes a bug that was introduced in commit 0382a0f,
where a rec.Keep() call was dropped in the spill/read path compared
to the old code.  This fix puts the call back in.

proc/spill/peeker.go
@@ -37,6 +37,9 @@ func newPeeker(filename string, recs []*zng.Record, zctx *resolver.Context) (*pe
 // do its heap management a bit more easily.
 func (p *peeker) read() (*zng.Record, bool, error) {
 	rec := p.nextRecord
+	if rec != nil {
+		rec = rec.Keep()
+	}
 	var err error
 	p.nextRecord, err = p.Read()
 	eof := p.nextRecord == nil && err == nil