848bc90ecb0defbdb986ef93eaba499556f97e53
Steven McCanne <steve@mccanne.com>
2020-10-12 23:41:36 -0700

    oops

---
+++ anymath/math.go
@@ -0,0 +1,70 @@
+package anymath
+
+import "math"
+
+type Init struct {
+	Float64 float64
+	Int64   int64
+	Uint64  uint64
+}
+
+type Int64 func(int64, int64) int64
+type Uint64 func(uint64, uint64) uint64
+type Float64 func(float64, float64) float64
+
+type Function struct {
+	Init
+	Int64
+	Uint64
+	Float64
+}
+
+var Min = &Function{
+	Float64: func(a, b float64) float64 {
+		if a < b {
+			return a
+		}
+		return b
+	},
+	Int64: func(a, b int64) int64 {
+		if a < b {
+			return a
+		}
+		return b
+	},
+	Uint64: func(a, b uint64) uint64 {
+		if a < b {
+			return a
+		}
+		return b
+	},
+	Init: Init{math.MaxFloat64, math.MaxInt64, math.MaxUint64},
+}
+
+var Max = &Function{
+	Init: Init{-math.MaxFloat64, math.MinInt64, 0},
+	Float64: func(a, b float64) float64 {
+		if a > b {
+			return a
+		}
+		return b
+	},
+	Int64: func(a, b int64) int64 {
+		if a > b {
+			return a
+		}
+		return b
+	},
+	Uint64: func(a, b uint64) uint64 {
+		if a > b {
+			return a
+		}
+		return b
+	},
+}
+
+var Add = &Function{
+	Float64: func(a, b float64) float64 { return a + b },
+	Int64:   func(a, b int64) int64 { return a + b },
+	Uint64:  func(a, b uint64) uint64 { return a + b },
+}
