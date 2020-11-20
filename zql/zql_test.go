1919f95e7570c2a5d7bd4b813debf87149ce5402
Steven McCanne <steve@mccanne.com>
2020-11-19 18:14:38 -0800

    unify the three variants of AST
    This commit unifies the parsed ASTs so that whether an AST is
    generated by pigeon, pegjs, or by marshaling an ast.Proc, the
    resulting JSON is deep equal for the same zql query text.
    Previously, different versions treated the zero and empty values
    differently.  This involved changes to the peg grammar actions
    as well as the go ast package.  We also updated the ast command
    so that it can display these three variations, as previously,
    there was no way to display the pigeon version of the ast.
    
    We udpated the unit tests to compare the three variants and
    added a test that looks through all the ztest yaml files to find
    zqls to run and compare over the three AST variants.
    
    Fixes #1638

--- zql/zql_test.go
+++ zql/zql_test.go
@@ -1,15 +1,85 @@
-package zql
+package zql_test
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/brimsec/zq/ast"
 	"github.com/brimsec/zq/pkg/fs"
+	"github.com/brimsec/zq/zql"
+	"github.com/brimsec/zq/ztest"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func searchForZqls() ([]string, error) {
+	var zqls []string
+	pattern := fmt.Sprintf(`.*ztests\%c.*\.yaml$`, filepath.Separator)
+	re := regexp.MustCompile(pattern)
+	err := filepath.Walk("..", func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && strings.HasSuffix(path, ".yaml") && re.MatchString(path) {
+			zt, err := ztest.FromYAMLFile(path)
+			if err != nil {
+				return err
+			}
+			z := zt.ZQL
+			if z == "" || z == "*" {
+				return nil
+			}
+			zqls = append(zqls, z)
+		}
+		return err
+	})
+	return zqls, err
+}
+
+func parsePEGjs(z string) ([]byte, error) {
+	cmd := exec.Command("node", "run.js", "-e", "start")
+	cmd.Stdin = strings.NewReader(z)
+	return cmd.Output()
+}
+
+func parseProc(z string) ([]byte, error) {
+	proc, err := zql.ParseProc(z)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(proc)
+}
+
+func parsePigeon(z string) ([]byte, error) {
+	ast, err := zql.Parse("", []byte(z))
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(ast)
+}
+
+// testZQL parses the zql query in line by both the Go and Javascript
+// parsers.  It checks both that the parse is successful and that the
+// two resulting ASTs are equivalent.  On the go side, we take a round
+// trip through json marshal and unmarshal to turn the parse-tree types
+// into generic JSON types.
+func testZQL(t *testing.T, line string) {
+	pegJSON, err := parsePEGjs(line)
+	assert.NoError(t, err, "parsePEGjs: %q", line)
+
+	pigeonJSON, err := parsePigeon(line)
+	assert.NoError(t, err, "parsePigeon: %q", line)
+
+	astJSON, err := parseProc(line)
+	assert.NoError(t, err, "parseProc: %q", line)
+
+	assert.JSONEq(t, string(pigeonJSON), string(pegJSON), "pigeon and PEGjs mismatch: %q", line)
+	assert.JSONEq(t, string(pigeonJSON), string(astJSON), "pigeon and ast.Proc mismatch: %q", line)
+}
 
 func TestValid(t *testing.T) {
 	file, err := fs.Open("valid.zql")
@@ -17,8 +87,15 @@ func TestValid(t *testing.T) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Bytes()
-		_, err := Parse("", line)
-		assert.NoError(t, err, "zql: %q", line)
+		testZQL(t, string(line))
+	}
+}
+
+func TestZtestZqls(t *testing.T) {
+	zqls, err := searchForZqls()
+	require.NoError(t, err)
+	for _, z := range zqls {
+		testZQL(t, z)
 	}
 }
 
@@ -28,7 +105,7 @@ func TestInvalid(t *testing.T) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Bytes()
-		_, err := Parse("", line)
+		_, err := zql.Parse("", line)
 		assert.Error(t, err, "zql: %q", line)
 	}
 }
@@ -38,7 +115,7 @@ func TestInvalid(t *testing.T) {
 // string from inside the AST.
 func parseString(in string) (string, error) {
 	code := fmt.Sprintf("s = \"%s\"", in)
-	tree, err := ParseProc(code)
+	tree, err := zql.ParseProc(code)
 	if err != nil {
 		return "", err
 	}
