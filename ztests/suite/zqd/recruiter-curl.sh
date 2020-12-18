92017d3fecd76c55b5a72faa45f39a524257ccc1
Mark Wallace <mark@Marks-MBP.lan>
2020-12-17 17:30:01 -0800

    reorder test

--- ztests/suite/zqd/recruiter-curl.sh
+++ ztests/suite/zqd/recruiter-curl.sh
@@ -4,6 +4,5 @@ zqd listen -personality=recruiter -l=localhost:0 -portfile="$portdir/zqd" -logle
 zqdpid=$!
 awaitfile $portdir/zqd
 
-trap "rm -rf $portdir; kill -9 $zqdpid" EXIT
-
 export ZQD_HOST=localhost:$(cat $portdir/zqd)
+trap "rm -rf $portdir; kill -9 $zqdpid" EXIT
