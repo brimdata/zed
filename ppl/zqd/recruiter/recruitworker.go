bedefb5d6b3f3de433f5df73870617c5ea5e701a
Mark Wallace <mark.t.wallace@gmail.com>
2020-12-22 14:05:34 -0800

    Recruit fewer workers than requested (#1868)
    I ran manual tests to verify that bug #1839 is fixed with the K8s deployment.

--- ppl/zqd/recruiter/recruitworker.go
+++ ppl/zqd/recruiter/recruitworker.go
@@ -22,10 +22,6 @@ func RecruitWorkers(ctx *proc.Context, workerCount int, conf worker.WorkerConfig
 		// BoundWorkers is a fixed list of workers bound to a root process.
 		// It is used for ZTests and simple clusters without a recruiter.
 		workers := strings.Split(conf.BoundWorkers, ",")
-		if workerCount > len(workers) {
-			return nil, fmt.Errorf("requested parallelism %d is greater than the number of workers %d",
-				workerCount, len(workers))
-		}
 		for _, w := range workers {
 			if _, _, err := net.SplitHostPort(w); err != nil {
 				return nil, err
@@ -40,21 +36,10 @@ func RecruitWorkers(ctx *proc.Context, workerCount int, conf worker.WorkerConfig
 		return nil, fmt.Errorf("flag -worker.recruiter does not have host:port")
 	}
 	conn := client.NewConnectionTo("http://" + conf.Recruiter)
-	recreq := api.RecruitRequest{NumberRequested: workerCount}
+	recreq := api.RecruitRequest{NumberRequested: workerCount, Label: conf.Host}
 	resp, err := conn.Recruit(ctx, recreq)
 	if err != nil {
 		return nil, fmt.Errorf("error on recruit for recruiter at %s : %v", conf.Recruiter, err)
-	}
-	if workerCount > len(resp.Workers) {
-		// This error currently causes an abort, but it will be fixed
-		// with issue #1839 which will allow the flowgraph (AST) to be used with
-		// less than the requested number of workers. -MTW
-		err := fmt.Errorf("requested workers %d greater than available workers %d",
-			workerCount, len(resp.Workers))
-		if !conf.Fallback {
-			return nil, err
-		}
-		logger.Warn("Worker fallback", zap.Error(err))
 	}
 	var workers []string
 	for _, w := range resp.Workers {
