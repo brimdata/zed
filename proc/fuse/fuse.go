64f7d41b8b4172eeeffba37241e961073ae5400a
Noah Treuhaft <noah.treuhaft@gmail.com>
2020-12-29 16:38:47 -0800

    Extract Fuser from proc/fuse.Proc (#1880)

--- proc/fuse/fuse.go
+++ proc/fuse/fuse.go
@@ -1,38 +1,30 @@
 package fuse
 
 import (
-	"errors"
 	"sync"
 
 	"github.com/brimsec/zq/proc"
-	"github.com/brimsec/zq/proc/spill"
 	"github.com/brimsec/zq/zbuf"
-	"github.com/brimsec/zq/zcode"
 	"github.com/brimsec/zq/zng"
-	"github.com/brimsec/zq/zng/resolver"
 )
 
 var MemMaxBytes = 128 * 1024 * 1024
 var BatchSize = 100
 
 type Proc struct {
-	pctx     *proc.Context
-	parent   proc.Interface
-	spiller  *spill.File
-	slotByID [][]int
-	slots    []slot
-	uberType *zng.TypeRecord
+	pctx   *proc.Context
+	parent proc.Interface
+
+	fuser    *Fuser
 	once     sync.Once
 	resultCh chan proc.Result
-	nbytes   int
-	recs     []*zng.Record
-	types    resolver.Slice
 }
 
 func New(pctx *proc.Context, parent proc.Interface) (*Proc, error) {
 	return &Proc{
 		pctx:     pctx,
 		parent:   parent,
+		fuser:    NewFuser(pctx.TypeContext, MemMaxBytes),
 		resultCh: make(chan proc.Result),
 	}, nil
 }
@@ -63,7 +55,7 @@ func (p *Proc) pullInput() error {
 			return err
 		}
 		if batch == nil {
-			return p.finish()
+			return nil
 		}
 		if err := p.writeBatch(batch); err != nil {
 			return err
@@ -76,63 +68,19 @@ func (p *Proc) writeBatch(batch zbuf.Batch) error {
 	l := batch.Length()
 	for i := 0; i < l; i++ {
 		rec := batch.Index(i)
-		id := rec.Type.ID()
-		typ := p.types.Lookup(id)
-		if typ == nil {
-			p.types.Enter(id, rec.Type)
-		}
-		if p.spiller != nil {
-			if err := p.spiller.Write(rec); err != nil {
-				return err
-			}
-		} else {
-			if err := p.stash(rec); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func (p *Proc) stash(rec *zng.Record) error {
-	p.nbytes += len(rec.Raw)
-	if p.nbytes >= MemMaxBytes {
-		var err error
-		p.spiller, err = spill.NewTempFile()
-		if err != nil {
+		if err := p.fuser.Write(rec); err != nil {
 			return err
 		}
-		if err := p.spiller.Write(rec); err != nil {
-			return err
-		}
-		for _, rec := range p.recs {
-			if err := p.spiller.Write(rec); err != nil {
-				return err
-			}
-		}
-		p.recs = nil
-		return nil
 	}
-	rec = rec.Keep()
-	p.recs = append(p.recs, rec)
 	return nil
 }
 
 func (p *Proc) pushOutput() error {
-	var reader zbuf.Reader
-	if p.spiller != nil {
-		if err := p.spiller.Rewind(p.pctx.TypeContext); err != nil {
-			return err
-		}
-		reader = p.spiller
-	} else {
-		reader = zbuf.Array(p.recs).NewReader()
-	}
 	for {
 		if err := p.pctx.Err(); err != nil {
 			return err
 		}
-		batch, err := p.nextBatch(reader)
+		batch, err := p.nextBatch()
 		if err != nil || batch == nil {
 			return err
 		}
@@ -148,8 +96,8 @@ func (p *Proc) sendResult(b zbuf.Batch, err error) {
 }
 
 func (p *Proc) shutdown(err error) {
-	if p.spiller != nil {
-		p.spiller.CloseAndRemove()
+	if err2 := p.fuser.Close(); err == nil {
+		err = err2
 	}
 	p.sendResult(nil, err)
 	close(p.resultCh)
@@ -159,77 +107,20 @@ func (p *Proc) Done() {
 	p.parent.Done()
 }
 
-type slot struct {
-	zv        zcode.Bytes
-	container bool
-}
-
-func (p *Proc) finish() error {
-	uber := newSchema()
-	// slotByID provides a map from a type ID to a slice of integers
-	// that represent the column position in the uber schema for each column
-	// of the input record type.
-	p.slotByID = make([][]int, len(p.types))
-	for _, typ := range p.types {
-		if typ != nil {
-			id := typ.ID()
-			p.slotByID[id] = uber.mixin(zng.AliasedType(typ).(*zng.TypeRecord))
-		}
-	}
-	var err error
-	p.uberType, err = p.pctx.TypeContext.LookupTypeRecord(uber.columns)
-	if err != nil {
-		return err
-	}
-	p.slots = make([]slot, len(p.uberType.Columns))
-	for k := range p.slots {
-		p.slots[k].container = zng.IsContainerType(p.uberType.Columns[k].Type)
-	}
-	return nil
-}
-
-func (p *Proc) nextBatch(reader zbuf.Reader) (zbuf.Batch, error) {
+func (p *Proc) nextBatch() (zbuf.Batch, error) {
 	var out []*zng.Record
 	for len(out) < BatchSize {
-		rec, err := reader.Read()
+		rec, err := p.fuser.Read()
 		if err != nil {
 			return nil, err
 		}
 		if rec == nil {
 			break
 		}
-		for k := range p.slots {
-			p.slots[k].zv = nil
-		}
-		slotList := p.slotByID[rec.Type.ID()]
-		it := rec.Raw.Iter()
-		for _, slot := range slotList {
-			zv, _, err := it.Next()
-			if err != nil {
-				return nil, err
-			}
-			p.slots[slot].zv = zv
-		}
-		if !it.Done() {
-			return nil, errors.New("column mismatch in fuse processor")
-		}
-		uberRec := splice(p.uberType, p.slots)
-		out = append(out, uberRec)
+		out = append(out, rec)
 	}
 	if out == nil {
 		return nil, nil
 	}
 	return zbuf.Array(out), nil
-}
-
-func splice(typ *zng.TypeRecord, slots []slot) *zng.Record {
-	var out zcode.Bytes
-	for _, s := range slots {
-		if s.container {
-			out = zcode.AppendContainer(out, s.zv)
-		} else {
-			out = zcode.AppendPrimitive(out, s.zv)
-		}
-	}
-	return zng.NewRecord(typ, out)
 }
---
+++ proc/fuse/fuser.go
@@ -0,0 +1,172 @@
+package fuse
+
+import (
+	"errors"
+
+	"github.com/brimsec/zq/proc/spill"
+	"github.com/brimsec/zq/zcode"
+	"github.com/brimsec/zq/zng"
+	"github.com/brimsec/zq/zng/resolver"
+)
+
+// Fuser buffers records written to it, assembling from them a unified schema of
+// fields and types.  Fuser then transforms those records to the unified schema
+// as they are read back from it.
+type Fuser struct {
+	zctx        *resolver.Context
+	memMaxBytes int
+
+	nbytes   int
+	recs     []*zng.Record
+	slotByID [][]int
+	slots    []slot
+	spiller  *spill.File
+	types    resolver.Slice
+	uberType *zng.TypeRecord
+}
+
+type slot struct {
+	zv        zcode.Bytes
+	container bool
+}
+
+// NewFuser returns a new Fuser.  The Fuser buffers records in memory until
+// their cumulative size (measured in zcode.Bytes length) exceeds memMaxBytes,
+// at which point it buffers them in a temporary file.
+func NewFuser(zctx *resolver.Context, memMaxBytes int) *Fuser {
+	return &Fuser{zctx: zctx, memMaxBytes: memMaxBytes}
+}
+
+// Close removes the receiver's temporary file if it created one.
+func (f *Fuser) Close() error {
+	if f.spiller != nil {
+		return f.spiller.CloseAndRemove()
+	}
+	return nil
+}
+
+// Write buffers rec. If called after Read, Write panics.
+func (f *Fuser) Write(rec *zng.Record) error {
+	if f.finished() {
+		panic("fuser: write after read")
+	}
+	id := rec.Type.ID()
+	typ := f.types.Lookup(id)
+	if typ == nil {
+		f.types.Enter(id, rec.Type)
+	}
+	if f.spiller != nil {
+		return f.spiller.Write(rec)
+	}
+	return f.stash(rec)
+}
+
+func (f *Fuser) stash(rec *zng.Record) error {
+	f.nbytes += len(rec.Raw)
+	if f.nbytes >= f.memMaxBytes {
+		var err error
+		f.spiller, err = spill.NewTempFile()
+		if err != nil {
+			return err
+		}
+		if err := f.spiller.Write(rec); err != nil {
+			return err
+		}
+		for _, rec := range f.recs {
+			if err := f.spiller.Write(rec); err != nil {
+				return err
+			}
+		}
+		f.recs = nil
+		return nil
+	}
+	rec = rec.Keep()
+	f.recs = append(f.recs, rec)
+	return nil
+}
+
+func (f *Fuser) finished() bool {
+	return f.slotByID != nil
+}
+
+func (f *Fuser) finish() error {
+	uber := newSchema()
+	// slotByID provides a map from a type ID to a slice of integers
+	// that represent the column position in the uber schema for each column
+	// of the input record type.
+	f.slotByID = make([][]int, len(f.types))
+	for _, typ := range f.types {
+		if typ != nil {
+			id := typ.ID()
+			f.slotByID[id] = uber.mixin(zng.AliasedType(typ).(*zng.TypeRecord))
+		}
+	}
+	var err error
+	f.uberType, err = f.zctx.LookupTypeRecord(uber.columns)
+	if err != nil {
+		return err
+	}
+	f.slots = make([]slot, len(f.uberType.Columns))
+	for k := range f.slots {
+		f.slots[k].container = zng.IsContainerType(f.uberType.Columns[k].Type)
+	}
+	if f.spiller != nil {
+		return f.spiller.Rewind(f.zctx)
+	}
+	return nil
+}
+
+// Read returns the next buffered record after transforming it to the unified
+// schema.
+func (f *Fuser) Read() (*zng.Record, error) {
+	if !f.finished() {
+		if err := f.finish(); err != nil {
+			return nil, err
+		}
+	}
+	rec, err := f.next()
+	if rec == nil || err != nil {
+		return nil, err
+	}
+	for k := range f.slots {
+		f.slots[k].zv = nil
+	}
+	slotList := f.slotByID[rec.Type.ID()]
+	it := rec.Raw.Iter()
+	for _, slot := range slotList {
+		zv, _, err := it.Next()
+		if err != nil {
+			return nil, err
+		}
+		f.slots[slot].zv = zv
+	}
+	if !it.Done() {
+		return nil, errors.New("column mismatch in fuse processor")
+	}
+	return splice(f.uberType, f.slots), nil
+}
+
+func (f *Fuser) next() (*zng.Record, error) {
+	if f.spiller != nil {
+		return f.spiller.Read()
+	}
+	var rec *zng.Record
+	if len(f.recs) > 0 {
+		rec = f.recs[0]
+		f.recs = f.recs[1:]
+	}
+	return rec, nil
+
+}
+
+func splice(typ *zng.TypeRecord, slots []slot) *zng.Record {
+	var out zcode.Bytes
+	for _, s := range slots {
+		if s.container {
+			out = zcode.AppendContainer(out, s.zv)
+		} else {
+			out = zcode.AppendPrimitive(out, s.zv)
+		}
+	}
+	return zng.NewRecord(typ, out)
+}
