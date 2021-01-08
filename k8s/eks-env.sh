6747378643b24f1ef0861caccdb88b5ba6a57ea7
Matthew Nibecker <hello@mattnibecker.com>
2021-01-08 10:20:31 -0500

    nwt simplifies things (again)

--- k8s/eks-env.sh
+++ k8s/eks-env.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-if ! command -v jq &> /dev/null
-then
-    echo "jq could not be found"
-    exit
-fi
+account=$(aws sts get-caller-identity --output text --query Account)
+arn=$(aws sts get-caller-identity --output text --query Arn)
+user=${arn#*/}
 
-if ! command -v yq &> /dev/null
-then
-    echo "yq could not be found"
-    exit
-fi
-
-id=$(aws sts get-caller-identity)
-userarn=$(echo ${id} | jq '.Arn')
-accountid=$(echo ${id} | jq --raw-output '.Account')
-username=$(kubectl get -o json -n kube-system configmap aws-auth \
-	| jq --raw-output '.data.mapUsers' \
-	| yq eval ".[] | select(.userarn == $userarn) | .username" -)
-
-export ZQD_ECR_HOST=${accountid}.dkr.ecr.us-east-2.amazonaws.com
-export ZQD_DATA_URI=s3://zqd-demo-1/${username}/zqd-meta
-export ZQD_K8S_USER=$username
+export ZQD_ECR_HOST=${account}.dkr.ecr.us-east-2.amazonaws.com
+export ZQD_DATA_URI=s3://zqd-demo-1/${user}/zqd-meta
+export ZQD_K8S_USER=$user
 export ZQD_TEST_CLUSTER=zq-test.us-east-2.eksctl.io
