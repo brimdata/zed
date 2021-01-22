52c926a9b0f00666a0a3c3a99928a62b446cec97
Mark Wallace <mark@Marks-MBP.lan>
2021-01-22 10:21:03 -0800

    test github action

--- k8s/zqd-port.sh
+++ k8s/zqd-port.sh
@@ -5,6 +5,17 @@ kill $(ps aux | grep '[p]ort-forward' | awk '{print $2}')
 sleep 1 # wait a second for the processes to get the signal
 kubectl port-forward svc/zsrv-recruiter 8020:9867 &
 kubectl port-forward svc/zsrv-root 9867:9867 &
-sleep 2 # wait longer for port-forwards to complete
+for i in {1..5} ; do
+  if nc -z localhost 8020 </dev/null ; then
+    break
+  fi
+  sleep 1
+done
+for i in {1..5} ; do
+  if nc -z localhost 9867 </dev/null ; then
+    break
+  fi
+  sleep 1
+done
 curl http://localhost:8020/recruiter/listfree
 zapi ls 
