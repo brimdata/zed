943813c264fdbf2200d1da4a8c5540f657208df9
Matthew Nibecker <hello@mattnibecker.com>
2021-02-03 11:17:10 -0500

    redis helm
    Add redis to umbrella zservice helm chart.
    
    Closes #1899

--- ppl/zqd/redis.go
+++ ppl/zqd/redis.go
@@ -2,27 +2,41 @@ package zqd
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"strings"
 
 	"github.com/go-redis/redis/v8"
+	"go.uber.org/zap"
 )
 
-func NewRedisClient(ctx context.Context, conf RedisConfig) (*redis.Client, error) {
+func NewRedisClient(ctx context.Context, logger *zap.Logger, conf RedisConfig) (*redis.Client, error) {
 	if !conf.Enabled {
 		return nil, nil
 	}
+
+	logger = logger.Named("redis")
 	client := redis.NewClient(&conf.Options)
-	if cmd := client.Ping(ctx); cmd.Err() != nil {
-		return nil, cmd.Err()
+
+	if err := client.Ping(ctx).Err(); err != nil {
+		logger.Error("Could not connect to server",
+			zap.String("url", conf.StringRedacted()),
+			zap.Error(err),
+		)
+		return nil, err
 	}
+
+	logger.Info("Connected", zap.String("url", conf.StringRedacted()))
 	return client, nil
 }
 
 type RedisConfig struct {
 	redis.Options
 
-	Enabled bool
+	Enabled      bool
+	PasswordFile string
 }
 
 func (c *RedisConfig) SetFlags(fs *flag.FlagSet) {
@@ -31,7 +45,22 @@ func (c *RedisConfig) SetFlags(fs *flag.FlagSet) {
 	fs.StringVar(&c.Addr, "redis.addr", "localhost:6379", "redis address")
 	fs.StringVar(&c.Username, "redis.user", "", "redis username")
 	fs.StringVar(&c.Password, "redis.password", "", "redis password")
+	fs.StringVar(&c.PasswordFile, "redis.passwordFile", "", "path to file containing redis password")
 	fs.IntVar(&c.DB, "redis.database", 0, "redis database number")
+}
+
+func (c *RedisConfig) Init() error {
+	if c.Password != "" && c.PasswordFile != "" {
+		return errors.New("redis.password and redis.passwordFile cannot both be set")
+	}
+	if c.PasswordFile != "" {
+		b, err := ioutil.ReadFile(c.PasswordFile)
+		if err != nil {
+			return nil
+		}
+		c.Password = string(b)
+	}
+	return nil
 }
 
 func (c *RedisConfig) Set(s string) error {
@@ -59,6 +88,14 @@ func (c RedisConfig) String() string {
 	str += c.Addr
 	str += fmt.Sprintf("/%d", c.DB)
 	return str
+}
+
+// StringRedacted is the same as string except password and a username are
+// redacted. This should be used in logs.
+func (c RedisConfig) StringRedacted() string {
+	c.Password = strings.Repeat("*", 5)
+	c.Username = strings.Repeat("*", 5)
+	return c.String()
 }
 
 func (c RedisConfig) IsEmpty() bool {
