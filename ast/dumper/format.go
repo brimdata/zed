ba9be397a21a9e3c09829ad3db8e033aa7c07065
Steven McCanne <steve@mccanne.com>
2021-02-16 00:14:42 -0800

    clean up dumper

--- ast/dumper/format.go
+++ ast/dumper/format.go
@@ -58,20 +58,30 @@ func (f *formatter) space() {
 }
 
 func (f *formatter) assignments(assignments []ast.Assignment) {
+	first := true
 	for _, a := range assignments {
+		if !first {
+			f.ret()
+		} else {
+			first = false
+		}
 		f.open("= ")
 		f.expr(a.LHS)
 		f.write(" ")
 		f.expr(a.RHS)
 		f.close()
-		f.ret()
 	}
 }
 
 func (f *formatter) exprs(exprs []ast.Expression) {
+	first := true
 	for _, e := range exprs {
+		if first {
+			first = false
+		} else {
+			f.space()
+		}
 		f.expr(e)
-		f.space()
 	}
 }
 
@@ -105,7 +115,7 @@ func (f *formatter) expr(e ast.Expression) {
 		f.write(e.Name)
 		f.close()
 	case *ast.RootRecord:
-		f.write(".")
+		f.write("$")
 	case *ast.UnaryExpression:
 		f.open(e.Operator)
 		f.write(" ")
@@ -154,7 +164,13 @@ func (f *formatter) expr(e ast.Expression) {
 //		EmitPart     bool         `json:"emit_part,omitempty"`
 
 func (f *formatter) procs(procs []ast.Proc) {
+	first := true
 	for _, p := range procs {
+		if first {
+			first = false
+		} else {
+			f.ret()
+		}
 		f.proc(p)
 	}
 }
@@ -166,13 +182,11 @@ func (f *formatter) proc(p ast.Proc) {
 		f.ret()
 		f.procs(p.Procs)
 		f.close()
-		f.ret()
 	case *ast.ParallelProc:
 		f.open("par")
 		f.ret()
 		f.procs(p.Procs)
 		f.close()
-		f.ret()
 	case *ast.GroupByProc:
 		f.open("groupby dur=%d dir=%d limit=%d", p.Duration, p.InputSortDir, p.Limit)
 		if p.ConsumePart {
@@ -189,96 +203,83 @@ func (f *formatter) proc(p ast.Proc) {
 		f.assignments(p.Reducers)
 		f.close()
 		f.close()
-		f.ret()
 	case *ast.CutProc:
 		f.open("cut")
+		f.ret()
 		f.assignments(p.Fields)
 		f.close()
-		f.ret()
 	case *ast.PickProc:
 		f.open("pick")
+		f.ret()
 		f.assignments(p.Fields)
 		f.close()
-		f.ret()
 	case *ast.DropProc:
 		f.open("drop")
+		f.ret()
 		f.exprs(p.Fields)
 		f.close()
-		f.ret()
 	case *ast.SortProc:
 		f.open(fmt.Sprintf("sort dir=%d nf=%t ", p.SortDir, p.NullsFirst))
+		f.ret()
 		f.exprs(p.Fields)
 		f.close()
-		f.ret()
 	case *ast.HeadProc:
 		f.open("head %d", p.Count)
 		f.close()
-		f.ret()
 	case *ast.TailProc:
 		f.open("tail %d", p.Count)
 		f.close()
-		f.ret()
 	case *ast.UniqProc:
 		f.open("uniq")
 		if p.Cflag {
 			f.write(" -c")
 		}
 		f.close()
-		f.ret()
 	case *ast.PassProc:
 		f.open("pass")
-		f.ret()
 	case *ast.FilterProc:
 		f.open("filter ")
 		f.expr(p.Filter)
 		f.close()
-		f.ret()
 	case *ast.TopProc:
 		f.open("top limit=%d flush=%t ", p.Limit, p.Flush)
 		f.ret()
 		f.exprs(p.Fields)
 		f.close()
-		f.ret()
 	case *ast.PutProc:
 		f.open("put ")
 		f.ret()
 		f.assignments(p.Clauses)
 		f.close()
-		f.ret()
 	case *ast.RenameProc:
 		f.open("put")
 		f.ret()
 		f.assignments(p.Fields)
 		f.close()
-		f.ret()
 	case *ast.FuseProc:
 		f.open("fuse")
 		f.close()
-		f.ret()
 	case *ast.FunctionCall:
 		f.open(p.Function)
 		f.space()
 		f.exprs(p.Args)
 		f.close()
-		f.ret()
 	case *ast.JoinProc:
 		f.open("join on ")
 		f.expr(p.LeftKey)
 		f.write(" = ")
 		f.expr(p.RightKey)
 		f.ret()
-		f.open("cut")
+		f.open("join-cut ")
 		f.assignments(p.Clauses)
 		f.close()
-		f.ret()
+		f.close()
 	case *ast.SqlExpression:
 		//XXX TBD
 		f.open("sql")
 		f.close()
-		f.ret()
 	default:
 		f.open("unknown proc: %T", p)
 		f.close()
-		f.ret()
 	}
 }
