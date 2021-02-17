88f98d7e011911fb82f6e485c742383b3bdc73fc
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-02-17 12:20:18 -0800

    Add some missing outputflags.Flags initialization (#2149)

--- ppl/cmd/zar/find/command.go
+++ ppl/cmd/zar/find/command.go
@@ -87,7 +87,7 @@ func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 
 func (c *Command) Run(args []string) error {
 	defer c.Cleanup()
-	if err := c.Init(); err != nil {
+	if err := c.Init(&c.outputFlags); err != nil {
 		return err
 	}
 
