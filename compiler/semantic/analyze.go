bf7a9d1e64623e91fd6e9d3c90a15977cc8705d8
Steven McCanne <steve@mccanne.com>
2021-02-18 15:30:16 -0800

    wip

---
+++ compiler/semantic/analyze.go
@@ -0,0 +1,15 @@
+package semantic
+
+import (
+	"github.com/brimsec/zq/compiler/ast"
+	"github.com/brimsec/zq/compiler/kernel"
+)
+
+func Analyze(program *ast.Program) (kernel.Program, error) {
+	//XXX TBD
+	return nil, nil
+}
+
+//func swing(a) {
+//
+//}
