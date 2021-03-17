4ed06ae99b8b712386c2ba5780f8fed7bf18e26b
Steven McCanne <steve@mccanne.com>
2021-03-16 18:42:02 -0700

    address PR feedback

--- zng/split.go
+++ zng/split.go
@@ -17,7 +17,7 @@ func Split(elemType Type, b zcode.Bytes) ([]Value, error) {
 	for it := b.Iter(); !it.Done(); {
 		zv, _, err := it.Next()
 		if err != nil {
-			return nil, fmt.Errorf("parsing element type '%s' value %q: %w", elemType.ZSON(), zv, err)
+			return nil, fmt.Errorf("parsing element type '%s' value %q: %w", elemType.ZSON(), b, err)
 		}
 		vals = append(vals, Value{elemType, zv})
 	}
