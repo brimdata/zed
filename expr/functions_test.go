f9dfe7b045442d75b7f771a3b6bcd6561691fc5d
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-03-26 18:01:59 -0700

    proc: warn about Function.Apply errors in applier.Pull
    proc.applier.Pull returns errors from proc.Function.Apply, effectively
    making them fatal when they needn't be.  Send warnings for these errors
    instead of returning them.

--- expr/functions_test.go
+++ expr/functions_test.go
@@ -10,13 +10,17 @@ import (
 	"github.com/brimsec/zq/zng"
 )
 
+func namedErrBadArgument(name string) error {
+	return fmt.Errorf("%s: %w", name, function.ErrBadArgument)
+}
+
 func zaddr(addr string) zng.Value {
 	parsed := net.ParseIP(addr)
 	return zng.Value{zng.TypeIP, zng.EncodeIP(parsed)}
 }
 
 func TestBadFunction(t *testing.T) {
-	testError(t, "notafunction()", "", function.ErrNoSuchFunction, "calling nonexistent function")
+	testError(t, "notafunction()", function.ErrNoSuchFunction, "calling nonexistent function")
 }
 
 func TestAbs(t *testing.T) {
@@ -30,9 +34,9 @@ func TestAbs(t *testing.T) {
 	testSuccessful(t, "abs(3.2)", record, zfloat64(3.2))
 	testSuccessful(t, "abs(u)", record, zuint64(50))
 
-	testError(t, "abs()", record, function.ErrTooFewArgs, "abs with no args")
-	testError(t, "abs(1, 2)", record, function.ErrTooManyArgs, "abs with too many args")
-	testError(t, `abs("hello")`, record, function.ErrBadArgument, "abs with non-number")
+	testError(t, "abs()", function.ErrTooFewArgs, "abs with no args")
+	testError(t, "abs(1, 2)", function.ErrTooManyArgs, "abs with too many args")
+	testWarning(t, `abs("hello")`, record, namedErrBadArgument("abs"), "abs with non-number")
 }
 
 func TestSqrt(t *testing.T) {
@@ -44,9 +48,9 @@ func TestSqrt(t *testing.T) {
 	testSuccessful(t, "sqrt(f)", record, zfloat64(2.5))
 	testSuccessful(t, "sqrt(i)", record, zfloat64(3.0))
 
-	testError(t, "sqrt()", record, function.ErrTooFewArgs, "sqrt with no args")
-	testError(t, "sqrt(1, 2)", record, function.ErrTooManyArgs, "sqrt with too many args")
-	testError(t, "sqrt(-1)", record, function.ErrBadArgument, "sqrt of negative")
+	testError(t, "sqrt()", function.ErrTooFewArgs, "sqrt with no args")
+	testError(t, "sqrt(1, 2)", function.ErrTooManyArgs, "sqrt with too many args")
+	testWarning(t, "sqrt(-1)", record, namedErrBadArgument("sqrt"), "sqrt of negative")
 }
 
 func TestMinMax(t *testing.T) {
@@ -63,8 +67,8 @@ func TestMinMax(t *testing.T) {
 	testSuccessful(t, "max(3, 2, 1)", record, zint64(3))
 
 	// Fails with no arguments
-	testError(t, "min()", record, function.ErrTooFewArgs, "min with no args")
-	testError(t, "max()", record, function.ErrTooFewArgs, "max with no args")
+	testError(t, "min()", function.ErrTooFewArgs, "min with no args")
+	testError(t, "max()", function.ErrTooFewArgs, "max with no args")
 
 	// Mixed types work
 	testSuccessful(t, "min(i, 2, 3)", record, zuint64(1))
@@ -77,10 +81,10 @@ func TestMinMax(t *testing.T) {
 	testSuccessful(t, "max(2.0, -1)", record, zfloat64(2))
 
 	// Fails on invalid types
-	testError(t, `min("hello", 2)`, record, function.ErrBadArgument, "min() on string")
-	testError(t, `max("hello", 2)`, record, function.ErrBadArgument, "max() on string")
-	testError(t, `min(1.2.3.4, 2)`, record, function.ErrBadArgument, "min() on ip")
-	testError(t, `max(1.2.3.4, 2)`, record, function.ErrBadArgument, "max() on ip")
+	testWarning(t, `min("hello", 2)`, record, function.ErrBadArgument, "min() on string")
+	testWarning(t, `max("hello", 2)`, record, function.ErrBadArgument, "max() on string")
+	testWarning(t, `min(1.2.3.4, 2)`, record, function.ErrBadArgument, "min() on ip")
+	testWarning(t, `max(1.2.3.4, 2)`, record, function.ErrBadArgument, "max() on ip")
 
 }
 
@@ -93,12 +97,12 @@ func TestCeilFloorRound(t *testing.T) {
 	testSuccessful(t, "floor(5)", "", zint64(5))
 	testSuccessful(t, "round(5)", "", zint64(5))
 
-	testError(t, "ceil()", "", function.ErrTooFewArgs, "ceil() with no args")
-	testError(t, "ceil(1, 2)", "", function.ErrTooManyArgs, "ceil() with too many args")
-	testError(t, "floor()", "", function.ErrTooFewArgs, "floor() with no args")
-	testError(t, "floor(1, 2)", "", function.ErrTooManyArgs, "floor() with too many args")
-	testError(t, "round()", "", function.ErrTooFewArgs, "round() with no args")
-	testError(t, "round(1, 2)", "", function.ErrTooManyArgs, "round() with too many args")
+	testError(t, "ceil()", function.ErrTooFewArgs, "ceil() with no args")
+	testError(t, "ceil(1, 2)", function.ErrTooManyArgs, "ceil() with too many args")
+	testError(t, "floor()", function.ErrTooFewArgs, "floor() with no args")
+	testError(t, "floor(1, 2)", function.ErrTooManyArgs, "floor() with too many args")
+	testError(t, "round()", function.ErrTooFewArgs, "round() with no args")
+	testError(t, "round(1, 2)", function.ErrTooManyArgs, "round() with too many args")
 }
 
 func TestLogPow(t *testing.T) {
@@ -111,14 +115,14 @@ func TestLogPow(t *testing.T) {
 	testSuccessful(t, "pow(10, 2)", "", zfloat64(100))
 	testSuccessful(t, "pow(4.0, 1.5)", "", zfloat64(8))
 
-	testError(t, "log()", "", function.ErrTooFewArgs, "log() with no args")
-	testError(t, "log(2, 3)", "", function.ErrTooManyArgs, "log() with too many args")
-	testError(t, "log(0)", "", function.ErrBadArgument, "log() of 0")
-	testError(t, "log(-1)", "", function.ErrBadArgument, "log() of negative number")
+	testError(t, "log()", function.ErrTooFewArgs, "log() with no args")
+	testError(t, "log(2, 3)", function.ErrTooManyArgs, "log() with too many args")
+	testWarning(t, "log(0)", "", namedErrBadArgument("log"), "log() of 0")
+	testWarning(t, "log(-1)", "", namedErrBadArgument("log"), "log() of negative number")
 
-	testError(t, "pow()", "", function.ErrTooFewArgs, "pow() with no args")
-	testError(t, "pow(2, 3, r)", "", function.ErrTooManyArgs, "pow() with too many args")
-	testError(t, "pow(-1, 0.5)", "", function.ErrBadArgument, "pow() with invalid arguments")
+	testError(t, "pow()", function.ErrTooFewArgs, "pow() with no args")
+	testError(t, "pow(2, 3, r)", function.ErrTooManyArgs, "pow() with too many args")
+	testWarning(t, "pow(-1, 0.5)", "", namedErrBadArgument("pow"), "pow() with invalid arguments")
 }
 
 func TestMod(t *testing.T) {
@@ -131,28 +135,28 @@ func TestMod(t *testing.T) {
 	testSuccessful(t, "mod(8, 5)", record, zint64(3))
 	testSuccessful(t, "mod(8, u)", record, zint64(3))
 
-	testError(t, "mod()", record, function.ErrTooFewArgs, "mod() with no args")
-	testError(t, "mod(1, 2, 3)", record, function.ErrTooManyArgs, "mod() with too many args")
-	testError(t, "mod(3.2, 2)", record, function.ErrBadArgument, "mod() with float64 arg")
+	testError(t, "mod()", function.ErrTooFewArgs, "mod() with no args")
+	testError(t, "mod(1, 2, 3)", function.ErrTooManyArgs, "mod() with too many args")
+	testWarning(t, "mod(3.2, 2)", record, namedErrBadArgument("mod"), "mod() with float64 arg")
 }
 
 func TestOtherStrFuncs(t *testing.T) {
 	testSuccessful(t, `replace("bann", "n", "na")`, "", zstring("banana"))
-	testError(t, `replace("foo", "bar")`, "", function.ErrTooFewArgs, "replace() with too few args")
-	testError(t, `replace("foo", "bar", "baz", "blort")`, "", function.ErrTooManyArgs, "replace() with too many args")
-	testError(t, `replace("foo", "o", 5)`, "", function.ErrBadArgument, "replace() with non-string arg")
+	testError(t, `replace("foo", "bar")`, function.ErrTooFewArgs, "replace() with too few args")
+	testError(t, `replace("foo", "bar", "baz", "blort")`, function.ErrTooManyArgs, "replace() with too many args")
+	testWarning(t, `replace("foo", "o", 5)`, "", namedErrBadArgument("replace"), "replace() with non-string arg")
 
 	testSuccessful(t, `to_lower("BOO")`, "", zstring("boo"))
-	testError(t, `to_lower()`, "", function.ErrTooFewArgs, "toLower() with no args")
-	testError(t, `to_lower("BOO", "HOO")`, "", function.ErrTooManyArgs, "toLower() with too many args")
+	testError(t, `to_lower()`, function.ErrTooFewArgs, "toLower() with no args")
+	testError(t, `to_lower("BOO", "HOO")`, function.ErrTooManyArgs, "toLower() with too many args")
 
 	testSuccessful(t, `to_upper("boo")`, "", zstring("BOO"))
-	testError(t, `to_upper()`, "", function.ErrTooFewArgs, "toUpper() with no args")
-	testError(t, `to_upper("boo", "hoo")`, "", function.ErrTooManyArgs, "toUpper() with too many args")
+	testError(t, `to_upper()`, function.ErrTooFewArgs, "toUpper() with no args")
+	testError(t, `to_upper("boo", "hoo")`, function.ErrTooManyArgs, "toUpper() with too many args")
 
 	testSuccessful(t, `trim("  hi  there   ")`, "", zstring("hi  there"))
-	testError(t, `trim()`, "", function.ErrTooFewArgs, "trim() with no args")
-	testError(t, `trim("  hi  ", "  there  ")`, "", function.ErrTooManyArgs, "trim() with too many args")
+	testError(t, `trim()`, function.ErrTooFewArgs, "trim() with no args")
+	testError(t, `trim("  hi  ", "  there  ")`, function.ErrTooManyArgs, "trim() with too many args")
 }
 
 func TestLen(t *testing.T) {
@@ -163,9 +167,9 @@ func TestLen(t *testing.T) {
 	testSuccessful(t, "len(s)", record, zint64(3))
 	testSuccessful(t, "len(a)", record, zint64(3))
 
-	testError(t, "len()", record, function.ErrTooFewArgs, "len() with no args")
-	testError(t, `len("foo", "bar")`, record, function.ErrTooManyArgs, "len() with too many args")
-	testError(t, "len(5)", record, function.ErrBadArgument, "len() with non-container arg")
+	testError(t, "len()", function.ErrTooFewArgs, "len() with no args")
+	testError(t, `len("foo", "bar")`, function.ErrTooManyArgs, "len() with too many args")
+	testWarning(t, "len(5)", record, namedErrBadArgument("len"), "len() with non-container arg")
 
 	record = `
 #0:record[s:string,bs:bstring,bs2:bstring]
@@ -194,6 +198,6 @@ func TestTime(t *testing.T) {
 
 	testSuccessful(t, "trunc(1590506867.967, 1)", "", zng.Value{zng.TypeTime, zng.EncodeTime(nano.Ts(1590506867 * 1_000_000_000))})
 
-	testError(t, "iso()", "", function.ErrTooFewArgs, "iso() with no args")
-	testError(t, `iso("abc", "def")`, "", function.ErrTooManyArgs, "iso() with too many args")
+	testError(t, "iso()", function.ErrTooFewArgs, "iso() with no args")
+	testError(t, `iso("abc", "def")`, function.ErrTooManyArgs, "iso() with too many args")
 }
