54a06dfb4f25b8c0b74a6449162770a434558697
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-04-03 17:48:21 -0700

    move some Go TZNG tests into zio/tzngio (#2480)

--- zio/tzngio/reader_test.go
+++ zio/tzngio/reader_test.go
@@ -2,6 +2,7 @@ package tzngio_test
 
 import (
 	"bytes"
+	"net"
 	"strings"
 	"testing"
 
@@ -45,6 +46,101 @@ func TestCtrl(t *testing.T) {
 	_, body, err = r.ReadPayload()
 	assert.NoError(t, err)
 	assert.Equal(t, "message4", string(body))
+}
+
+func TestDescriptors(t *testing.T) {
+	// Step 1 - Test a simple zng descriptor and corresponding value
+	src := "#port=uint16\n"
+	src += "#1:record[s:string,n:int32]\n"
+	src += "1:[foo;5;]\n"
+	// Step 2 - Create a second descriptor of a different type
+	src += "#2:record[a:ip,p:port]\n"
+	src += "2:[10.5.5.5;443;]\n"
+	// Step 3 - can still use the first descriptor
+	src += "1:[bar;100;]\n"
+	// Step 4 - Test that referencing an invalid descriptor is an error.
+	src += "100:[something;somethingelse;]\n"
+
+	r := tzngio.NewReader(strings.NewReader(src), resolver.NewContext())
+
+	// Check Step 1
+	record, err := r.Read()
+	require.NoError(t, err)
+	s, err := record.AccessString("s")
+	require.NoError(t, err)
+	assert.Equal(t, "foo", s, "Parsed string value properly")
+	n, err := record.AccessInt("n")
+	require.NoError(t, err)
+	assert.Equal(t, 5, int(n), "Parsed int value properly")
+
+	// Check Step 2
+	record, err = r.Read()
+	require.NoError(t, err)
+	a, err := record.AccessIP("a")
+	require.NoError(t, err)
+	expectAddr := net.ParseIP("10.5.5.5").To4()
+	assert.Equal(t, expectAddr, a, "Parsed addr value properly")
+	n, err = record.AccessInt("p")
+	require.NoError(t, err)
+	assert.Equal(t, 443, int(n), "Parsed port value properly")
+
+	// Check Step 3
+	record, err = r.Read()
+	require.NoError(t, err)
+	s, err = record.AccessString("s")
+	require.NoError(t, err)
+	assert.Equal(t, "bar", s, "Parsed another string properly")
+	n, err = record.AccessInt("n")
+	require.NoError(t, err)
+	assert.Equal(t, 100, int(n), "Parsed another int properly")
+
+	// XXX test other types, sets, arrays, etc.
+
+	// Check Step 4 - Test that referencing an invalid descriptor is an error.
+	_, err = r.Read()
+	assert.Error(t, err, "invalid descriptor", "invalid descriptor")
+
+	// Test various malformed zng:
+	def1 := "#1:record[s:string,n:int32]\n"
+	zngs := []string{
+		def1 + "1:string;123;\n",  // missing brackets
+		def1 + "1:[string;123]\n", // missing semicolon
+	}
+
+	for _, z := range zngs {
+		r := tzngio.NewReader(strings.NewReader(z), resolver.NewContext())
+		_, err = r.Read()
+		assert.Error(t, err, "tzng parse error", "invalid tzng")
+	}
+
+	// Descriptor with an invalid type is rejected
+	r = tzngio.NewReader(strings.NewReader("#4:notatype\n"), resolver.NewContext())
+	_, err = r.Read()
+	assert.Error(t, err, "unknown type", "descriptor with invalid type")
+}
+
+func TestSyntax(t *testing.T) {
+	const bad1 = `
+#0:record[_path:string,ts:time,uid:string,resp_ip_bytes:count,tunnel_parents:set[string]]
+0:[conn;1425565514.419939;CogZFI3py5JsFZGik;0;]`
+	r := tzngio.NewReader(strings.NewReader(bad1), resolver.NewContext())
+	_, err := r.Read()
+	require.Error(t, err, "bad1 must have error")
+
+	const bad2 = `
+#0:record[a:string,record[b:string]]
+0:[foo;[bar;]]`
+	r = tzngio.NewReader(strings.NewReader(bad2), resolver.NewContext())
+	_, err = r.Read()
+	require.Error(t, err, "bad2 must have error")
+
+	const bad3 = `
+#0:record[_path:string,ts:time,uid:string,resp_ip_bytes:count,tunnel_parents:set[string]]
+0:[conn;1425565514.419939;CogZFI3py5JsFZGik;0;0;[]]`
+	r = tzngio.NewReader(strings.NewReader(bad3), resolver.NewContext())
+	_, err = r.Read()
+	require.Error(t, err, "bad3 must have error")
+
 }
 
 func TestAlias(t *testing.T) {
