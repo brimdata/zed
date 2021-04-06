9db5e481524045b4f393a60feb52932034d3e3ba
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-04-06 08:24:40 -0700

    remove Kubernetes configuration

--- k8s/aws-credentials.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x #echo on
-kubectl delete secret aws-credentials # delete the old ones
-kubectl create secret generic aws-credentials \
-  --from-literal="aws-region=$(aws configure get region)" \
-  --from-literal="aws-access-key-id=$(aws configure get aws_access_key_id)" \
-  --from-literal="aws-secret-access-key=$(aws configure get aws_secret_access_key)" \
-  --from-literal="aws-session-token=$(aws configure get aws_session_token)"
--- k8s/linkerd-port.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x #echo on
-# start by killing existing dashboard
-kill $(ps aux | grep '[l]inkerd dashboard' | awk '{print $2}')
-sleep 1 # wait a second for the processes to get the signal
-linkerd dashboard &
