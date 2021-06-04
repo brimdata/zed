brimdata/zed

commit 36ac92e22b957e9f4074431d00e0e4bd3c8f014d

Reintroduce zapi command (#2774)
Reintroduce a new version of the zapi command the uses the exact same
Commands that the zed lake command uses. The idea is to have functional
parity between zapi and zed lake.

service/search/search.go
@@ -184,8 +184,8 @@ func (d *searchdriver) end(id int64) error {
 
 func (d *searchdriver) abort(id int64, err error) error {
 	if errors.Is(err, journal.ErrEmpty) {
-		// XXX (nibs) - A search on an empty space should return an error. This
-		// check should be in the driver though.
+		// XXX (nibs) - A search on an empty space should not return an error.
+		// This check should not be in the driver though.
 		return d.end(id)
 	}
 	verr := &api.Error{Type: "INTERNAL", Message: err.Error()}
@@ -220,5 +220,8 @@ func (d *searchdriver) ChannelEnd(cid int) error {
 		ChannelID: cid,
 		Reason:    "eof",
 	}
-	return d.output.SendControl(v)
+	if err := d.output.SendControl(v); err != nil {
+		return err
+	}
+	return d.end(0)
 }

api/client/connection.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"mime"
 	"net/http"
+	"net/url"
 	"path"
 	"strconv"
 	"time"
@@ -189,6 +190,13 @@ func (c *Connection) ZtoAST(ctx context.Context, zprog string) ([]byte, error) {
 	return resp.Body(), nil
 }
 
+func (c *Connection) ScanPools(ctx context.Context) (*ReadCloser, error) {
+	req := c.Request(ctx)
+	req.Method = http.MethodGet
+	req.URL = "/pool"
+	return c.stream(req)
+}
+
 // PoolGet retrieves information about the specified pool.
 func (c *Connection) PoolGet(ctx context.Context, id ksuid.KSUID) (*ReadCloser, error) {
 	req := c.Request(ctx)
@@ -234,13 +242,6 @@ func (c *Connection) PoolPut(ctx context.Context, id ksuid.KSUID, req api.PoolPu
 	return err
 }
 
-func (c *Connection) PoolScan(ctx context.Context) (*ReadCloser, error) {
-	req := c.Request(ctx)
-	req.Method = http.MethodGet
-	req.URL = "/pool"
-	return c.stream(req)
-}
-
 func (c *Connection) PoolDelete(ctx context.Context, id ksuid.KSUID) error {
 	path := path.Join("/pool", id.String())
 	_, err := c.Request(ctx).Delete(path)
@@ -248,6 +249,38 @@ func (c *Connection) PoolDelete(ctx context.Context, id ksuid.KSUID) error {
 		return ErrPoolNotFound
 	}
 	return err
+}
+
+func (c *Connection) ScanStaging(ctx context.Context, pool ksuid.KSUID, tags []ksuid.KSUID) (*ReadCloser, error) {
+	t := make([]string, len(tags))
+	for i, tag := range tags {
+		t[i] = tag.String()
+	}
+	req := c.Request(ctx).
+		SetQueryParamsFromValues(url.Values{"tag": t})
+	req.Method = http.MethodGet
+	req.URL = path.Join("/pool", pool.String(), "staging")
+	return c.stream(req)
+}
+
+func (c *Connection) ScanSegments(ctx context.Context, pool ksuid.KSUID, at string, partitions bool) (*ReadCloser, error) {
+	req := c.Request(ctx)
+	if at != "" {
+		req.SetQueryParam("at", at)
+	}
+	if partitions {
+		req.SetQueryParam("partition", "T")
+	}
+	req.Method = http.MethodGet
+	req.URL = path.Join("/pool", pool.String(), "segments")
+	return c.stream(req)
+}
+
+func (c *Connection) ScanLog(ctx context.Context, pool ksuid.KSUID) (*ReadCloser, error) {
+	req := c.Request(ctx)
+	req.Method = http.MethodGet
+	req.URL = path.Join("/pool", pool.String(), "log")
+	return c.stream(req)
 }
 
 func (c *Connection) SearchRaw(ctx context.Context, search api.SearchRequest, params map[string]string) (*ReadCloser, error) {

api/ksuid.go
@@ -38,3 +38,13 @@ func ParseKSUID(s string) (ksuid.KSUID, error) {
 	}
 	return id, nil
 }
+
+func ParseKSUIDs(in []string) (ids []ksuid.KSUID, err error) {
+	ids = make([]ksuid.KSUID, len(in))
+	for i, s := range in {
+		if ids[i], err = ParseKSUID(s); err != nil {
+			return nil, err
+		}
+	}
+	return ids, nil
+}

api/mime.go
@@ -15,6 +15,14 @@ const (
 	MediaTypeZSON   = "application/x-zson"
 )
 
+func IsAmbiguousMediaType(s string) bool {
+	typ, _, err := mime.ParseMediaType(s)
+	if err != nil && !errors.Is(err, mime.ErrInvalidMediaParameter) {
+		return false
+	}
+	return typ == "" || typ == "*/*"
+}
+
 func MediaTypeToFormat(s string) (string, error) {
 	typ, _, err := mime.ParseMediaType(s)
 	if err != nil && !errors.Is(err, mime.ErrInvalidMediaParameter) {

cli/lakecli/lakecli.go
@@ -1,0 +1,126 @@
+package lakecli
+
+import (
+	"context"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"strconv"
+
+	"github.com/brimdata/zed/api"
+	"github.com/brimdata/zed/cmd/zed/query"
+	"github.com/brimdata/zed/expr/extent"
+	"github.com/brimdata/zed/lake"
+	"github.com/brimdata/zed/lake/index"
+	"github.com/brimdata/zed/lake/journal"
+	"github.com/brimdata/zed/order"
+	"github.com/brimdata/zed/zbuf"
+	"github.com/brimdata/zed/zio"
+	"github.com/segmentio/ksuid"
+)
+
+type Flags interface {
+	SetFlags(fs *flag.FlagSet)
+	Quiet() bool
+	PoolName() string
+	Create(ctx context.Context) (Root, error)
+	Open(ctx context.Context) (Root, error)
+	OpenPool(ctx context.Context) (Pool, error)
+	CreatePool(ctx context.Context, layout order.Layout, thresh int64) (Pool, error)
+}
+
+type Root interface {
+	AddIndex(context.Context, []index.Index) error
+	CreatePool(context.Context, string, order.Layout, int64) (Pool, error)
+	OpenPool(context.Context, ksuid.KSUID) (Pool, error)
+	ScanPools(context.Context, zio.Writer) error
+	LookupPoolByName(context.Context, string) (*lake.PoolConfig, error)
+	RemovePool(context.Context, ksuid.KSUID) error
+	DeleteIndices(context.Context, []ksuid.KSUID) ([]index.Index, error)
+	LookupIndices(context.Context, []ksuid.KSUID) ([]index.Index, error)
+	ScanIndex(context.Context, zio.Writer, []ksuid.KSUID) error
+	Query(context.Context, zio.Writer, query.Includes, query.Includes) (zbuf.ScannerStats, error)
+}
+
+type Pool interface {
+	Config() lake.PoolConfig
+	// Add adds the contents of the reader to a pool. If the commit is not nil
+	// the data will be committed to the pool.
+	Add(ctx context.Context, r zio.Reader, commit *api.CommitRequest) (ksuid.KSUID, error)
+	Delete(ctx context.Context, tags []ksuid.KSUID, commit *api.CommitRequest) (ksuid.KSUID, error)
+	Commit(ctx context.Context, id ksuid.KSUID, commit api.CommitRequest) error
+	ScanLog(ctx context.Context, w zio.Writer, head, tail journal.ID) error
+	ScanStaging(ctx context.Context, w zio.Writer, ids []ksuid.KSUID) error
+	ScanSegments(ctx context.Context, w zio.Writer, at string, partitions bool, span extent.Span) error
+	Squash(ctx context.Context, ids []ksuid.KSUID, commit api.CommitRequest) (ksuid.KSUID, error)
+	Index(ctx context.Context, rules []index.Index, ids []ksuid.KSUID) (ksuid.KSUID, error)
+}
+
+type baseFlags struct {
+	quiet    bool
+	poolName string
+}
+
+func (b *baseFlags) SetFlags(set *flag.FlagSet) {
+	set.BoolVar(&b.quiet, "q", false, "quiet mode")
+	set.StringVar(&b.poolName, "p", "", "name of pool")
+}
+
+func (b *baseFlags) Quiet() bool      { return b.quiet }
+func (b *baseFlags) PoolName() string { return b.poolName }
+
+func ParseID(s string) (ksuid.KSUID, error) {
+	// Check if this is a cut-and-paste from ZNG, which encodes
+	// the 20-byte KSUID as a 40 character hex string with 0x prefix.
+	var id ksuid.KSUID
+	if len(s) == 42 && s[0:2] == "0x" {
+		b, err := hex.DecodeString(s[2:])
+		if err != nil {
+			return ksuid.Nil, fmt.Errorf("illegal hex tag: %s", s)
+		}
+		id, err = ksuid.FromBytes(b)
+		if err != nil {
+			return ksuid.Nil, fmt.Errorf("illegal hex tag: %s", s)
+		}
+	} else {
+		var err error
+		id, err = ksuid.Parse(s)
+		if err != nil {
+			return ksuid.Nil, fmt.Errorf("%s: invalid commit ID", s)
+		}
+	}
+	return id, nil
+}
+
+func ParseIDs(ss []string) ([]ksuid.KSUID, error) {
+	ids := make([]ksuid.KSUID, 0, len(ss))
+	for _, s := range ss {
+		id, err := ParseID(s)
+		if err != nil {
+			return nil, err
+		}
+		ids = append(ids, id)
+	}
+	return ids, nil
+}
+
+func parseJournalID(ctx context.Context, pool *lake.Pool, at string) (journal.ID, error) {
+	if num, err := strconv.Atoi(at); err == nil {
+		ok, err := pool.IsJournalID(ctx, journal.ID(num))
+		if err != nil {
+			return journal.Nil, err
+		}
+		if ok {
+			return journal.ID(num), nil
+		}
+	}
+	commitID, err := ParseID(at)
+	if err != nil {
+		return journal.Nil, fmt.Errorf("not a valid journal number or a commit tag: %s", at)
+	}
+	id, err := pool.Log().JournalIDOfCommit(ctx, 0, commitID)
+	if err != nil {
+		return journal.Nil, fmt.Errorf("not a valid journal number or a commit tag: %s", at)
+	}
+	return id, nil
+}

cli/lakecli/local.go
@@ -1,0 +1,268 @@
+package lakecli
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/brimdata/zed/api"
+	"github.com/brimdata/zed/cmd/zed/query"
+	"github.com/brimdata/zed/driver"
+	"github.com/brimdata/zed/expr/extent"
+	"github.com/brimdata/zed/lake"
+	"github.com/brimdata/zed/lake/index"
+	"github.com/brimdata/zed/lake/journal"
+	"github.com/brimdata/zed/order"
+	"github.com/brimdata/zed/pkg/rlimit"
+	"github.com/brimdata/zed/pkg/storage"
+	"github.com/brimdata/zed/zbuf"
+	"github.com/brimdata/zed/zio"
+	"github.com/brimdata/zed/zson"
+	"github.com/segmentio/ksuid"
+)
+
+const RootEnv = "ZED_LAKE_ROOT"
+
+func DefaultRoot() string {
+	return os.Getenv(RootEnv)
+}
+
+type LocalFlags struct {
+	baseFlags
+	engine storage.Engine
+	root   string
+}
+
+func NewLocalFlags(set *flag.FlagSet) *LocalFlags {
+	l := new(LocalFlags)
+	set.StringVar(&l.root, "R", DefaultRoot(), "URI of path to Zed lake store")
+	l.baseFlags.SetFlags(set)
+	l.engine = storage.NewLocalEngine()
+	return l
+}
+
+func (l *LocalFlags) RootPath() (*storage.URI, error) {
+	if l.root == "" {
+		return nil, errors.New("no lake path specied: use -R or set ZED_LAKE_ROOT")
+	}
+	return storage.ParseURI(l.root)
+}
+
+func (l *LocalFlags) Create(ctx context.Context) (Root, error) {
+	path, err := l.RootPath()
+	if err != nil {
+		return nil, err
+	}
+	root, err := lake.Create(ctx, l.engine, path)
+	if err != nil {
+		return nil, err
+	}
+	return newLocal(root, l.baseFlags), nil
+}
+
+func (l *LocalFlags) Open(ctx context.Context) (Root, error) {
+	path, err := l.RootPath()
+	if err != nil {
+		return nil, err
+	}
+	root, err := lake.Open(ctx, l.engine, path)
+	if err != nil {
+		return nil, err
+	}
+	return newLocal(root, l.baseFlags), nil
+}
+
+func (l LocalFlags) OpenPool(ctx context.Context) (Pool, error) {
+	if l.poolName == "" {
+		return nil, errors.New("no pool name provided")
+	}
+	root, err := l.Open(ctx)
+	if err != nil {
+		return nil, err
+	}
+	pool, err := root.LookupPoolByName(ctx, l.poolName)
+	if pool == nil {
+		return nil, fmt.Errorf("%s: pool not found", l.poolName)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return root.OpenPool(ctx, pool.ID)
+}
+
+func (l LocalFlags) CreatePool(ctx context.Context, layout order.Layout, thresh int64) (Pool, error) {
+	if l.poolName == "" {
+		return nil, errors.New("no pool name provided")
+	}
+	root, err := l.Open(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return root.CreatePool(ctx, l.poolName, layout, thresh)
+}
+
+func (l *LocalFlags) SetRoot(s string) {
+	l.root = s
+}
+
+// Create(ctx context.Context) (Root, error)
+// OpenPool(ctx context.Context) (Pool, error)
+
+type LocalRoot struct {
+	*lake.Root
+	flags baseFlags
+}
+
+func newLocal(r *lake.Root, flags baseFlags) Root {
+	return &LocalRoot{r, flags}
+}
+
+func (r *LocalRoot) CreatePool(ctx context.Context, name string, layout order.Layout, thresh int64) (Pool, error) {
+	pool, err := r.Root.CreatePool(ctx, name, layout, thresh)
+	if err != nil {
+		return nil, err
+	}
+	return &LocalPool{pool}, nil
+}
+
+func (r *LocalRoot) OpenPool(ctx context.Context, id ksuid.KSUID) (Pool, error) {
+	pool, err := r.Root.OpenPool(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &LocalPool{pool}, nil
+}
+
+func (r *LocalRoot) ScanIndex(ctx context.Context, w zio.Writer, ids []ksuid.KSUID) error {
+	if ids == nil {
+		ids = r.Root.ListIndexIDs()
+	}
+	return r.Root.ScanIndex(ctx, w, ids)
+}
+
+func (r *LocalRoot) Query(ctx context.Context, w zio.Writer, args, includes query.Includes) (zbuf.ScannerStats, error) {
+	zedQuery, err := query.ParseSources(args, includes)
+	if err != nil {
+		return zbuf.ScannerStats{}, fmt.Errorf("zed lake query: %w", err)
+	}
+	if _, err := rlimit.RaiseOpenFilesLimit(); err != nil {
+		return zbuf.ScannerStats{}, err
+	}
+	d := driver.NewCLI(w)
+	if !r.flags.Quiet() {
+		d.SetWarningsWriter(os.Stderr)
+	}
+	return driver.RunWithLake(ctx, d, zedQuery, zson.NewContext(), r.Root)
+}
+
+func (r *LocalRoot) LookupPool(ctx context.Context, id ksuid.KSUID) (*lake.PoolConfig, error) {
+	return r.Root.LookupPool(ctx, id), nil
+}
+
+func (r *LocalRoot) LookupPoolByName(ctx context.Context, name string) (*lake.PoolConfig, error) {
+	return r.Root.LookupPoolByName(ctx, name), nil
+}
+
+var _ Pool = &LocalPool{}
+
+type LocalPool struct {
+	pool *lake.Pool
+}
+
+func (p *LocalPool) Config() lake.PoolConfig {
+	return p.pool.PoolConfig
+}
+
+func (p *LocalPool) Add(ctx context.Context, r zio.Reader, commit *api.CommitRequest) (ksuid.KSUID, error) {
+	id, err := p.pool.Add(ctx, r)
+	if err != nil {
+		return ksuid.Nil, err
+	}
+	if commit != nil {
+		if err := p.pool.Commit(ctx, id, commit.Date, commit.Author, commit.Message); err != nil {
+			return ksuid.Nil, err
+		}
+	}
+	return id, nil
+}
+
+func (p *LocalPool) Commit(ctx context.Context, id ksuid.KSUID, commit api.CommitRequest) error {
+	return p.pool.Commit(ctx, id, commit.Date, commit.Author, commit.Message)
+}
+
+func (p *LocalPool) Delete(ctx context.Context, tags []ksuid.KSUID, commit *api.CommitRequest) (ksuid.KSUID, error) {
+	pool := p.pool
+	ids, err := pool.LookupTags(ctx, tags)
+	if err != nil {
+		return ksuid.Nil, err
+	}
+	commitID, err := pool.Delete(ctx, ids)
+	if err != nil {
+		return ksuid.Nil, err
+	}
+	if commit != nil {
+		if err := pool.Commit(ctx, commitID, commit.Date, commit.Author, commit.Message); err != nil {
+			return ksuid.Nil, err
+		}
+	}
+	return commitID, nil
+}
+
+func (p *LocalPool) Index(ctx context.Context, indices []index.Index, tags []ksuid.KSUID) (ksuid.KSUID, error) {
+	tags, err := p.pool.LookupTags(ctx, tags)
+	if err != nil {
+		return ksuid.Nil, err
+	}
+	commit, err := p.pool.Index(ctx, indices, tags)
+	if err != nil {
+		return ksuid.Nil, err
+	}
+	return commit, nil
+}
+
+func (p *LocalPool) Squash(ctx context.Context, ids []ksuid.KSUID, commit api.CommitRequest) (ksuid.KSUID, error) {
+	return p.pool.Squash(ctx, ids, commit.Date, commit.Author, commit.Message)
+}
+
+func (p *LocalPool) ScanStaging(ctx context.Context, w zio.Writer, ids []ksuid.KSUID) error {
+	if len(ids) == 0 {
+		// Show all of staging.
+		var err error
+		ids, err = p.pool.ListStagedCommits(ctx)
+		if err != nil {
+			return err
+		}
+		if len(ids) == 0 {
+			return errors.New("staging area empty")
+		}
+	}
+	return p.pool.ScanStaging(ctx, w, ids)
+}
+
+func (p *LocalPool) ScanLog(ctx context.Context, w zio.Writer, head, tail journal.ID) error {
+	r, err := p.pool.Log().OpenAsZNG(ctx, head, tail)
+	if err != nil {
+		return err
+	}
+	return zio.CopyWithContext(ctx, w, r)
+}
+
+func (p *LocalPool) ScanSegments(ctx context.Context, w zio.Writer, at string, partition bool, span extent.Span) (err error) {
+	var id journal.ID
+	if at != "" {
+		id, err = parseJournalID(ctx, p.pool, at)
+		if err != nil {
+			return err
+		}
+	}
+	snap, err := p.pool.Log().Snapshot(ctx, id)
+	if err != nil {
+		return err
+	}
+	if partition {
+		return p.pool.ScanPartitions(ctx, w, snap, span)
+	}
+	return p.pool.ScanSegments(ctx, w, snap, span)
+}

cli/lakecli/remote.go
@@ -1,0 +1,291 @@
+package lakecli
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/brimdata/zed/api"
+	"github.com/brimdata/zed/api/client"
+	"github.com/brimdata/zed/cmd/zed/query"
+	"github.com/brimdata/zed/expr/extent"
+	"github.com/brimdata/zed/lake"
+	"github.com/brimdata/zed/lake/index"
+	"github.com/brimdata/zed/lake/journal"
+	"github.com/brimdata/zed/order"
+	"github.com/brimdata/zed/zbuf"
+	"github.com/brimdata/zed/zio"
+	"github.com/brimdata/zed/zio/anyio"
+	"github.com/brimdata/zed/zio/zngio"
+	"github.com/brimdata/zed/zio/zsonio"
+	"github.com/brimdata/zed/zson"
+	"github.com/segmentio/ksuid"
+)
+
+const HostEnv = "ZED_LAKE_HOST"
+
+func DefaultHost() string {
+	host := os.Getenv(HostEnv)
+	if host == "" {
+		host = "localhost:9867"
+	}
+	return host
+}
+
+type RemoteFlags struct {
+	baseFlags
+	host string
+}
+
+func NewRemoteFlags(set *flag.FlagSet) Flags {
+	l := new(RemoteFlags)
+	set.StringVar(&l.host, "host", DefaultHost(), "host[:port] of Zed lake service")
+	l.baseFlags.SetFlags(set)
+	return l
+}
+
+func (l *RemoteFlags) Conn() *client.Connection {
+	host := l.host
+	if !strings.HasPrefix(l.host, "http") {
+		host = "http://" + host
+	}
+	return client.NewConnectionTo(host)
+}
+
+func (l *RemoteFlags) Create(ctx context.Context) (Root, error) {
+	return nil, errors.New("cannot create new lake for remove lake")
+}
+
+func (l *RemoteFlags) Open(ctx context.Context) (Root, error) {
+	return &RemoteRoot{l.Conn()}, nil
+}
+
+func (l *RemoteFlags) OpenPool(ctx context.Context) (Pool, error) {
+	if l.poolName == "" {
+		return nil, errors.New("no pool name provided")
+	}
+	root, err := l.Open(ctx)
+	if err != nil {
+		return nil, err
+	}
+	pool, err := root.LookupPoolByName(ctx, l.poolName)
+	if err != nil {
+		return nil, err
+	}
+	if pool == nil {
+		return nil, fmt.Errorf("%s: pool not found", l.poolName)
+	}
+	return root.OpenPool(ctx, pool.ID)
+}
+
+func (l *RemoteFlags) CreatePool(ctx context.Context, layout order.Layout, thresh int64) (Pool, error) {
+	if l.poolName == "" {
+		return nil, errors.New("no pool name provided")
+	}
+	root, err := l.Open(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return root.CreatePool(ctx, l.poolName, layout, thresh)
+}
+
+type RemoteRoot struct {
+	conn *client.Connection
+}
+
+func (r *RemoteRoot) ScanPools(ctx context.Context, zw zio.Writer) error {
+	res, err := r.conn.ScanPools(ctx)
+	if err != nil {
+		return err
+	}
+	defer res.Close()
+	zr := zngio.NewReader(res, zson.NewContext())
+	return zio.CopyWithContext(ctx, zw, zr)
+}
+
+func (r *RemoteRoot) LookupPoolByName(ctx context.Context, name string) (*lake.PoolConfig, error) {
+	res, err := r.conn.ScanPools(ctx)
+	if err != nil {
+		return nil, nil
+	}
+	defer res.Close()
+	format, err := api.MediaTypeToFormat(res.ContentType)
+	if err != nil {
+		return nil, err
+	}
+	zr, err := anyio.NewReaderWithOpts(res, zson.NewContext(), anyio.ReaderOpts{Format: format})
+	if err != nil {
+		return nil, nil
+	}
+	for {
+		rec, err := zr.Read()
+		if rec == nil || err != nil {
+			return nil, err
+		}
+		var pool lake.PoolConfig
+		if err := zson.UnmarshalZNGRecord(rec, &pool); err != nil {
+			return nil, err
+		}
+		if pool.Name == name {
+			return &pool, nil
+		}
+	}
+}
+
+func (r *RemoteRoot) OpenPool(ctx context.Context, id ksuid.KSUID) (Pool, error) {
+	res, err := r.conn.PoolGet(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	var config lake.PoolConfig
+	if err := unmarshal(res, &config); err != nil {
+		return nil, err
+	}
+	return newRemotePool(r.conn, config), nil
+}
+
+func (r *RemoteRoot) CreatePool(ctx context.Context, name string, layout order.Layout, thresh int64) (Pool, error) {
+	res, err := r.conn.PoolPost(ctx, api.PoolPostRequest{
+		Name:   name,
+		Layout: layout,
+		Thresh: thresh,
+	})
+	if err != nil {
+		return nil, err
+	}
+	var config lake.PoolConfig
+	unmarshal(res, &config)
+	return newRemotePool(r.conn, config), nil
+}
+
+func (r *RemoteRoot) RemovePool(context.Context, ksuid.KSUID) error {
+	return errors.New("unsupported")
+}
+
+func (r *RemoteRoot) AddIndex(context.Context, []index.Index) error {
+	return errors.New("unsupported")
+}
+
+func (r *RemoteRoot) DeleteIndices(context.Context, []ksuid.KSUID) ([]index.Index, error) {
+	return nil, errors.New("unsupported")
+}
+
+func (r *RemoteRoot) LookupIndices(context.Context, []ksuid.KSUID) ([]index.Index, error) {
+	return nil, errors.New("unsupported")
+}
+
+func (r *RemoteRoot) ScanIndex(ctx context.Context, w zio.Writer, ids []ksuid.KSUID) error {
+	return errors.New("unsupported")
+}
+
+func (r *RemoteRoot) Query(ctx context.Context, w zio.Writer, args, includes query.Includes) (zbuf.ScannerStats, error) {
+	return zbuf.ScannerStats{}, errors.New("unsupported")
+}
+
+type RemotePool struct {
+	lake.PoolConfig
+	conn *client.Connection
+}
+
+func newRemotePool(conn *client.Connection, conf lake.PoolConfig) *RemotePool {
+	return &RemotePool{PoolConfig: conf, conn: conn}
+}
+
+func unmarshal(r *client.ReadCloser, i interface{}) error {
+	format, err := api.MediaTypeToFormat(r.ContentType)
+	if err != nil {
+		return err
+	}
+	zr, err := anyio.NewReaderWithOpts(r, zson.NewContext(), anyio.ReaderOpts{Format: format})
+	if err != nil {
+		return nil
+	}
+	var buf bytes.Buffer
+	// XXX maybe just have requests that do this request a zson response?
+	zw := zsonio.NewWriter(zio.NopCloser(&buf), zsonio.WriterOpts{})
+	if err := zio.Copy(zw, zr); err != nil {
+		return err
+	}
+	if err := zw.Close(); err != nil {
+		return err
+	}
+	return zson.Unmarshal(buf.String(), i)
+}
+
+func (p *RemotePool) Config() lake.PoolConfig {
+	return p.PoolConfig
+}
+
+func (p *RemotePool) Add(ctx context.Context, r zio.Reader, commit *api.CommitRequest) (ksuid.KSUID, error) {
+	pr, pw := io.Pipe()
+	w := zngio.NewWriter(pw, zngio.WriterOpts{})
+	go func() {
+		zio.CopyWithContext(ctx, w, r)
+		w.Close()
+	}()
+	rc, err := p.conn.Add(ctx, p.ID, pr)
+	if err != nil {
+		return ksuid.Nil, err
+	}
+	var res api.AddResponse
+	if err := unmarshal(rc, &res); err != nil {
+		return ksuid.Nil, err
+	}
+	if commit != nil {
+		err = p.conn.Commit(ctx, p.ID, res.Commit, *commit)
+	}
+	return res.Commit, err
+}
+
+func (p *RemotePool) Delete(ctx context.Context, ids []ksuid.KSUID, commit *api.CommitRequest) (ksuid.KSUID, error) {
+	return ksuid.Nil, errors.New("unsupported")
+}
+
+func (p *RemotePool) Commit(ctx context.Context, id ksuid.KSUID, commit api.CommitRequest) error {
+	return p.conn.Commit(ctx, p.ID, id, commit)
+}
+
+func (p *RemotePool) Squash(ctx context.Context, ids []ksuid.KSUID, commit api.CommitRequest) (ksuid.KSUID, error) {
+	return ksuid.Nil, errors.New("unsupported")
+}
+
+func (p *RemotePool) ScanStaging(ctx context.Context, w zio.Writer, ids []ksuid.KSUID) error {
+	res, err := p.conn.ScanStaging(ctx, p.ID, ids)
+	if err != nil {
+		return err
+	}
+	defer res.Close()
+	zr := zngio.NewReader(res, zson.NewContext())
+	return zio.CopyWithContext(ctx, w, zr)
+
+}
+
+func (p *RemotePool) ScanLog(ctx context.Context, w zio.Writer, head, tail journal.ID) error {
+	res, err := p.conn.ScanLog(ctx, p.ID)
+	if err != nil {
+		return err
+	}
+	defer res.Close()
+	zr := zngio.NewReader(res, zson.NewContext())
+	return zio.CopyWithContext(ctx, w, zr)
+}
+
+func (p *RemotePool) ScanSegments(ctx context.Context, w zio.Writer, at string, partitions bool, span extent.Span) error {
+	// TODO add segments to connection.PoolScanSegments
+	res, err := p.conn.ScanSegments(ctx, p.ID, at, partitions)
+	if err != nil {
+		return err
+	}
+	defer res.Close()
+	zr := zngio.NewReader(res, zson.NewContext())
+	return zio.CopyWithContext(ctx, w, zr)
+}
+
+func (p *RemotePool) Index(ctx context.Context, rules []index.Index, ids []ksuid.KSUID) (ksuid.KSUID, error) {
+	return ksuid.Nil, errors.New("unsupported")
+}

cmd/zapi/main.go
@@ -3,14 +3,33 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/brimdata/zed/cmd/zed/api"
+	_ "github.com/brimdata/zed/cmd/zed/lake/add"
+	_ "github.com/brimdata/zed/cmd/zed/lake/commit"
+	_ "github.com/brimdata/zed/cmd/zed/lake/create"
+	_ "github.com/brimdata/zed/cmd/zed/lake/delete"
+	_ "github.com/brimdata/zed/cmd/zed/lake/drop"
+	_ "github.com/brimdata/zed/cmd/zed/lake/find"
+	_ "github.com/brimdata/zed/cmd/zed/lake/index"
+	_ "github.com/brimdata/zed/cmd/zed/lake/init"
+	_ "github.com/brimdata/zed/cmd/zed/lake/load"
+	_ "github.com/brimdata/zed/cmd/zed/lake/log"
+	_ "github.com/brimdata/zed/cmd/zed/lake/ls"
+	_ "github.com/brimdata/zed/cmd/zed/lake/merge"
+	_ "github.com/brimdata/zed/cmd/zed/lake/query"
+	_ "github.com/brimdata/zed/cmd/zed/lake/serve"
+	_ "github.com/brimdata/zed/cmd/zed/lake/squash"
+	_ "github.com/brimdata/zed/cmd/zed/lake/status"
+	_ "github.com/brimdata/zed/cmd/zed/lake/vacate"
+	"github.com/brimdata/zed/cmd/zed/root"
 )
 
 func main() {
-	fmt.Fprintf(os.Stderr, "disabled for now")
-	// root.Zed.Add(api.Cmd)
-	// args := append([]string{"api"}, os.Args[1:]...)
-	// if err := root.Zed.ExecRoot(args); err != nil {
-	// fmt.Fprintf(os.Stderr, "%s\n", err)
-	// os.Exit(1)
-	// }
+	root.Zed.Add(api.Cmd)
+	args := append([]string{"api"}, os.Args[1:]...)
+	if err := root.Zed.ExecRoot(args); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }

commit 276c03edebced22cebd2ab1b5ad4571c4da908cd

Fix search endpoint
Fix bug introduced in #2774

service/search/search.go
@@ -220,8 +220,5 @@ func (d *searchdriver) ChannelEnd(cid int) error {
 		ChannelID: cid,
 		Reason:    "eof",
 	}
-	if err := d.output.SendControl(v); err != nil {
-		return err
-	}
-	return d.end(0)
+	return d.output.SendControl(v)
 }