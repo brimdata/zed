a28a6c74df6ba539dc9fae764b9f70e9ed8ab803
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-08-26 21:22:42 -0400

    lake: make rangeWrapper filters safe for concurrent use
    Because of concurrency in the ZNG scanner, separate filters returned by
    a zbuf.Filter's methods must be safe for concurrent use in separate
    goroutines.  Filters returned by lake.rangeWrapper.AsFilter do not have
    this property because they share the single expr.ValueCompareFn in
    rangeWrapper.compare.  Fix this by creating a new expr.ValueCompareFn
    for each filter returned.

--- lake/sorted.go
+++ lake/sorted.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/brimdata/zed/expr"
 	"github.com/brimdata/zed/expr/extent"
-	"github.com/brimdata/zed/field"
+	"github.com/brimdata/zed/order"
 	"github.com/brimdata/zed/zbuf"
 	"github.com/brimdata/zed/zio/zngio"
 	"github.com/brimdata/zed/zng"
@@ -65,7 +65,7 @@ func newSortedScanner(ctx context.Context, pool *Pool, zctx *zson.Context, filte
 		if len(pool.Layout.Keys) != 0 {
 			// If the scan span does not wholly contain the segment, then
 			// we must filter out records that fall outside the range.
-			f = wrapRangeFilter(f, scan.Span, scan.compare, segref.First, segref.Last, pool.Layout.Keys[0])
+			f = wrapRangeFilter(f, scan.Span, scan.compare, segref.First, segref.Last, pool.Layout)
 		}
 		scanner, err := reader.NewScanner(ctx, f)
 		if err != nil {
@@ -95,10 +95,9 @@ func newSortedScanner(ctx context.Context, pool *Pool, zctx *zson.Context, filte
 
 type rangeWrapper struct {
 	zbuf.Filter
-	first   zng.Value
-	last    zng.Value
-	key     field.Path
-	compare expr.ValueCompareFn
+	first  zng.Value
+	last   zng.Value
+	layout order.Layout
 }
 
 func (r *rangeWrapper) AsFilter() (expr.Filter, error) {
@@ -106,21 +105,22 @@ func (r *rangeWrapper) AsFilter() (expr.Filter, error) {
 	if err != nil {
 		return nil, err
 	}
+	compare := extent.CompareFunc(r.layout.Order)
 	return func(rec *zng.Record) bool {
-		keyVal, err := rec.Deref(r.key)
+		keyVal, err := rec.Deref(r.layout.Keys[0])
 		if err != nil {
 			// XXX match keyless records.
 			// See issue #2637.
 			return true
 		}
-		if r.compare(keyVal, r.first) < 0 || r.compare(keyVal, r.last) > 0 {
+		if compare(keyVal, r.first) < 0 || compare(keyVal, r.last) > 0 {
 			return false
 		}
 		return f == nil || f(rec)
 	}, nil
 }
 
-func wrapRangeFilter(f zbuf.Filter, scan extent.Span, cmp expr.ValueCompareFn, first, last zng.Value, key field.Path) zbuf.Filter {
+func wrapRangeFilter(f zbuf.Filter, scan extent.Span, cmp expr.ValueCompareFn, first, last zng.Value, layout order.Layout) zbuf.Filter {
 	scanFirst := scan.First()
 	scanLast := scan.Last()
 	if cmp(scanFirst, first) <= 0 {
@@ -129,10 +129,9 @@ func wrapRangeFilter(f zbuf.Filter, scan extent.Span, cmp expr.ValueCompareFn, f
 		}
 	}
 	return &rangeWrapper{
-		Filter:  f,
-		first:   scanFirst,
-		last:    scanLast,
-		key:     key,
-		compare: cmp,
+		Filter: f,
+		first:  scanFirst,
+		last:   scanLast,
+		layout: layout,
 	}
 }
