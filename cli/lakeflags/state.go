882418925884e7b2cd43425b8e95f0bd79df9705
Steve McCanne <steve@mccanne.com>
2021-09-07 07:33:09 -0700

    change behavior of lake checkout state (#3024)
    This commit changes the default behavior of the checkout command
    to store the HEAD pointer in $HOME.  The environment variable
    ZED_HEAD_DIR can be defined to override this behavior.  In particular,
    setting ZED_HEAD_DIR to ".", will revert to the previous behavior
    where the HEAD pointer is stored in the current working directory.

--- cli/lakeflags/state.go
+++ cli/lakeflags/state.go
@@ -3,13 +3,25 @@ package lakeflags
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 const headFile = ".zed_head"
 
+func headPath() string {
+	dir := os.Getenv("ZED_HEAD_DIR")
+	if dir == "" {
+		dir, _ = os.UserHomeDir()
+	}
+	if dir == "." {
+		dir = ""
+	}
+	return filepath.Join(dir, headFile)
+}
+
 func readHead() (string, error) {
-	b, err := os.ReadFile(headFile)
+	b, err := os.ReadFile(headPath())
 	if err != nil {
 		return "", err
 	}
@@ -18,7 +30,7 @@ func readHead() (string, error) {
 
 func WriteHead(pool, branch string) error {
 	head := fmt.Sprintf("%s@%s\n", pool, branch)
-	err := os.WriteFile(headFile, []byte(head), 0644)
+	err := os.WriteFile(headPath(), []byte(head), 0644)
 	if err != nil {
 		err = fmt.Errorf("%q: failed to write HEAD: %w", headFile, err)
 	}
