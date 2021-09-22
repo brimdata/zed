1f0e00248fab0290ee7b68d694b7e161d2f87d89
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-09-22 15:37:36 -0400

    fix zero-length output check in script-style ztests (#3108)
    Script-style ztests often check for a zero-length output with the
    equivalent of "data: ''".  I inadvertently disabled those checks in
    #3091.  Re-enable them.

--- ztest/ztest_test.go
+++ ztest/ztest_test.go
@@ -1,10 +1,13 @@
 package ztest
 
 import (
+	"os"
+	"path/filepath"
 	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestShouldSkip(t *testing.T) {
@@ -21,16 +24,30 @@ func TestRunScript(t *testing.T) {
 	if runtime.GOOS == "windows" {
 		t.Skip("skipping test on Windows because RunScript uses cmd.exe instead of bash")
 	}
-	t.Run("success", func(t *testing.T) {
-		stdout := "1\n"
-		stderr := "2\n"
+	t.Run("outputs", func(t *testing.T) {
+		testDir := t.TempDir()
+		require.NoError(t, os.WriteFile(filepath.Join(testDir, "testdirfile"), []byte("testdirfile\n"), 0644))
+		strptr := func(s string) *string { return &s }
 		err := (&ZTest{
-			Script: "echo 1; echo 2 >&2",
+			Script: `
+				echo stdout
+				echo stderr >&2
+				touch empty
+				echo notempty > notempty
+				echo regexp > regexp
+				echo testdirfile > testdirfile
+				echo testdirfile > testdirfile2
+				`,
 			Outputs: []File{
-				{Name: "stdout", Data: &stdout},
-				{Name: "stderr", Data: &stderr},
+				{Name: "stdout", Data: strptr("stdout\n")},
+				{Name: "stderr", Data: strptr("stderr\n")},
+				{Name: "empty", Data: strptr("")},
+				{Name: "notempty", Data: strptr("notempty\n")},
+				{Name: "regexp", Re: "^re"},
+				{Name: "testdirfile"},
+				{Name: "testdirfile2", Source: "testdirfile"},
 			},
-		}).RunScript("", "", "")
+		}).RunScript("", testDir, t.TempDir())
 		assert.NoError(t, err)
 	})
 	t.Run("error", func(t *testing.T) {
