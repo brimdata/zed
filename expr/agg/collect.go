2ed5f3edc40fc1cf1c4782978a6d6f6c5c56cfff
Steven McCanne <steve@mccanne.com>
2021-10-12 08:21:14 -0700

    change collect() to handle heterogeneous types with a type union

--- expr/agg/collect.go
+++ expr/agg/collect.go
@@ -8,75 +8,113 @@ import (
 )
 
 type Collect struct {
-	typ  zed.Type
-	val  []zcode.Bytes
-	size int
+	values []zed.Value
+	size   int
 }
 
 func (c *Collect) Consume(v zed.Value) error {
 	if v.IsNil() {
 		return nil
 	}
-	if c.typ == nil {
-		c.typ = v.Type
-	} else if c.typ != v.Type {
-		//c.TypeMismatch++
-		return nil
-	}
-	c.update(v.Bytes)
+	c.update(v)
 	return nil
 }
 
-func (c *Collect) update(b zcode.Bytes) {
-	stash := make(zcode.Bytes, len(b))
-	copy(stash, b)
-	c.val = append(c.val, stash)
-	c.size += len(b)
+func (c *Collect) update(v zed.Value) {
+	stash := make(zcode.Bytes, len(v.Bytes))
+	copy(stash, v.Bytes)
+	c.values = append(c.values, zed.Value{v.Type, stash})
+	c.size += len(v.Bytes)
 	for c.size > MaxValueSize {
 		// XXX See issue #1813.  For now we silently discard entries
 		// to maintain the size limit.
 		//c.MemExceeded++
-		c.size -= len(c.val[0])
-		c.val = c.val[1:]
+		c.size -= len(c.values[0].Bytes)
+		c.values = c.values[1:]
 	}
 }
 
 func (c *Collect) Result(zctx *zed.Context) (zed.Value, error) {
-	if c.typ == nil {
+	if len(c.values) == 0 {
 		// no values found
 		return zed.Value{Type: zed.TypeNull}, nil
 	}
+	m := make(map[zed.Type]int)
+	for _, zv := range c.values {
+		m[zv.Type] = 0
+	}
+	if len(m) == 1 {
+		return c.build(zctx)
+	}
+	return c.buildUnion(zctx, m)
+}
+
+func (c *Collect) build(zctx *zed.Context) (zed.Value, error) {
+	typ := c.values[0].Type
 	var b zcode.Builder
-	container := zed.IsContainerType(c.typ)
-	for _, item := range c.val {
+	container := zed.IsContainerType(typ)
+	for _, v := range c.values {
 		if container {
-			b.AppendContainer(item)
+			b.AppendContainer(v.Bytes)
 		} else {
-			b.AppendPrimitive(item)
+			b.AppendPrimitive(v.Bytes)
 		}
 	}
-	typ := zctx.LookupTypeArray(c.typ)
-	return zed.Value{typ, b.Bytes()}, nil
+	arrayType := zctx.LookupTypeArray(typ)
+	return zed.Value{arrayType, b.Bytes()}, nil
+}
+
+func (c *Collect) buildUnion(zctx *zed.Context, selectors map[zed.Type]int) (zed.Value, error) {
+	// XXX When all of the types are unions we should combine them into a
+	// a merged union.  This will allow partials that compute different
+	// unions to do the right thing.  See issue #3171.
+	// XXX This map needs to be put in canonical order but we haven't
+	// done canonical type order of unions yet.  See #2145.
+	// Also, we should have a nice Zed library function
+	// to create union values instead of doing the work here to find the
+	// index of the type.
+	types := make([]zed.Type, 0, len(selectors))
+	for typ, _ := range selectors {
+		selectors[typ] = len(types)
+		types = append(types, typ)
+	}
+	var b zcode.Builder
+	for _, v := range c.values {
+		selector := selectors[v.Type]
+		b.BeginContainer()
+		b.AppendPrimitive(zed.EncodeInt(int64(selector)))
+		if zed.IsContainerType(v.Type) {
+			b.AppendContainer(v.Bytes)
+		} else {
+			b.AppendPrimitive(v.Bytes)
+		}
+		b.EndContainer()
+	}
+	unionType := zctx.LookupTypeUnion(types)
+	arrayType := zctx.LookupTypeArray(unionType)
+	return zed.Value{arrayType, b.Bytes()}, nil
 }
 
 func (c *Collect) ConsumeAsPartial(zv zed.Value) error {
-	if c.typ == nil {
-		typ, ok := zv.Type.(*zed.TypeArray)
-		if !ok {
-			return errors.New("partial not an array type")
-		}
-		c.typ = typ.Type
+	//XXX These should not be passed in here. See issue #3175
+	if len(zv.Bytes) == 0 {
+		return nil
 	}
+	arrayType, ok := zv.Type.(*zed.TypeArray)
+	if !ok {
+		return errors.New("partial is not an array type in collect aggregator: " + zv.String())
+	}
+	typ := arrayType.Type
 	for it := zv.Iter(); !it.Done(); {
-		elem, _, err := it.Next()
+		b, _, err := it.Next()
 		if err != nil {
 			return err
 		}
-		c.update(elem)
+		c.update(zed.Value{typ, b})
 	}
 	return nil
 }
 
-func (c *Collect) ResultAsPartial(tc *zed.Context) (zed.Value, error) {
-	return c.Result(tc)
+func (c *Collect) ResultAsPartial(zctx *zed.Context) (zed.Value, error) {
+	return c.Result(zctx)
 }
