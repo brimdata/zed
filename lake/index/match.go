e39c25529ef8f036923380da97de5c09f1c989f9
Matthew Nibecker <hello@mattnibecker.com>
2021-10-18 14:47:57 -0400

    index: Use KeyValue for lookup (#3188)
    Instead of constructing a record for Lookup of an index value, have
    finder.Lookup* accept KeyValue.

--- lake/index/match.go
+++ lake/index/match.go
@@ -1,38 +1,31 @@
 package index
 
 import (
-	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/compiler/ast/dag"
-	"github.com/brimdata/zed/field"
+	"github.com/brimdata/zed/index"
 	"github.com/brimdata/zed/zson"
 )
 
 type match struct {
 	rule       Rule
-	lookupKeys []*zed.Value
+	lookupKeys []index.KeyValue
 }
 
 type matcher func([]Rule) []match
 
-type predicate struct {
-	field field.Path
-	value zed.Value
-}
-
 func compilePredicates(in []dag.IndexPredicate) (matcher, error) {
-	predicates := make([]predicate, 0, len(in))
+	kvs := make([]index.KeyValue, 0, len(in))
 	for _, d := range in {
 		zv, err := zson.ParsePrimitive(d.Value.Type, d.Value.Text)
 		if err != nil {
 			return nil, err
 		}
-		predicates = append(predicates, predicate{d.Key.Name, zv})
+		kvs = append(kvs, index.KeyValue{Key: d.Key.Name, Value: zv})
 	}
-	zctx := zed.NewContext()
 	return func(rules []Rule) []match {
 		var matches []match
 		for _, r := range rules {
-			if values := matchRule(zctx, r, predicates); len(values) > 0 {
+			if values := matchRule(r, kvs); len(values) > 0 {
 				// XXX Maybe store RuleID in lookup table so we don't have to
 				// loop through all the predicates for rules we've already
 				// matched.
@@ -43,18 +36,14 @@ func compilePredicates(in []dag.IndexPredicate) (matcher, error) {
 	}, nil
 }
 
-func matchRule(zctx *zed.Context, rule Rule, predicates []predicate) []*zed.Value {
-	var recs []*zed.Value
-	for _, p := range predicates {
+func matchRule(rule Rule, in []index.KeyValue) []index.KeyValue {
+	var kvs []index.KeyValue
+	for _, kv := range in {
 		// XXX support indexes with multiple keys #3162
 		// and other rule types.
-		if f, ok := rule.(*FieldRule); ok && p.field.Equal(f.Fields[0]) {
-			rec, err := newLookupKey(zctx, rule, []zed.Value{p.value})
-			if err != nil {
-				panic(err)
-			}
-			recs = append(recs, rec)
+		if f, ok := rule.(*FieldRule); ok && kv.Key.Equal(f.Fields[0]) {
+			kvs = append(kvs, kv)
 		}
 	}
-	return recs
+	return kvs
 }
