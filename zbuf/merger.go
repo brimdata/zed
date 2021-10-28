4fcce72d1d3a5504129fb4570a15b9195888c710
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-10-28 17:06:49 -0400

    improve zbuf.Merger performance with a heap (#3221)
    In zbuf.Merger, findLeader and overlaps both do a linear search over
    pullers.  If pullers is large and cmp is expensive, these searches are
    slow.  Improve performance by replacing them with a heap using
    container/heap.

--- zbuf/merger.go
+++ zbuf/merger.go
@@ -2,12 +2,15 @@ package zbuf
 
 import (
 	"bytes"
+	"container/heap"
 	"context"
 	"sync"
 
 	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/expr"
 	"github.com/brimdata/zed/order"
+	"github.com/brimdata/zed/zio"
+	"golang.org/x/sync/errgroup"
 )
 
 // A Merger merges multiple upstream Pullers into one downstream Puller.
@@ -17,25 +20,20 @@ import (
 // would otherwise block waiting for an adjacent puller to finish but the
 // Merger is waiting on the upstream puller.
 type Merger struct {
-	ctx     context.Context
-	cancel  context.CancelFunc
 	cmp     expr.CompareFn
-	once    sync.Once
-	pullers []*mergerPuller
-	wg      sync.WaitGroup
+	pullers []Puller
+
+	cancel context.CancelFunc
+	ctx    context.Context
+	group  *errgroup.Group
+	once   sync.Once
+	// Maintained as a min-heap on cmp of mergerUpstream.zvals[0] (see Less)
+	// so that the next Read always returns upstreams[0].zvals[0].
+	upstreams []*mergerUpstream
 }
 
-type mergerPuller struct {
-	Puller
-	ch    chan batch
-	batch Batch
-	zvals []zed.Value
-}
-
-type batch struct {
-	Batch
-	err error
-}
+var _ Puller = (*Merger)(nil)
+var _ zio.Reader = (*Merger)(nil)
 
 func NewCompareFn(layout order.Layout) expr.CompareFn {
 	nullsMax := layout.Order == order.Asc
@@ -63,127 +61,113 @@ func totalOrderCompare(fn expr.CompareFn) expr.CompareFn {
 
 func NewMerger(ctx context.Context, pullers []Puller, cmp expr.CompareFn) *Merger {
 	ctx, cancel := context.WithCancel(ctx)
-	m := &Merger{
-		ctx:    ctx,
-		cmp:    cmp,
-		cancel: cancel,
+	group, ctx := errgroup.WithContext(ctx)
+	return &Merger{
+		cmp:     cmp,
+		pullers: append([]Puller(nil), pullers...),
+		cancel:  cancel,
+		ctx:     ctx,
+		group:   group,
 	}
-	m.pullers = make([]*mergerPuller, 0, len(pullers))
-	for _, p := range pullers {
-		m.pullers = append(m.pullers, &mergerPuller{Puller: p, ch: make(chan batch)})
-	}
-	return m
-}
-
-func (m *Merger) run() {
-	for _, p := range m.pullers {
-		m.wg.Add(1)
-		puller := p
-		go func() {
-			defer m.wg.Done()
-			for {
-				b, err := puller.Pull()
-				select {
-				case puller.ch <- batch{Batch: b, err: err}:
-					if b == nil && err == nil {
-						// EOS
-						return
-					}
-				case <-m.ctx.Done():
-					return
-				}
-			}
-		}()
-	}
-}
-
-// Read fulfills Reader so that we can use ReadBatch or
-// use Merger as a Reader directly.
-func (m *Merger) Read() (*zed.Value, error) {
-	m.once.Do(m.run)
-	leader, err := m.findLeader()
-	if leader < 0 || err != nil {
-		m.Cancel()
-		m.wg.Wait()
-		return nil, err
-	}
-	return m.pullers[leader].next(), nil
-}
-
-func (m *mergerPuller) next() *zed.Value {
-	rec := m.zvals[0]
-	m.zvals = m.zvals[1:]
-	m.batch = nil
-	return &rec
-}
-
-func (m *Merger) findLeader() (int, error) {
-	leader := -1
-	for k, p := range m.pullers {
-		if p == nil {
-			continue
-		}
-		if len(p.zvals) == 0 {
-			select {
-			case b := <-p.ch:
-				if b.err != nil {
-					return -1, b.err
-				}
-				if b.Batch == nil {
-					// EOS
-					m.pullers[k] = nil
-					continue
-				}
-				// We're keeping records owned by res.Batch so don't call Unref.
-				// XXX this means the batch won't be returned to
-				// the pool and instead will run through GC.
-				p.zvals = b.Batch.Values()
-				p.batch = b.Batch
-			case <-m.ctx.Done():
-				return -1, m.ctx.Err()
-			}
-		}
-		if leader == -1 || m.cmp(&p.zvals[0], &m.pullers[leader].zvals[0]) < 0 {
-			leader = k
-		}
-	}
-	return leader, nil
-}
-
-func (m *Merger) overlaps(leader int) bool {
-	hol := m.pullers[leader]
-	if hol.batch == nil {
-		return true
-	}
-	last := &hol.zvals[len(hol.zvals)-1]
-	for k, p := range m.pullers {
-		if k == leader || p == nil {
-			continue
-		}
-		if m.cmp(last, &p.zvals[0]) > 0 {
-			return true
-		}
-	}
-	return false
-}
-
-func (m *Merger) Pull() (Batch, error) {
-	m.once.Do(m.run)
-	leader, err := m.findLeader()
-	if leader < 0 || err != nil {
-		m.Cancel()
-		m.wg.Wait()
-		return nil, err
-	}
-	if !m.overlaps(leader) {
-		b := m.pullers[leader].batch
-		m.pullers[leader].zvals = nil
-		return b, nil
-	}
-	const batchLen = 100 // XXX
-	return readBatch(m, batchLen)
 }
 
 func (m *Merger) Cancel() {
 	m.cancel()
+}
+
+func (m *Merger) Pull() (Batch, error) {
+	m.once.Do(m.run)
+	if m.Len() == 0 {
+		return nil, m.group.Wait()
+	}
+	min := heap.Pop(m).(*mergerUpstream)
+	if m.Len() == 0 || m.cmp(&min.zvals[len(min.zvals)-1], &m.upstreams[0].zvals[0]) <= 0 {
+		// Either min is the only upstreams or min's last value is less
+		// than or equal to the next upstream's first value.  Either
+		// way, it's safe to return min's remaining values as a batch.
+		batch := min.batch
+		if len(min.zvals) < len(batch.Values()) {
+			batch = Array(min.zvals)
+		}
+		if min.receive() {
+			heap.Push(m, min)
+		}
+		return batch, nil
+	}
+	heap.Push(m, min)
+	const batchLen = 100 // XXX
+	return readBatch(m, batchLen)
+}
+
+func (m *Merger) Read() (*zed.Value, error) {
+	if m.Len() == 0 {
+		return nil, m.group.Wait()
+	}
+	u := m.upstreams[0]
+	zv := &u.zvals[0]
+	u.zvals = u.zvals[1:]
+	if len(u.zvals) > 0 || u.receive() {
+		heap.Fix(m, 0)
+	} else {
+		heap.Pop(m)
+	}
+	return zv, nil
+}
+
+func (m *Merger) run() {
+	for _, p := range m.pullers {
+		p := p
+		ch := make(chan Batch)
+		m.group.Go(func() error {
+			defer close(ch)
+			for {
+				batch, err := p.Pull()
+				if batch == nil || err != nil {
+					return err
+				}
+				select {
+				case ch <- batch:
+				case <-m.ctx.Done():
+					return m.ctx.Err()
+				}
+			}
+		})
+		if u := (&mergerUpstream{ch: ch}); u.receive() {
+			m.upstreams = append(m.upstreams, u)
+		}
+	}
+	heap.Init(m)
+}
+
+func (m *Merger) Len() int { return len(m.upstreams) }
+
+func (m *Merger) Less(i, j int) bool {
+	return m.cmp(&m.upstreams[i].zvals[0], &m.upstreams[j].zvals[0]) < 0
+}
+
+func (m *Merger) Swap(i, j int) { m.upstreams[i], m.upstreams[j] = m.upstreams[j], m.upstreams[i] }
+
+func (m *Merger) Push(x interface{}) { m.upstreams = append(m.upstreams, x.(*mergerUpstream)) }
+
+func (m *Merger) Pop() interface{} {
+	x := m.upstreams[len(m.upstreams)-1]
+	m.upstreams = m.upstreams[:len(m.upstreams)-1]
+	return x
+}
+
+type mergerUpstream struct {
+	ch    <-chan Batch
+	batch Batch
+	zvals []zed.Value
+}
+
+// receive tries to receive the next batch.  It returns false when no batches
+// remain.
+func (m *mergerUpstream) receive() bool {
+	batch, ok := <-m.ch
+	m.batch = batch
+	if m.batch != nil {
+		m.zvals = batch.Values()
+	}
+	return ok
 }
