dc88a06998819116fe15c64c1857fdf3f14dab62
Matthew Nibecker <hello@mattnibecker.com>
2021-10-28 17:27:58 -0400

    Update bool.go
    Co-authored-by: Noah Treuhaft <noah.treuhaft@gmail.com>

--- bool.go
+++ bool.go
@@ -13,9 +13,9 @@ func IsTrue(zv zcode.Bytes) bool {
 	return zv[0] != 0
 }
 
-// Not returns the inverse Value of the boolean typed bytes value of zv.
-func Not(zv zcode.Bytes) Value {
-	if IsTrue(zv) {
+// Not returns the inverse Value of the Boolean-typed bytes value of zb.
+func Not(zb zcode.Bytes) Value {
+	if IsTrue(zb) {
 		return False
 	}
 	return True
