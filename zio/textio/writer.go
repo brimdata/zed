4f760bc289533a2d2c0ba8c788a772591082bc5b
Noah Treuhaft <noah.treuhaft@gmail.com>
2021-12-18 11:08:55 -0500

    remove -F and -T output flags (#3347)
    The -F and -T output flags alter "-f text" output to include field names
    and types, respectively, but they see no use.

--- zio/textio/writer.go
+++ zio/textio/writer.go
@@ -12,27 +12,20 @@ import (
 )
 
 type Writer struct {
-	WriterOpts
 	writer    io.WriteCloser
 	flattener *expr.Flattener
 	format    tzngio.OutFmt
 }
 
-type WriterOpts struct {
-	ShowTypes  bool
-	ShowFields bool
-}
-
-func NewWriter(w io.WriteCloser, utf8 bool, opts WriterOpts) *Writer {
+func NewWriter(w io.WriteCloser, utf8 bool) *Writer {
 	format := tzngio.OutFormatZeekAscii
 	if utf8 {
 		format = tzngio.OutFormatZeek
 	}
 	return &Writer{
-		WriterOpts: opts,
-		writer:     w,
-		flattener:  expr.NewFlattener(zed.NewContext()),
-		format:     format,
+		writer:    w,
+		flattener: expr.NewFlattener(zed.NewContext()),
+		format:    format,
 	}
 }
 
@@ -47,28 +40,22 @@ func (w *Writer) Write(rec *zed.Value) error {
 	}
 	var out []string
 	for k, col := range zed.TypeRecordOf(rec.Type).Columns {
-		var s, v string
+		var s string
 		value := rec.ValueByColumn(k)
 		if col.Type == zed.TypeTime {
 			if value.IsUnsetOrNil() {
-				v = "-"
+				s = "-"
 			} else {
 				ts, err := zed.DecodeTime(value.Bytes)
 				if err != nil {
 					return err
 				}
-				v = ts.Time().UTC().Format(time.RFC3339Nano)
+				s = ts.Time().UTC().Format(time.RFC3339Nano)
 			}
 		} else {
-			v = tzngio.FormatValue(value, w.format)
+			s = tzngio.FormatValue(value, w.format)
 		}
-		if w.ShowFields {
-			s = col.Name + ":"
-		}
-		if w.ShowTypes {
-			s = s + tzngio.TypeString(col.Type) + ":"
-		}
-		out = append(out, s+v)
+		out = append(out, s)
 	}
 	s := strings.Join(out, "\t")
 	_, err = fmt.Fprintf(w.writer, "%s\n", s)
