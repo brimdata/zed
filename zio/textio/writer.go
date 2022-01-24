b2fc05579aff6b9f6d128239fdc983a73bb4ca99
Steven McCanne <steve@mccanne.com>
2022-01-23 21:22:38 -0800

    fix textio for non-record values
    This commit fixes the textio record to handle non-record value by
    printing them using the zeek TSV value format.

--- zio/textio/writer.go
+++ zio/textio/writer.go
@@ -27,7 +27,15 @@ func (w *Writer) Close() error {
 	return w.writer.Close()
 }
 
-func (w *Writer) Write(rec *zed.Value) error {
+func (w *Writer) Write(val *zed.Value) error {
+	if _, ok := zed.TypeUnder(val.Type).(*zed.TypeRecord); ok {
+		return w.writeRecord(val)
+	}
+	_, err := fmt.Fprintf(w.writer, "%s\n", zeekio.FormatValue(*val))
+	return err
+}
+
+func (w *Writer) writeRecord(rec *zed.Value) error {
 	rec, err := w.flattener.Flatten(rec)
 	if err != nil {
 		return err
