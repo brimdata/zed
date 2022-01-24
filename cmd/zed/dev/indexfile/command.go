68ce9093dbc144a8100f9713815bcee1c7ea379c
Steven McCanne <steve@mccanne.com>
2022-01-24 15:55:51 -0800

    some fit and finish for Zed index files
    This commit cleans up some old unnecssary assumptions about index
    files (e.g., keys must always be present) and brings the design
    into alignment with where Zed landed.  Now that indexes are built
    with Zed programs, there was no longer a need to have both indexfile
    create and conver.  The create code was deleted (along with index.Memtable)
    and the convert code converted to create, pun intended.
    
    We also updated the command-line help with more details about
    Zed indexes and the indexfile commands.

--- cmd/zed/dev/indexfile/command.go
+++ cmd/zed/dev/indexfile/command.go
@@ -14,6 +14,51 @@ var Cmd = &charm.Spec{
 	Short: "create and search Zed indexes",
 	Long: `
 "zed dev indexfile" is command-line utility for creating and manipulating Zed indexes.
+
+A Zed index is a sectioned ZNG file organized around one or more search keys.
+The values in the first section are Zed records that are searchable according
+to their key field(s) and are are sorted in either ascending
+or descending order as determined when the file is created.
+The records are organized into a sequence of indexable chunks,
+as support by ZNG end-of-streams.
+
+The first section is followed by one or more additional sections comprising
+the hierarchy of a constant B-Tree whose order is derived from the sort key(s)
+specified.
+
+A Zed trailer provides the sizes of all the section, the keys, the sort order of the keys.
+
+Since a Zed index is just a ZNG file, its contents can be easily examined
+with "zq" or with the "section" and "trailer" sub-commands of "zed dev dig".
+
+The "zed indexfile create" command generates Zed indexes from input data.
+
+The "zed indexfile lookup" command uses a Zed index to location a given value
+of the keyed field using the B-Tree.  This command is useful for test and debug
+and not typically expected to be used from the command line.  Instead, Zed indexes
+are more typically used programatically, e.g., by a Zed lake, to index the
+data objects of a large-scale data store.
+
+In this design, each index lookup is not itself particular performant
+due to the round-trips required to traverse the B-Tree,
+but a large number of parallel index lookups hitting cached portions of
+a large index (comprised of many individual Zed indexes) performs quite well in practice.
+
+Note that any type of Zed data can comprise the input: e.g., the target
+data to search, the values of an indexed field where the data is stored
+elsewhere, pre-computed partials aggregations indexed by group-by key(s),
+and so forth.
+
+Note also that the values comprising the keyed fields may be any Zed type and
+need not be specified ahead of time.  Since the space of all Zed values has
+a total order, an indexed of mixed types has a well-defined order and searches
+work without issue across fields of heterogeneous types.
+
+Finally, the input values are assumed to be records which are accesssed to
+determine their key value.  If any inputs values are no records, an error
+is returned.  (This does not mean that non-record values cannot be indexed;
+rather, the use of indexes much use records to represent the searchable
+entities.)
 `,
 	New: New,
 }
--- index/keys.go
+++ index/keys.go
@@ -1,19 +1,15 @@
 package index
 
 import (
-	"fmt"
-
 	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/compiler"
 	"github.com/brimdata/zed/expr"
 	"github.com/brimdata/zed/field"
-	"github.com/brimdata/zed/zson"
 )
 
 type Keyer struct {
 	keys   []field.Path
 	cutter *expr.Cutter
-	valid  map[zed.Type]struct{}
 }
 
 func NewKeyer(zctx *zed.Context, keys []field.Path) (*Keyer, error) {
@@ -25,7 +21,6 @@ func NewKeyer(zctx *zed.Context, keys []field.Path) (*Keyer, error) {
 	return &Keyer{
 		keys:   keys,
 		cutter: cutter,
-		valid:  make(map[zed.Type]struct{}),
 	}, nil
 }
 
@@ -33,22 +28,10 @@ func (k *Keyer) Keys() []field.Path {
 	return k.keys
 }
 
-func (k *Keyer) KeyOf(ectx expr.Context, val *zed.Value) (*zed.Value, error) {
-	key := k.cutter.Eval(ectx, val)
-	if _, ok := k.valid[key.Type]; ok {
-		return key, nil
-	}
-	recType, ok := key.Type.(*zed.TypeRecord)
-	if !ok {
-		return nil, fmt.Errorf("index key is not a record: %s", zson.MustFormatValue(*val))
-	}
-	if _, ok := k.valid[key.Type]; !ok {
-		for _, col := range recType.Columns {
-			if _, ok := zed.TypeUnder(col.Type).(*zed.TypeError); ok {
-				return nil, fmt.Errorf("no index key field %q present in record: %s", col.Name, zson.MustFormatValue(*val))
-			}
-		}
-		k.valid[key.Type] = struct{}{}
-	}
-	return key, nil
+func (k *Keyer) valueOfKeys(ectx expr.Context, val *zed.Value) *zed.Value {
+	//XXX right now we use cutter but we might just keep the list of values?
+	// If the key isn't present, the cutter will return error missing values.
+	// I think we shouldn't error but return these when searching for null?
+	// Or why not search for missing?
+	return k.cutter.Eval(ectx, val)
 }
