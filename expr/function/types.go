8c474da57d36be9d16044a0b030ee0e04133a8f0
Steve McCanne <steve@mccanne.com>
2022-01-24 11:05:06 -0800

    move types functions into expr/function/types.go (no code changed) (#3494)

---
+++ expr/function/types.go
@@ -0,0 +1,112 @@
+package function
+
+import (
+	"github.com/brimdata/zed"
+	"github.com/brimdata/zed/zson"
+)
+
+// https://github.com/brimdata/zed/blob/main/docs/language/functions.md#typeof
+type TypeOf struct {
+	zctx *zed.Context
+}
+
+func (t *TypeOf) Call(ctx zed.Allocator, args []zed.Value) *zed.Value {
+	return ctx.CopyValue(*t.zctx.LookupTypeValue(args[0].Type))
+}
+
+type typeUnder struct {
+	zctx *zed.Context
+}
+
+func (t *typeUnder) Call(ctx zed.Allocator, args []zed.Value) *zed.Value {
+	typ := zed.TypeUnder(args[0].Type)
+	return ctx.CopyValue(*t.zctx.LookupTypeValue(typ))
+}
+
+// https://github.com/brimdata/zed/blob/main/docs/language/functions.md#nameof
+type NameOf struct {
+	zctx *zed.Context
+}
+
+func (n *NameOf) Call(ctx zed.Allocator, args []zed.Value) *zed.Value {
+	typ := args[0].Type
+	if alias, ok := typ.(*zed.TypeAlias); ok {
+		return newString(ctx, alias.Name)
+	}
+	return n.zctx.Missing()
+}
+
+// https://github.com/brimdata/zed/blob/main/docs/language/functions.md#typename
+type typeName struct {
+	zctx *zed.Context
+}
+
+func (t *typeName) Call(ectx zed.Allocator, args []zed.Value) *zed.Value {
+	if zed.TypeUnder(args[0].Type) != zed.TypeString {
+		return newErrorf(t.zctx, ectx, "typename: first argument not a string")
+	}
+	name := string(args[0].Bytes)
+	if len(args) == 1 {
+		typ := t.zctx.LookupTypeDef(name)
+		if typ == nil {
+			return t.zctx.Missing()
+		}
+		return t.zctx.LookupTypeValue(typ)
+	}
+	if zed.TypeUnder(args[1].Type) != zed.TypeType {
+		return newErrorf(t.zctx, ectx, "typename: second argument not a type value")
+	}
+	typ, err := t.zctx.LookupByValue(args[1].Bytes)
+	if err != nil {
+		return newError(t.zctx, ectx, err)
+	}
+	return t.zctx.LookupTypeValue(typ)
+}
+
+// https://github.com/brimdata/zed/blob/main/docs/language/functions.md#iserr
+type IsErr struct{}
+
+func (*IsErr) Call(_ zed.Allocator, args []zed.Value) *zed.Value {
+	if args[0].IsError() {
+		return zed.True
+	}
+	return zed.False
+}
+
+// https://github.com/brimdata/zed/blob/main/docs/language/functions.md#is
+type Is struct {
+	zctx *zed.Context
+}
+
+func (i *Is) Call(_ zed.Allocator, args []zed.Value) *zed.Value {
+	zvSubject := args[0]
+	zvTypeVal := args[1]
+	if len(args) == 3 {
+		zvSubject = args[1]
+		zvTypeVal = args[2]
+	}
+	var typ zed.Type
+	var err error
+	if zvTypeVal.IsString() {
+		typ, err = zson.ParseType(i.zctx, string(zvTypeVal.Bytes))
+	} else {
+		typ, err = i.zctx.LookupByValue(zvTypeVal.Bytes)
+	}
+	if err == nil && typ == zvSubject.Type {
+		return zed.True
+	}
+	return zed.False
+}
+
+// https://github.com/brimdata/zed/blob/main/docs/language/functions.md#quiet
+type Quiet struct {
+	zctx *zed.Context
+}
+
+func (q *Quiet) Call(_ zed.Allocator, args []zed.Value) *zed.Value {
+	val := args[0]
+	if val.IsMissing() {
+		return q.zctx.Quiet()
+	}
+	return &val
+}
