brimdata/zed

commit fd9aa63c066662a3a56d45a89c33452b1c518f35

Peeker: fill at least until min (#3375)
Peeker was previously holding up reads until it had been able to
completely fill up its entire buffer. This caused a problem when reading
from slow drip streams of data- you wouldn't see any data until the
buffer was full which may take a while when the buffer is large. Change
this behavior so Peeker.fill reads until it has at least enough data to
meet the min argument.

pkg/peeker/reader.go
@@ -47,22 +47,14 @@ func (r *Reader) fill(min int) error {
 	r.buffer = r.buffer[:cap(r.buffer)]
 	copy(r.buffer, r.cursor)
 	clen := len(r.cursor)
-	space := len(r.buffer) - clen
-	for space > 0 {
-		cc, err := r.Reader.Read(r.buffer[clen:])
-		if cc > 0 {
-			clen += cc
-			space -= cc
-		}
-		if err != nil {
-			if err == io.EOF {
-				r.eof = true
-				break
-			}
+	n, err := io.ReadAtLeast(r.Reader, r.buffer[clen:], min)
+	if err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
 			return err
 		}
+		r.eof = true
 	}
-	r.buffer = r.buffer[:clen]
+	r.buffer = r.buffer[:clen+n]
 	r.cursor = r.buffer
 	return nil
 }

commit 59222bd84dd62257f53e99af4f6466e2f693732e

fix peeker bug and rework interactive behavior
This commit fixes a bug introduced in #3375 where ReadAtLeast()
could be called with a bigger request than the buffer provided.
The design intent behind Peeker is for parsers that need to look
ahead to have a convenient and efficient API for issuing large
reads but peeking ahead tiny amounts.  The design intent was not
to cause tiny reads to percolate through the syscall interface.

The "slow drip" use case is not clarified in #3375 but presumably
has to do with receiving stats updates by the Go client library
during a long-running server query or load operation.  In this case,
the updates accumulate slowly and are not returned by the Peeker
thus defeating the purposes of stats update.

Since the design change in #3375 interferes with the more common
use case of lake scans or zq issuing bulk reads, the fix here
is to condition interactive buffering on an option that can be
enabled by the client query library.  And while we're making this
change, the bug identified above has been fixed.

api/queryio/client.go
@@ -20,7 +20,7 @@ type Query struct {
 // from res and decodes it.
 func NewQuery(res *client.Response) *Query {
 	return &Query{
-		reader: zngio.NewReader(res.Body, zed.NewContext()),
+		reader: zngio.NewReaderWithOpts(res.Body, zed.NewContext(), zngio.ReaderOpts{Interactive: true}),
 	}
 }
 

pkg/peeker/reader.go
@@ -7,10 +7,11 @@ import (
 
 type Reader struct {
 	io.Reader
-	limit  int
-	buffer []byte
-	cursor []byte
-	eof    bool
+	limit       int
+	buffer      []byte
+	cursor      []byte
+	eof         bool
+	interactive bool
 }
 
 var (
@@ -18,13 +19,14 @@ var (
 	ErrTruncated      = errors.New("truncated input")
 )
 
-func NewReader(reader io.Reader, size, max int) *Reader {
+func NewReader(reader io.Reader, size, max int, interactive bool) *Reader {
 	b := make([]byte, size)
 	return &Reader{
-		Reader: reader,
-		limit:  max,
-		buffer: b,
-		cursor: b[:0],
+		Reader:      reader,
+		limit:       max,
+		buffer:      b,
+		cursor:      b[:0],
+		interactive: interactive,
 	}
 }
 
@@ -37,17 +39,23 @@ func (r *Reader) Limit() int {
 	return r.limit
 }
 
-func (r *Reader) fill(min int) error {
-	if min > r.limit {
+func (r *Reader) fill(need int) error {
+	if need > r.limit {
 		return ErrBufferOverflow
 	}
-	if min > cap(r.buffer) {
-		r.buffer = make([]byte, min)
+	if need > cap(r.buffer) {
+		r.buffer = make([]byte, need)
 	}
 	r.buffer = r.buffer[:cap(r.buffer)]
 	copy(r.buffer, r.cursor)
 	clen := len(r.cursor)
-	n, err := io.ReadAtLeast(r.Reader, r.buffer[clen:], min)
+	var request int
+	if r.interactive {
+		request = need - clen
+	} else {
+		request = len(r.buffer) - clen
+	}
+	n, err := io.ReadAtLeast(r.Reader, r.buffer[clen:], request)
 	if err != nil {
 		if err != io.EOF && err != io.ErrUnexpectedEOF {
 			return err

zio/zng21io/reader.go
@@ -54,7 +54,7 @@ func NewReaderWithOpts(reader io.Reader, sctx *zed.Context, opts zngio.ReaderOpt
 		opts.Size = opts.Max
 	}
 	return &Reader{
-		peeker:    peeker.NewReader(reader, opts.Size, opts.Max),
+		peeker:    peeker.NewReader(reader, opts.Size, opts.Max, false),
 		converter: &converter{sctx},
 		zctx:      zed21.NewContext(),
 		mapper:    make(map[int]zed21.Type),

zio/zngio/reader.go
@@ -29,10 +29,11 @@ type Reader struct {
 var _ zbuf.ScannerAble = (*Reader)(nil)
 
 type ReaderOpts struct {
-	Validate bool
-	Size     int
-	Max      int
-	Threads  int
+	Validate    bool
+	Size        int
+	Max         int
+	Threads     int
+	Interactive bool
 }
 
 type Control struct {

zio/zngio/scanner.go
@@ -35,7 +35,7 @@ func newScanner(ctx context.Context, zctx *zed.Context, r io.Reader, filter zbuf
 		ctx:    ctx,
 		cancel: cancel,
 		parser: parser{
-			peeker: peeker.NewReader(r, opts.Size, opts.Max),
+			peeker: peeker.NewReader(r, opts.Size, opts.Max, opts.Interactive),
 			types:  NewDecoder(zctx),
 		},
 		validate:   opts.Validate,

zio/zngio/sync.go
@@ -26,7 +26,7 @@ func newScannerSync(ctx context.Context, zctx *zed.Context, r io.Reader, filter 
 		ctx:    ctx,
 		cancel: cancel,
 		parser: parser{
-			peeker: peeker.NewReader(r, opts.Size, opts.Max),
+			peeker: peeker.NewReader(r, opts.Size, opts.Max, opts.Interactive),
 			types:  NewDecoder(zctx),
 		},
 	}

pkg/peeker/reader_test.go
@@ -9,11 +9,43 @@ import (
 )
 
 func TestEOFOnEmptyFill(t *testing.T) {
-	p := NewReader(strings.NewReader("0123456789"), 1024, 1024)
+	p := NewReader(strings.NewReader("0123456789"), 1024, 1024, false)
 	n, err := p.Read(10)
 	assert.NoError(t, err)
 	assert.Len(t, n, 10)
 	n, err = p.Peek(10)
 	assert.ErrorIs(t, err, io.EOF)
 	assert.Len(t, n, 0)
+}
+
+type drip struct {
+	b []byte
+}
+
+func (d *drip) Read(b []byte) (int, error) {
+	if len(b) == 0 {
+		return 0, io.ErrShortBuffer
+	}
+	if len(d.b) == 0 {
+		return 0, io.EOF
+	}
+	b[0] = d.b[0]
+	d.b = d.b[1:]
+	return 1, nil
+}
+
+func TestInteractive(t *testing.T) {
+	p := NewReader(&drip{[]byte("0123456789")}, 1024, 1024, true)
+	b, err := p.Peek(3)
+	assert.NoError(t, err)
+	assert.Equal(t, "012", string(b))
+	assert.Equal(t, 3, len(p.buffer))
+}
+
+func TestNonInteractive(t *testing.T) {
+	p := NewReader(&drip{[]byte("0123456789")}, 1024, 1024, false)
+	b, err := p.Peek(3)
+	assert.NoError(t, err)
+	assert.Equal(t, "012", string(b))
+	assert.Equal(t, 10, len(p.buffer))
 }