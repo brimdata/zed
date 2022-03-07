c2303c0dcc65e53c37d4f911dc6800366efbabff
Noah Treuhaft <noah.treuhaft@gmail.com>
2022-03-07 08:47:55 -0500

    allow slicing for Zed bytes and string types (#3683)
    A string slice index is interpreted as a rune (i.e., Unicode code point)
    index, not a byte index.

--- runtime/expr/slice.go
+++ runtime/expr/slice.go
@@ -2,11 +2,11 @@ package expr
 
 import (
 	"errors"
+	"unicode/utf8"
 
 	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/runtime/expr/coerce"
 	"github.com/brimdata/zed/zcode"
-	"github.com/brimdata/zed/zson"
 )
 
 type Slice struct {
@@ -25,10 +25,66 @@ func NewSlice(zctx *zed.Context, elem, from, to Evaluator) *Slice {
 	}
 }
 
-var ErrSliceIndex = errors.New("array slice is not a number")
-var ErrSliceIndexEmpty = errors.New("array slice is empty")
+var ErrSliceIndex = errors.New("slice index is not a number")
+var ErrSliceIndexEmpty = errors.New("slice index is empty")
 
-func sliceIndex(ectx Context, this *zed.Value, slot Evaluator, elem *zed.Value) (int, error) {
+func (s *Slice) Eval(ectx Context, this *zed.Value) *zed.Value {
+	elem := s.elem.Eval(ectx, this)
+	if elem.IsError() {
+		return elem
+	}
+	var length int
+	switch zed.TypeUnder(elem.Type).(type) {
+	case *zed.TypeOfBytes:
+		length = len(elem.Bytes)
+	case *zed.TypeOfString:
+		length = utf8.RuneCount(elem.Bytes)
+	case *zed.TypeArray:
+		n, err := elem.ContainerLength()
+		if err != nil {
+			panic(err)
+		}
+		length = n
+	default:
+		return s.zctx.WrapError("sliced value is not array, bytes, or string", elem)
+	}
+	if elem.IsNull() {
+		return elem
+	}
+	from, err := sliceIndex(ectx, this, s.from, length)
+	if err != nil && err != ErrSliceIndexEmpty {
+		return s.zctx.NewError(err)
+	}
+	to, err := sliceIndex(ectx, this, s.to, length)
+	if err != nil {
+		if err != ErrSliceIndexEmpty {
+			return s.zctx.NewError(err)
+		}
+		to = length
+	}
+	bytes := elem.Bytes
+	switch zed.TypeUnder(elem.Type).(type) {
+	case *zed.TypeOfBytes:
+		bytes = bytes[from:to]
+	case *zed.TypeOfString:
+		bytes = bytes[utf8PrefixLen(bytes, from):]
+		bytes = bytes[:utf8PrefixLen(bytes, to-from)]
+	case *zed.TypeArray:
+		it := bytes.Iter()
+		for k := 0; k < to && !it.Done(); k++ {
+			if k == from {
+				bytes = zcode.Bytes(it)
+			}
+			it.Next()
+		}
+		bytes = bytes[:len(bytes)-len(it)]
+	default:
+		panic(elem.Type)
+	}
+	return ectx.NewValue(elem.Type, bytes)
+}
+
+func sliceIndex(ectx Context, this *zed.Value, slot Evaluator, length int) (int, error) {
 	if slot == nil {
 		//XXX
 		return 0, ErrSliceIndexEmpty
@@ -40,53 +96,30 @@ func sliceIndex(ectx Context, this *zed.Value, slot Evaluator, elem *zed.Value) 
 	}
 	index := int(v)
 	if index < 0 {
-		n, err := elem.ContainerLength()
-		if err != nil {
-			return 0, err
-		}
-		index += n
+		index += length
+	}
+	if index < 0 {
+		return 0, nil
+	}
+	if index > length {
+		return length, nil
 	}
 	return index, nil
 }
 
-func (s *Slice) Eval(ectx Context, this *zed.Value) *zed.Value {
-	elem := s.elem.Eval(ectx, this)
-	if elem.IsError() {
-		return elem
-	}
-	if _, ok := zed.TypeUnder(elem.Type).(*zed.TypeArray); !ok {
-		// XXX use structured error
-		return s.zctx.NewErrorf("sliced value is not an array: %s", zson.MustFormatValue(*elem))
-	}
-	if elem.IsNull() {
-		// If array is null, just return the null array.
-		return elem
-	}
-	from, err := sliceIndex(ectx, this, s.from, elem)
-	if err != nil && err != ErrSliceIndexEmpty {
-		return s.zctx.NewError(err)
-	}
-	to, err := sliceIndex(ectx, this, s.to, elem)
-	if err != nil {
-		if err != ErrSliceIndexEmpty {
-			return s.zctx.NewError(err)
+// utf8PrefixLen returns the length in bytes of the first runeCount runes in b.
+// It returns 0 if runeCount<0 and len(b) if runeCount>utf8.RuneCount(b).
+func utf8PrefixLen(b []byte, runeCount int) int {
+	var i, runeCurrent int
+	for {
+		if runeCurrent >= runeCount {
+			return i
 		}
-		n, err := elem.ContainerLength()
-		if err != nil {
-			panic(err)
+		r, n := utf8.DecodeRune(b[i:])
+		if r == utf8.RuneError && n == 0 {
+			return i
 		}
-		to = int(n)
+		i += n
+		runeCurrent++
 	}
-	bytes := elem.Bytes
-	it := bytes.Iter()
-	if from < 0 {
-		from = 0
-	}
-	for k := 0; k < to && !it.Done(); k++ {
-		if k == from {
-			bytes = zcode.Bytes(it)
-		}
-		it.Next()
-	}
-	return ectx.NewValue(elem.Type, bytes[:len(bytes)-len(it)])
 }
