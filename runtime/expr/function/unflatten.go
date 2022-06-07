4a06bcbd4b8c9b7ce2ace619267bc7db781bc8c9
Matthew Nibecker <hello@mattnibecker.com>
2022-06-07 12:33:28 -0400

    unflatten: fix panic on invalid key types (#3926)
    Fix issue where unflatten would panic when encountering an unsupported
    key type. Also add support so that key can be a string and return an
    error value if an invalid key type is encountered.

--- runtime/expr/function/unflatten.go
+++ runtime/expr/function/unflatten.go
@@ -1,9 +1,12 @@
 package function
 
 import (
+	"fmt"
+
 	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/pkg/field"
 	"github.com/brimdata/zed/zcode"
+	"github.com/brimdata/zed/zson"
 )
 
 // https://github.com/brimdata/zed/blob/main/docs/language/functions.md#unflatten
@@ -36,49 +39,65 @@ func (u *Unflatten) Call(ctx zed.Allocator, args []zed.Value) *zed.Value {
 	u.types = u.types[:0]
 	u.values = u.values[:0]
 	for it := val.Bytes.Iter(); !it.Done(); {
-		path, typ, vb := u.parseElem(array.Type, it.Next())
+		bytes := it.Next()
+		path, typ, vb, err := u.parseElem(array.Type, bytes)
+		if err != nil {
+			return u.zctx.WrapError(err.Error(), ctx.NewValue(array.Type, bytes))
+		}
 		if typ == nil {
 			continue
 		}
-		root.addPath(&u.recordCache, path)
+		if removed := root.addPath(&u.recordCache, path); removed > 0 {
+			u.types = u.types[:len(u.types)-removed]
+			u.values = u.values[:len(u.values)-removed]
+		}
 		u.types = append(u.types, typ)
 		u.values = append(u.values, vb)
 	}
 	u.builder.Reset()
 	types, values := u.types, u.values
-	typ := root.build(u.zctx, &u.builder, func() (zed.Type, zcode.Bytes) {
+	typ, err := root.build(u.zctx, &u.builder, func() (zed.Type, zcode.Bytes) {
 		typ, value := types[0], values[0]
 		types, values = types[1:], values[1:]
 		return typ, value
 	})
+	if err != nil {
+		return u.zctx.WrapError(err.Error(), &val)
+	}
 	return ctx.NewValue(typ, u.builder.Bytes())
 }
 
-func (u *Unflatten) parseElem(inner zed.Type, vb zcode.Bytes) (field.Path, zed.Type, zcode.Bytes) {
+func (u *Unflatten) parseElem(inner zed.Type, vb zcode.Bytes) (field.Path, zed.Type, zcode.Bytes, error) {
 	if union, ok := zed.TypeUnder(inner).(*zed.TypeUnion); ok {
 		inner, vb = union.SplitZNG(vb)
 	}
 	typ := zed.TypeRecordOf(inner)
 	if typ == nil || len(typ.Columns) != 2 {
-		return nil, nil, nil
+		return nil, nil, nil, nil
 	}
 	nkey, ok := typ.ColumnOfField("key")
 	if !ok {
-		return nil, nil, nil
+		return nil, nil, nil, nil
 	}
-	if a, ok := zed.TypeUnder(typ.Columns[nkey].Type).(*zed.TypeArray); !ok && a.Type != zed.TypeString {
-		return nil, nil, nil
-	}
+
 	vtyp, ok := typ.TypeOfField("value")
 	if !ok {
-		return nil, nil, nil
+		return nil, nil, nil, nil
 	}
 	it := vb.Iter()
 	kbytes, vbytes := it.Next(), it.Next()
 	if nkey == 1 {
 		kbytes, vbytes = vbytes, kbytes
 	}
-	return u.decodeKey(kbytes), vtyp, vbytes
+	ktyp := typ.Columns[nkey].Type
+	if ktyp.ID() == zed.IDString {
+		u.path = append(u.path[:0], zed.DecodeString(kbytes))
+		return u.path, vtyp, vbytes, nil
+	}
+	if a, ok := zed.TypeUnder(ktyp).(*zed.TypeArray); ok && a.Type.ID() == zed.IDString {
+		return u.decodeKey(kbytes), vtyp, vbytes, nil
+	}
+	return nil, nil, nil, fmt.Errorf("invalid key type %s: expected either string or [string]", zson.FormatType(ktyp))
 }
 
 func (u *Unflatten) decodeKey(b zcode.Bytes) field.Path {
@@ -114,22 +133,44 @@ type record struct {
 	records []*record
 }
 
-func (r *record) addPath(c *recordCache, p []string) {
+func (r *record) addPath(c *recordCache, p []string) (removed int) {
 	if len(p) == 0 {
-		return
+		return 0
 	}
-	if len(r.columns) == 0 || r.columns[len(r.columns)-1].Name != p[0] {
+	at := len(r.columns) - 1
+	if len(r.columns) == 0 || r.columns[at].Name != p[0] {
 		r.columns = append(r.columns, zed.NewColumn(p[0], nil))
 		var rec *record
 		if len(p) > 1 {
 			rec = c.new()
 		}
 		r.records = append(r.records, rec)
+	} else if len(p) == 1 || r.records[at] == nil {
+		// If this isn't a new column and we're either at a leaf or the
+		// previously value was a leaf, we're stacking on a previously created
+		// record and need to signal that values have been removed.
+		removed = r.records[at].countLeaves()
+		if len(p) > 1 {
+			r.records[at] = c.new()
+		} else {
+			r.records[at] = nil
+		}
 	}
-	r.records[len(r.records)-1].addPath(c, p[1:])
+	return removed + r.records[len(r.records)-1].addPath(c, p[1:])
 }
 
-func (r *record) build(zctx *zed.Context, b *zcode.Builder, next func() (zed.Type, zcode.Bytes)) zed.Type {
+func (r *record) countLeaves() int {
+	if r == nil {
+		return 1
+	}
+	var count int
+	for _, rec := range r.records {
+		count += rec.countLeaves()
+	}
+	return count
+}
+
+func (r *record) build(zctx *zed.Context, b *zcode.Builder, next func() (zed.Type, zcode.Bytes)) (zed.Type, error) {
 	for i, rec := range r.records {
 		if rec == nil {
 			typ, value := next()
@@ -138,8 +179,12 @@ func (r *record) build(zctx *zed.Context, b *zcode.Builder, next func() (zed.Typ
 			continue
 		}
 		b.BeginContainer()
-		r.columns[i].Type = rec.build(zctx, b, next)
+		var err error
+		r.columns[i].Type, err = rec.build(zctx, b, next)
+		if err != nil {
+			return nil, err
+		}
 		b.EndContainer()
 	}
-	return zctx.MustLookupTypeRecord(r.columns)
+	return zctx.LookupTypeRecord(r.columns)
 }
