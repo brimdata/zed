953cd0912c3d5da2c349f1ef70094772cb8a8e9e
Steven McCanne <steve@mccanne.com>
2022-07-17 19:41:09 -0700

    prune bug down to small scope

--- bug/main.go
+++ bug/main.go
@@ -1,158 +1,48 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"math/rand"
-	"os"
-	"time"
 
 	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/zcode"
-	"github.com/brimdata/zed/zio/zngio"
 	"github.com/brimdata/zed/zson"
-	"github.com/kr/pretty"
 )
 
-const NumRecords = 100
-const NumRuns = 20
-
-func dump(b []byte) {
-	f, err := os.OpenFile("t.zng", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0644)
-	if err != nil {
-		panic(err)
-	}
-	f.Write(b)
-	f.Close()
-}
+const NumRecords = 4
 
 func main() {
-	/*f, err := os.Create("PROF")
-	if err != nil {
-		panic(err)
-	}
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()*/
-
-	zedThings := make([]Thing, NumRecords)
+	things := make([]Thing, NumRecords)
 	for i := 0; i < NumRecords; i++ {
-		zedThings[i] = Make()
+		things[i] = Make()
 	}
-	//fmt.Println(MarshalZSON(zedThings))
-	dump(MarshalZNG(zedThings))
-	return
-	jsonThings := convertToJSON(zedThings)
-	run("MarshalJSON", func() {
-		MarshalJSON(jsonThings)
-	})
-	encodedJSON := MarshalJSON(jsonThings)
-	run("UnmarshalJSON", func() {
-		UnmarshalJSON(encodedJSON)
-	})
-	fmt.Println("JSON size", len(encodedJSON))
-	run("MashalZSON", func() {
-		MarshalZSON(zedThings)
-	})
-	encodedZSON := MarshalZSON(zedThings)
-	run("UnmarshalZSON", func() {
-		UnmarshalZSON(encodedZSON)
-	})
-	fmt.Println("ZSON size", len(encodedZSON))
-	run("MarshalZNG", func() {
-		MarshalZNG(zedThings)
-	})
-	encodedZNG := MarshalZNG(zedThings)
-	run("UnmarshalZNG", func() {
-		UnmarshalZNG(encodedZNG)
-	})
-	fmt.Println("ZNG size", len(encodedZNG))
-}
-
-func run(which string, marshal func()) {
-	start := time.Now()
-	for i := 0; i < NumRuns; i++ {
-		marshal()
-	}
-	fmt.Println(which, time.Since(start))
-}
-
-func MarshalJSON(things []JSONThing) []byte {
-	for i := 0; i < 10; i++ {
-		pretty.Println(things[i])
-	}
-	b, err := json.Marshal(things)
-	if err != nil {
-		panic(err)
-	}
-	return b
-}
-
-func MarshalZSON(vals []Thing) string {
-	m := zson.NewMarshaler()
-	m.Decorate(zson.StyleSimple)
-	s, err := m.Marshal(vals)
-	if err != nil {
-		panic(err)
-	}
-	return s
-}
-
-func UnmarshalZSON(s string) []Thing {
-	u := zson.NewUnmarshaler()
-	u.Bind(ThingA{}, ThingB{}, ThingC{})
-	var things []Thing
-	if err := u.Unmarshal(s, &things); err != nil {
-		panic(err)
-	}
-	return things
-}
-
-func MarshalZNG(things []Thing) []byte {
 	m := zson.NewZNGMarshaler()
-	var out bytes.Buffer
-	writer := zngio.NewWriter(&NopCloser{&out})
 	m.Decorate(zson.StyleSimple)
 	val, err := m.Marshal(things)
 	if err != nil {
 		panic(err)
 	}
 	dumpVal(val)
-	if err := writer.Write(val); err != nil {
+	return
+	m = zson.NewZNGMarshaler()
+	m.Decorate(zson.StyleSimple)
+	val, err = m.Marshal(things[:99])
+	if err != nil {
 		panic(err)
 	}
-	if err := writer.Close(); err != nil {
-		panic(err)
-	}
-	return out.Bytes()
+	dumpVal(val)
 }
 
 func dumpVal(val *zed.Value) {
 	zed.Walk(val.Type, val.Bytes, func(typ zed.Type, bytes zcode.Bytes) error {
 		if zed.IsContainerType(typ) {
-			fmt.Println(zson.FormatType(typ))
+			fmt.Println("CONT", zson.FormatType(typ))
 			return nil
 		}
-		fmt.Println(zson.String(zed.NewValue(typ, bytes)))
+		fmt.Println("PRIM", zson.String(zed.NewValue(typ, bytes)))
 		return nil
 	})
-	panic("x")
-}
-
-func UnmarshalZNG(b []byte) []Thing {
-	u := zson.NewZNGUnmarshaler()
-	u.Bind(ThingA{}, ThingB{}, ThingC{})
-	reader := zngio.NewReader(zed.NewContext(), bytes.NewReader(b))
-	val, err := reader.Read()
-	if err != nil {
-		panic(err)
-	}
-	var things []Thing
-	if err := u.Unmarshal(val, &things); err != nil {
-		panic(err)
-	}
-	return things
 }
 
 type Thing interface {
@@ -230,119 +120,6 @@ func (t *ThingC) GetID() int64 {
 	return t.ID
 }
 
-type JSONThing interface {
-	GetID() int64
-}
-
-func convertToJSON(things []Thing) []JSONThing {
-	out := make([]JSONThing, 0, len(things))
-	for _, thing := range things {
-		switch thing := thing.(type) {
-		case *ThingA:
-			out = append(out, &JSONThingA{
-				Kind:  "A",
-				ID:    thing.ID,
-				Name:  thing.Name,
-				Stuff: thing.Stuff,
-			})
-		case *ThingB:
-			out = append(out, &JSONThingB{
-				Kind:  "B",
-				ID:    thing.ID,
-				Name:  thing.Name,
-				Stuff: thing.Stuff,
-			})
-		case *ThingC:
-			out = append(out, &JSONThingC{
-				Kind:  "C",
-				ID:    thing.ID,
-				Name:  thing.Name,
-				Stuff: thing.Stuff,
-			})
-		default:
-			panic("bad type in convertToJSON")
-		}
-	}
-	return out
-}
-
-type JSONThingA struct {
-	Kind  string
-	ID    int64
-	Name  string
-	Stuff []int64
-}
-
-func (j *JSONThingA) GetID() int64 {
-	return j.ID
-}
-
-type JSONThingB struct {
-	Kind  string
-	ID    int64
-	Name  string
-	Stuff []int8
-}
-
-func (j *JSONThingB) GetID() int64 {
-	return j.ID
-}
-
-type JSONThingC struct {
-	Kind  string
-	ID    int64
-	Name  string
-	Stuff []bool
-}
-
-func (j *JSONThingC) GetID() int64 {
-	return j.ID
-}
-
-type Header struct {
-	Kind string
-}
-
-func UnmarshalJSON(b []byte) []JSONThing {
-	var rawMessages []*json.RawMessage
-	if err := json.Unmarshal(b, &rawMessages); err != nil {
-		panic(err)
-	}
-	var things []JSONThing
-	for _, message := range rawMessages {
-		var hdr Header
-		if err := json.Unmarshal(*message, &hdr); err != nil {
-			panic(err)
-		}
-		var thing JSONThing
-		switch hdr.Kind {
-		case "A":
-			var a JSONThingA
-			if err := json.Unmarshal(*message, &a); err != nil {
-				panic(err)
-			}
-			thing = &a
-		case "B":
-			var b JSONThingB
-			if err := json.Unmarshal(*message, &b); err != nil {
-				panic(err)
-			}
-			thing = &b
-		case "C":
-			var c JSONThingC
-			if err := json.Unmarshal(*message, &c); err != nil {
-				panic(err)
-			}
-			thing = &c
-		default:
-			fmt.Println(string(*message))
-			panic("bad kind in JSON unmarshaler: " + hdr.Kind)
-		}
-		things = append(things, thing)
-	}
-	return things
-}
-
 var names = []string{
 	"Redding",
 	"Burney",
@@ -372,10 +149,10 @@ func NewInt64s() []int64 {
 }
 
 func NewInt8s() []int8 {
-	spread := rand.Intn(200) + 1
+	spread := rand.Intn(80) + 10
 	vals := make([]int8, rand.Intn(10))
 	for i := 0; i < len(vals); i++ {
-		vals[i] = int8(rand.Intn(spread))
+		vals[i] = int8(rand.Intn(spread) - spread/2)
 	}
 	return vals
 }
