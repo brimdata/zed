3c52a824d5b27723ffd76bda1cadb79c8d925900
Noah Treuhaft <noah.treuhaft@gmail.com>
2022-12-09 21:32:35 -0500

    zio/zstio: remove pkg/storage.Reader requirement
    NewReader requires its io.Reader parameter to implement
    pkg/storage.Reader.  Remove that requirement.

--- zio/zstio/reader.go
+++ zio/zstio/reader.go
@@ -10,24 +10,21 @@ import (
 )
 
 func NewReader(zctx *zed.Context, r io.Reader) (*zst.Reader, error) {
-	reader, ok := r.(storage.Reader)
-	if ok {
-		if !storage.IsSeekable(reader) {
-			return nil, errors.New("zst must be used with a seekable input")
-		}
-		size, err := storage.Size(reader)
-		if err != nil {
-			return nil, err
-		}
-		o, err := zst.NewObject(zctx, reader, size)
-		if err != nil {
-			return nil, err
-		}
-		return zst.NewReader(o)
+	s, ok := r.(io.Seeker)
+	if sr, ok2 := r.(storage.Reader); !ok || (ok2 && !storage.IsSeekable(sr)) {
+		return nil, errors.New("zst must be used with a seekable input")
 	}
-	// This can't be the zed system (which always using package storage)
-	// so it must be a third party using he zst library.  We could assert
-	// for io.ReaderAt and io.Seeker and use the seeker to get the size
-	// and make a zst reader this way, but for now, we just say this is not supported.
-	return nil, errors.New("zst does not yet support non-storage package implementations")
+	ra, ok := r.(io.ReaderAt)
+	if !ok {
+		return nil, errors.New("zst must be used with an io.ReaderAt")
+	}
+	size, err := s.Seek(0, io.SeekEnd)
+	if err != nil {
+		return nil, err
+	}
+	o, err := zst.NewObject(zctx, ra, size)
+	if err != nil {
+		return nil, err
+	}
+	return zst.NewReader(o)
 }
