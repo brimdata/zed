34c9ad69916ee7ed29d3a5ebbc6fc7f847577ee4
Noah Treuhaft <noah.treuhaft@gmail.com>
2022-12-29 18:37:37 -0500

    zio/anyio: alphabetize format lists
    Format lists -- particularly lenthy ones in switch statements -- are
    easier to scan this way.

--- zio/anyio/lookup.go
+++ zio/anyio/lookup.go
@@ -26,28 +26,28 @@ func lookupReader(zctx *zed.Context, r io.Reader, opts ReaderOpts) (zio.ReadClos
 		return zio.NopReadCloser(csvio.NewReader(zctx, r)), nil
 	case "line":
 		return zio.NopReadCloser(lineio.NewReader(r)), nil
-	case "zeek":
-		return zio.NopReadCloser(zeekio.NewReader(zctx, r)), nil
 	case "json":
 		return zio.NopReadCloser(jsonio.NewReader(zctx, r)), nil
-	case "zjson":
-		return zio.NopReadCloser(zjsonio.NewReader(zctx, r)), nil
-	case "zng":
-		return zngio.NewReaderWithOpts(zctx, r, opts.ZNG), nil
-	case "zson":
-		return zio.NopReadCloser(zsonio.NewReader(zctx, r)), nil
-	case "vng":
-		zr, err := vngio.NewReader(zctx, r)
-		if err != nil {
-			return nil, err
-		}
-		return zio.NopReadCloser(zr), nil
 	case "parquet":
 		zr, err := parquetio.NewReader(zctx, r)
 		if err != nil {
 			return nil, err
 		}
 		return zio.NopReadCloser(zr), nil
+	case "vng":
+		zr, err := vngio.NewReader(zctx, r)
+		if err != nil {
+			return nil, err
+		}
+		return zio.NopReadCloser(zr), nil
+	case "zeek":
+		return zio.NopReadCloser(zeekio.NewReader(zctx, r)), nil
+	case "zjson":
+		return zio.NopReadCloser(zjsonio.NewReader(zctx, r)), nil
+	case "zng":
+		return zngio.NewReaderWithOpts(zctx, r, opts.ZNG), nil
+	case "zson":
+		return zio.NopReadCloser(zsonio.NewReader(zctx, r)), nil
 	}
 	return nil, fmt.Errorf("no such format: \"%s\"", opts.Format)
 }
--- zio/anyio/writer.go
+++ zio/anyio/writer.go
@@ -23,42 +23,42 @@ import (
 type WriterOpts struct {
 	Format string
 	Lake   lakeio.WriterOpts
+	VNG    vngio.WriterOpts
 	ZNG    *zngio.WriterOpts // Nil means use defaults via zngio.NewWriter.
 	ZSON   zsonio.WriterOpts
-	VNG    vngio.WriterOpts
 }
 
 func NewWriter(w io.WriteCloser, opts WriterOpts) (zio.WriteCloser, error) {
 	switch opts.Format {
 	case "arrows":
 		return arrowio.NewWriter(w), nil
+	case "csv":
+		return csvio.NewWriter(w), nil
+	case "json":
+		return jsonio.NewWriter(w), nil
+	case "lake":
+		return lakeio.NewWriter(w, opts.Lake), nil
 	case "null":
 		return &nullWriter{}, nil
+	case "parquet":
+		return parquetio.NewWriter(w), nil
+	case "table":
+		return tableio.NewWriter(w), nil
+	case "text":
+		return textio.NewWriter(w), nil
+	case "vng":
+		return vngio.NewWriter(w, opts.VNG)
+	case "zeek":
+		return zeekio.NewWriter(w), nil
+	case "zjson":
+		return zjsonio.NewWriter(w), nil
 	case "zng":
 		if opts.ZNG == nil {
 			return zngio.NewWriter(w), nil
 		}
 		return zngio.NewWriterWithOpts(w, *opts.ZNG), nil
-	case "zeek":
-		return zeekio.NewWriter(w), nil
-	case "json":
-		return jsonio.NewWriter(w), nil
-	case "zjson":
-		return zjsonio.NewWriter(w), nil
 	case "zson", "":
 		return zsonio.NewWriter(w, opts.ZSON), nil
-	case "vng":
-		return vngio.NewWriter(w, opts.VNG)
-	case "text":
-		return textio.NewWriter(w), nil
-	case "table":
-		return tableio.NewWriter(w), nil
-	case "csv":
-		return csvio.NewWriter(w), nil
-	case "parquet":
-		return parquetio.NewWriter(w), nil
-	case "lake":
-		return lakeio.NewWriter(w, opts.Lake), nil
 	default:
 		return nil, fmt.Errorf("unknown format: %s", opts.Format)
 	}
