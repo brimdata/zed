brimdata/zed

commit 02302b2fa15a13045a5967505ba264c6f9daaea9

ztest: set Bundle.FileName to relative path in Load (#4265)
ztest.Load sets Bundle.FileName to an absolute path under the assumption
that this makes the YAML file easier to find.  I don't think it does, so
just set the field to the (shorter) relative path.

Before:

    $ make test-system
    --- FAIL: TestZed (4.77s)
        --- FAIL: TestZed/ztests (0.00s)
            --- FAIL: TestZed/ztests/fail (0.01s)
                ztest.go:411: /Users/noah/brimdata/zed/ztests/fail.yaml: exit status 1
    FAIL
    FAIL	github.com/brimdata/zed	15.275s
    FAIL
    make: *** [test-system] Error 1

After:

    $ make test-system
    --- FAIL: TestZed (4.75s)
        --- FAIL: TestZed/ztests (0.00s)
            --- FAIL: TestZed/ztests/fail (0.01s)
                ztest.go:407: ztests/fail.yaml: exit status 1
    FAIL
    FAIL	github.com/brimdata/zed	14.166s
    FAIL
    make: *** [test-system] Error 1

ztest/ztest.go
@@ -174,12 +174,8 @@ func Load(dirname string) ([]Bundle, error) {
 			continue
 		}
 		testname := strings.TrimSuffix(filename, dotyaml)
-		// An absolute path in errors makes the offending file easier to find.
-		filename, err := filepath.Abs(filepath.Join(dirname, filename))
-		var zt *ZTest
-		if err == nil {
-			zt, err = FromYAMLFile(filename)
-		}
+		filename = filepath.Join(dirname, filename)
+		zt, err := FromYAMLFile(filename)
 		bundles = append(bundles, Bundle{testname, filename, zt, err})
 	}
 	return bundles, nil

commit 2a150b8b7a99a4e01b28061f55735ac87c523933

ztest: remove broken symlink input support
Symlink inputs in script-style ztests have been broken since #4265.
They clearly aren't getting much use since no one has noticed the
breakage, so remove support for them.

ztest/ztest.go
@@ -219,9 +219,6 @@ type File struct {
 	// Re is a regular expression describing the contents of the file,
 	// which is only applicable to output files.
 	Re string `yaml:"regexp,omitempty"`
-	// Symlink creates a symlink on the specified directory into a test's local
-	// directory. Only applicable to input files.
-	Symlink string `yaml:"symlink,omitempty"`
 }
 
 func (f *File) check() error {
@@ -230,9 +227,6 @@ func (f *File) check() error {
 		cnt++
 	}
 	if f.Source != "" {
-		cnt++
-	}
-	if f.Symlink != "" {
 		cnt++
 	}
 	if cnt > 1 {
@@ -252,10 +246,6 @@ func (f *File) load(dir string) ([]byte, *regexp.Regexp, error) {
 	if f.Re != "" {
 		re, err := regexp.Compile(f.Re)
 		return nil, re, err
-	}
-	if f.Symlink != "" {
-		f.Symlink = filepath.Join(dir, f.Symlink)
-		return nil, nil, nil
 	}
 	b, err := os.ReadFile(filepath.Join(dir, f.Name))
 	if err == nil {
@@ -305,9 +295,6 @@ func (z *ZTest) check() error {
 		for _, f := range z.Outputs {
 			if err := f.check(); err != nil {
 				return err
-			}
-			if f.Symlink != "" {
-				return fmt.Errorf("%s: cannot use symlink in an output", f.Name)
 			}
 		}
 	} else if z.Zed == "" {
@@ -432,12 +419,6 @@ func runsh(path, testDir, tempDir string, zt *ZTest) error {
 		b, _, err := f.load(testDir)
 		if err != nil {
 			return err
-		}
-		if f.Symlink != "" {
-			if err := os.Symlink(f.Symlink, filepath.Join(tempDir, f.Name)); err != nil {
-				return err
-			}
-			continue
 		}
 		if f.Name == "stdin" {
 			stdin = bytes.NewReader(b)