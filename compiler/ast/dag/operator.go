a993388c34e09e358bb4725f63f350390aef881a
Noah Treuhaft <noah.treuhaft@gmail.com>
2023-02-28 16:45:01 -0500

    compiler/ast/dag: move Agg to expr.go (#4407)
    It's an Expr, not an Op.

--- compiler/ast/dag/operator.go
+++ compiler/ast/dag/operator.go
@@ -238,12 +238,6 @@ func (*From) OpNode() {}
 // Various Op fields
 
 type (
-	Agg struct {
-		Kind  string `json:"kind" unpack:""`
-		Name  string `json:"name"`
-		Expr  Expr   `json:"expr"`
-		Where Expr   `json:"where"`
-	}
 	Case struct {
 		Expr Expr `json:"expr"`
 		Op   Op   `json:"op"`
