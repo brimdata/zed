ee6805c345aa484d489457f7553086fb94055f30
Matthew Nibecker <hello@mattnibecker.com>
2023-05-18 09:48:35 -0700

    Remove zed error when files is not found

--- runtime/op/meta/sequence.go
+++ runtime/op/meta/sequence.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"io"
-	"io/fs"
 
 	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/lake"
@@ -148,12 +147,6 @@ func newObjectScanner(ctx context.Context, zctx *zed.Context, pool *lake.Pool, o
 	}
 	rc, err := object.NewReader(ctx, pool.Storage(), pool.DataPath, ranges)
 	if err != nil {
-		if errors.Is(err, fs.ErrNotExist) {
-			// If the object does not exist return a zbuf.Puller that returns a
-			// single value- a zed error.
-			vals := zbuf.NewArray([]zed.Value{*zctx.NewError(err)})
-			return zbuf.NewPuller(vals), nil
-		}
 		return nil, err
 	}
 	scanner, err := zngio.NewReader(zctx, rc).NewScanner(ctx, filter)
