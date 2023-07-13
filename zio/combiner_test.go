brimdata/zed

commit 114f2e54fa955b4598caa859dadfc7e27d1ba83a

Use more golang.org/x/exp/slices functions (#4699)

zio/combiner.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/brimdata/zed"
+	"golang.org/x/exp/slices"
 )
 
 // A Combiner is a Reader that returns records by reading from multiple Readers.
@@ -60,12 +61,7 @@ func (c *Combiner) run() {
 }
 
 func (c *Combiner) finished() bool {
-	for i := range c.done {
-		if !c.done[i] {
-			return false
-		}
-	}
-	return true
+	return slices.Contains(c.done, false)
 }
 
 func (c *Combiner) Read() (*zed.Value, error) {

cli/queryflags/flags.go
@@ -12,6 +12,7 @@ import (
 	"github.com/brimdata/zed/zbuf"
 	"github.com/brimdata/zed/zfmt"
 	"github.com/brimdata/zed/zson"
+	"golang.org/x/exp/slices"
 )
 
 type Flags struct {
@@ -83,12 +84,7 @@ func isURLWithKnownScheme(path string, schemes ...string) bool {
 	if err != nil {
 		return false
 	}
-	for _, s := range schemes {
-		if u.Scheme == s {
-			return true
-		}
-	}
-	return false
+	return slices.Contains(schemes, u.Scheme)
 }
 
 func (f *Flags) PrintStats(stats zbuf.Progress) {

compiler/kernel/op.go
@@ -39,6 +39,7 @@ import (
 	"github.com/brimdata/zed/zio"
 	"github.com/brimdata/zed/zson"
 	"github.com/segmentio/ksuid"
+	"golang.org/x/exp/slices"
 )
 
 var ErrJoinParents = errors.New("join requires two upstream parallel query paths")
@@ -707,15 +708,9 @@ func isEntry(seq dag.Seq) bool {
 	case *dag.UserOpCall:
 		return isEntry(op.Body)
 	case *dag.Fork:
-		if len(op.Paths) == 0 {
-			return false
-		}
-		for _, seq := range op.Paths {
-			if !isEntry(seq) {
-				return false
-			}
-		}
-		return true
+		return len(op.Paths) > 0 && !slices.ContainsFunc(op.Paths, func(seq dag.Seq) bool {
+			return !isEntry(seq)
+		})
 	}
 	return false
 }

pkg/field/field.go
@@ -2,6 +2,8 @@ package field
 
 import (
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 type Path []string
@@ -18,21 +20,7 @@ func (p Path) Leaf() string {
 }
 
 func (p Path) Equal(to Path) bool {
-	if p == nil {
-		return to == nil
-	}
-	if to == nil {
-		return false
-	}
-	if len(p) != len(to) {
-		return false
-	}
-	for k := range p {
-		if p[k] != to[k] {
-			return false
-		}
-	}
-	return true
+	return slices.Equal(p, to)
 }
 
 func (p Path) IsEmpty() bool {
@@ -52,12 +40,7 @@ func (p Path) In(list List) bool {
 }
 
 func (p Path) HasPrefixIn(set []Path) bool {
-	for _, item := range set {
-		if p.HasPrefix(item) {
-			return true
-		}
-	}
-	return false
+	return slices.ContainsFunc(set, p.HasPrefix)
 }
 
 func Dotted(s string) Path {
@@ -83,22 +66,11 @@ func (l List) String() string {
 }
 
 func (l List) Has(in Path) bool {
-	for _, f := range l {
-		if f.Equal(in) {
-			return true
-		}
-	}
-	return false
+	return slices.ContainsFunc(l, in.Equal)
 }
 
 func (l List) Equal(to List) bool {
-	if len(l) != len(to) {
-		return false
-	}
-	for k, f := range l {
-		if !f.Equal(to[k]) {
-			return false
-		}
-	}
-	return true
+	return slices.EqualFunc(l, to, func(a, b Path) bool {
+		return a.Equal(b)
+	})
 }

recordbuilder.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/brimdata/zed/pkg/field"
 	"github.com/brimdata/zed/zcode"
+	"golang.org/x/exp/slices"
 )
 
 // fieldInfo encodes the structure of a particular proc that writes a
@@ -70,7 +71,7 @@ func NewRecordBuilder(zctx *Context, fields field.List) (*RecordBuilder, error) 
 		// figure out which records we are stepping in and out of.
 		record := names[:len(names)-1]
 		var containerBegins []string
-		if !sameRecord(record, currentRecord) {
+		if !slices.Equal(record, currentRecord) {
 			// currentRecord is what nested record the zcode.Builder
 			// is currently working on, record is the nested
 			// record for the current field.  First figure out
@@ -146,18 +147,6 @@ func isIn(fieldname field.Path, fis []fieldInfo) bool {
 		}
 	}
 	return false
-}
-
-func sameRecord(names1, names2 []string) bool {
-	if len(names1) != len(names2) {
-		return false
-	}
-	for i := range names1 {
-		if names1[i] != names2[i] {
-			return false
-		}
-	}
-	return true
 }
 
 func (r *RecordBuilder) Reset() {

runtime/expr/putter.go
@@ -6,6 +6,7 @@ import (
 	"github.com/brimdata/zed"
 	"github.com/brimdata/zed/pkg/field"
 	"github.com/brimdata/zed/zcode"
+	"golang.org/x/exp/slices"
 )
 
 // Putter is an Evaluator that modifies the record stream with computed values.
@@ -267,21 +268,9 @@ func (p *Putter) deriveRecordSteps(parentPath field.Path, inFields []zed.Field, 
 }
 
 func hasField(name string, fields []zed.Field) bool {
-	for _, f := range fields {
-		if f.Name == name {
-			return true
-		}
-	}
-	return false
-}
-
-func sameTypes(types []zed.Type, vals []zed.Value) bool {
-	for k, typ := range types {
-		if vals[k].Type != typ {
-			return false
-		}
-	}
-	return true
+	return slices.ContainsFunc(fields, func(f zed.Field) bool {
+		return f.Name == name
+	})
 }
 
 func (p *Putter) lookupRule(inType *zed.TypeRecord, vals []zed.Value, clauses []Assignment) putRule {
@@ -297,6 +286,12 @@ func (p *Putter) lookupRule(inType *zed.TypeRecord, vals []zed.Value, clauses []
 	rule = putRule{typ, clauseTypes, step}
 	p.rules[inType.ID()] = rule
 	return rule
+}
+
+func sameTypes(types []zed.Type, vals []zed.Value) bool {
+	return slices.EqualFunc(types, vals, func(typ zed.Type, val zed.Value) bool {
+		return typ == val.Type
+	})
 }
 
 func (p *Putter) Eval(ectx Context, this *zed.Value) *zed.Value {

runtime/expr/shaper.go
@@ -173,10 +173,10 @@ func shaperType(zctx *zed.Context, tf ShaperTransform, in, out zed.Type) (zed.Ty
 				return nil, err
 			}
 			if tf&Cast != 0 {
-				if sameFields(fields, outRec.Fields) {
+				if slices.Equal(fields, outRec.Fields) {
 					return out, nil
 				}
-			} else if sameFields(fields, inRec.Fields) {
+			} else if slices.Equal(fields, inRec.Fields) {
 				return in, nil
 			}
 			return zctx.LookupTypeRecord(fields)
@@ -274,18 +274,6 @@ func bestUnionTag(in, out zed.Type) int {
 		return underlying
 	}
 	return compatible
-}
-
-func sameFields(a, b []zed.Field) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
 }
 
 func isArray(t zed.Type) bool {

runtime/op/router.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/brimdata/zed/zbuf"
+	"golang.org/x/exp/slices"
 )
 
 type Selector interface {
@@ -79,12 +80,9 @@ func (r *Router) run() {
 }
 
 func (r *Router) blocked() bool {
-	for _, p := range r.routes {
-		if !p.blocked {
-			return false
-		}
-	}
-	return true
+	return !slices.ContainsFunc(r.routes, func(route *route) bool {
+		return !route.blocked
+	})
 }
 
 // Send an EOS to each unblocked route.  On return evertyhing is unblocked

commit f40699e8ae5e3731395f17a15e19f999c7a3ae13

Fix zio.Combiner.finished
Fix a bug introduced in #4699.

zio/combiner.go
@@ -61,7 +61,7 @@ func (c *Combiner) run() {
 }
 
 func (c *Combiner) finished() bool {
-	return slices.Contains(c.done, false)
+	return !slices.Contains(c.done, false)
 }
 
 func (c *Combiner) Read() (*zed.Value, error) {

zio/combiner_test.go
@@ -1,0 +1,55 @@
+package zio
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/brimdata/zed"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCombiner(t *testing.T) {
+	r1 := &sliceReader{zed.NewInt64(0)}
+	r2 := &sliceReader{zed.NewInt64(1), zed.NewInt64(2)}
+	r3 := &sliceReader{zed.NewInt64(3), zed.NewInt64(4), zed.NewInt64(5)}
+	c := NewCombiner(context.Background(), []Reader{r1, r2, r3})
+	var vs []int64
+	for {
+		val, err := c.Read()
+		require.NoError(t, err)
+		if val == nil {
+			break
+		}
+		vs = append(vs, val.Int())
+	}
+	require.ElementsMatch(t, vs, []int64{0, 1, 2, 3, 4, 5})
+}
+
+func TestCombinerError(t *testing.T) {
+	r1 := &sliceReader{zed.Null}
+	r2 := &errorReader{errors.New("read error")}
+	c := NewCombiner(context.Background(), []Reader{r1, r2})
+	for {
+		val, err := c.Read()
+		if val == nil {
+			require.Error(t, err)
+			return
+		}
+	}
+}
+
+type errorReader struct{ error }
+
+func (e *errorReader) Read() (*zed.Value, error) { return nil, e }
+
+type sliceReader []*zed.Value
+
+func (t *sliceReader) Read() (*zed.Value, error) {
+	if len(*t) == 0 {
+		return nil, nil
+	}
+	val := (*t)[0]
+	*t = (*t)[1:]
+	return val, nil
+}