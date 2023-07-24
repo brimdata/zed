6950512b886f3d5ce3b80f8110a92dd2e2faa46c
Matthew Nibecker <hello@mattnibecker.com>
2023-07-24 13:24:28 -0700

    zed manage: Change default behavior (#4728)
    Change default behavior of zed manage so the default behavior runs
    compact once and exits. The -monitor flag can be used to continuously
    monitor and update pools.

--- cmd/zed/manage/command.go
+++ cmd/zed/manage/command.go
@@ -15,15 +15,15 @@ import (
 var Cmd = &charm.Spec{
 	Name:  "manage",
 	Usage: "manage",
-	Short: "continuously run compaction and other maintenance tasks on a lake",
+	Short: "run compaction and other maintenance tasks on a lake",
 	New:   New,
 }
 
 type Command struct {
 	*root.Command
 	logFlags logflags.Flags
-	once     bool
 	config   lakemanage.Config
+	monitor  bool
 }
 
 func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
@@ -38,8 +38,8 @@ func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
 		d.KnownFields(true) // returns error for unknown fields
 		return d.Decode(&c.config)
 	})
-	c.config.Interval = f.Duration("interval", lakemanage.DefaultInterval, "interval between updates")
-	f.BoolVar(&c.once, "once", false, "run once and exit")
+	c.config.Interval = f.Duration("interval", lakemanage.DefaultInterval, "interval between updates (only applicable with -monitor")
+	f.BoolVar(&c.monitor, "monitor", false, "continuously monitor the lake for updates")
 	return c, nil
 }
 
@@ -54,16 +54,16 @@ func (c *Command) Run(args []string) error {
 		return err
 	}
 	defer logger.Sync()
-	if c.once {
-		lk, err := c.LakeFlags.Open(ctx)
+	if c.monitor {
+		conn, err := c.LakeFlags.Connection()
 		if err != nil {
 			return err
 		}
-		return lakemanage.Update(ctx, lk, c.config, logger)
+		return lakemanage.Monitor(ctx, conn, c.config, logger)
 	}
-	conn, err := c.LakeFlags.Connection()
+	lk, err := c.LakeFlags.Open(ctx)
 	if err != nil {
 		return err
 	}
-	return lakemanage.Monitor(ctx, conn, c.config, logger)
+	return lakemanage.Update(ctx, lk, c.config, logger)
 }
