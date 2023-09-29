b947d5723d8d5cbb23fe33f4d19778da3d0dd410
Noah Treuhaft <noah.treuhaft@gmail.com>
2023-09-29 17:16:08 -0400

    Avoid recorder for io.ReadSeeker in anyio.NewReaderWithOpts
    NewReaderWithOpts always creates a Recorder, which buffers its input and
    can cause format detection to fail if the buffer is too small.  Avoid
    that problem when the passed reader implements io.ReadSeeker by
    modifying Track to create a Recorder only when the reader cannot seek.
    
    Closes #4586.

--- zio/anyio/track.go
+++ zio/anyio/track.go
@@ -1,24 +1,51 @@
 package anyio
 
+import "io"
+
 const TrackSize = InitBufferSize
 
 type Track struct {
+	rs      io.ReadSeeker
+	initial int64
+
 	recorder *Recorder
 	off      int
 }
 
-func NewTrack(r *Recorder) *Track {
+func NewTrack(r io.Reader) *Track {
+	if rs, ok := r.(io.ReadSeeker); ok {
+		if n, err := rs.Seek(0, io.SeekCurrent); err == nil {
+			return &Track{rs: rs, initial: n}
+		}
+	}
 	return &Track{
-		recorder: r,
+		recorder: NewRecorder(r),
 	}
 }
 
 func (t *Track) Reset() {
+	if t.rs != nil {
+		// We ignore errors here under the assumption that a subsequent
+		// call to Read will also fail.
+		t.rs.Seek(t.initial, io.SeekStart)
+		return
+	}
 	t.off = 0
 }
 
 func (t *Track) Read(b []byte) (int, error) {
+	if t.rs != nil {
+		return t.rs.Read(b)
+	}
 	n, err := t.recorder.ReadAt(t.off, b)
 	t.off += n
 	return n, err
+}
+
+func (t *Track) Reader() io.Reader {
+	if t.rs != nil {
+		t.Reset()
+		return t.rs
+	}
+	return t.recorder
 }
