831e2bca08f3f79f9582655e3746f934567e0441
Jamie Brandon <jamie@scattered-thoughts.net>
2023-11-02 13:17:18 -0700

    Close reader in trailer

--- vng/object.go
+++ vng/object.go
@@ -175,7 +175,7 @@ func (o *Object) newSectionReader(level int, sectionOff int64) *zngio.Reader {
 	off += sectionOff
 	len -= sectionOff
 	reader := io.NewSectionReader(o.readerAt, off, len)
-	return zngio.NewReaderWithOpts(o.zctx, reader, zngio.ReaderOpts{Threads: 1})
+	return zngio.NewReaderWithOpts(o.zctx, reader, zngio.ReaderOpts{})
 }
 
 func (o *Object) NewReassemblyReader() *zngio.Reader {
