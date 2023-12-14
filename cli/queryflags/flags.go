59b72d885646ca0fb952f2461e069099bbb91cd2
Matthew Nibecker <hello@mattnibecker.com>
2023-12-14 10:09:01 -0700

    zq: fix panic when trying to read from a pool (#4936)
    Closes #4935

--- cli/queryflags/flags.go
+++ cli/queryflags/flags.go
@@ -11,7 +11,9 @@ import (
 	"github.com/brimdata/zed/cli"
 	"github.com/brimdata/zed/compiler"
 	"github.com/brimdata/zed/compiler/ast"
+	"github.com/brimdata/zed/compiler/data"
 	"github.com/brimdata/zed/compiler/semantic"
+	"github.com/brimdata/zed/pkg/storage"
 	"github.com/brimdata/zed/zbuf"
 	"github.com/brimdata/zed/zson"
 )
@@ -37,7 +39,7 @@ func (f *Flags) ParseSourcesAndInputs(paths []string) ([]string, ast.Seq, bool, 
 			// and appears to start with a from or yield operator.
 			// Otherwise, consider it a file.
 			if query, err := compiler.Parse(src, f.Includes...); err == nil {
-				if s, err := semantic.Analyze(context.Background(), query, nil, nil); err == nil {
+				if s, err := semantic.Analyze(context.Background(), query, data.NewSource(storage.NewLocalEngine(), nil), nil); err == nil {
 					if semantic.HasSource(s) {
 						return nil, query, false, nil
 					}
