84c3af27e38e498380074687296fbc5a9e5bb540
Matthew Nibecker <hello@mattnibecker.com>
2023-12-18 13:24:52 -0700

    Update runtime/expr/agg/collect.go
    Co-authored-by: Noah Treuhaft <noah.treuhaft@gmail.com>

--- runtime/expr/agg/collect.go
+++ runtime/expr/agg/collect.go
@@ -22,11 +22,7 @@ func (c *Collect) Consume(val *zed.Value) {
 }
 
 func (c *Collect) update(val *zed.Value) {
-	// Untag the value if it's a union.
-	if union, ok := zed.TypeUnder(val.Type).(*zed.TypeUnion); ok {
-		val = zed.NewValue(union.Untag(val.Bytes()))
-	}
-	c.values = append(c.values, *val.Copy())
+	c.values = append(c.values, *val.Under(&zed.Value{}).Copy())
 	c.size += len(val.Bytes())
 	for c.size > MaxValueSize {
 		// XXX See issue #1813.  For now we silently discard entries
