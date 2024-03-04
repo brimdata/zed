b36c7bf4f112e4062034cd4885c93bcacdcf682d
Matthew Nibecker <hello@mattnibecker.com>
2024-03-04 14:50:34 -0700

    zq: Better error messages for single argument commands (#5053)

--- cli/queryflags/flags.go
+++ cli/queryflags/flags.go
@@ -2,16 +2,19 @@ package queryflags
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"net/url"
 	"os"
 	"slices"
+	"strings"
 
 	"github.com/brimdata/zed/cli"
 	"github.com/brimdata/zed/compiler"
 	"github.com/brimdata/zed/compiler/ast"
 	"github.com/brimdata/zed/compiler/data"
+	"github.com/brimdata/zed/compiler/parser"
 	"github.com/brimdata/zed/compiler/semantic"
 	"github.com/brimdata/zed/pkg/storage"
 	"github.com/brimdata/zed/zbuf"
@@ -38,7 +41,8 @@ func (f *Flags) ParseSourcesAndInputs(paths []string) ([]string, ast.Seq, bool, 
 			// Consider a lone argument to be a query if it compiles
 			// and appears to start with a from or yield operator.
 			// Otherwise, consider it a file.
-			if query, err := compiler.Parse(src, f.Includes...); err == nil {
+			query, err := compiler.Parse(src, f.Includes...)
+			if err == nil {
 				if s, err := semantic.Analyze(context.Background(), query, data.NewSource(storage.NewLocalEngine(), nil), nil); err == nil {
 					if semantic.HasSource(s) {
 						return nil, query, false, nil
@@ -48,7 +52,7 @@ func (f *Flags) ParseSourcesAndInputs(paths []string) ([]string, ast.Seq, bool, 
 					}
 				}
 			}
-			return nil, nil, false, fmt.Errorf("no such file: %s", src)
+			return nil, nil, false, singleArgError(src, err)
 		}
 	}
 	query, err := compiler.Parse(src, f.Includes...)
@@ -74,4 +78,27 @@ func (f *Flags) PrintStats(stats zbuf.Progress) {
 		}
 		fmt.Fprintln(os.Stderr, out)
 	}
+}
+
+func singleArgError(src string, err error) error {
+	var b strings.Builder
+	b.WriteString("could not invoke zq with a single argument because:")
+	if len(src) > 20 {
+		src = src[:20] + "..."
+	}
+	fmt.Fprintf(&b, "\n - a file could not be found with the name %q", src)
+	var perr *parser.Error
+	if errors.As(err, &perr) {
+		b.WriteString("\n - the argument could not be compiled as a valid Zed query due to parse error (")
+		if perr.LineNum > 0 {
+			fmt.Fprintf(&b, "line %d, ", perr.LineNum)
+		}
+		fmt.Fprintf(&b, "column %d):", perr.Column)
+		for _, l := range strings.Split(perr.ParseErrorContext(), "\n") {
+			fmt.Fprintf(&b, "\n   %s", l)
+		}
+	} else {
+		b.WriteString("\n - the argument did not parse as a valid Zed query")
+	}
+	return errors.New(b.String())
 }
