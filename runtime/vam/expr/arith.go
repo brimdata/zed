e11f38242fa003516181c43d72e1f0331f1ac8cb
Noah Treuhaft <noah.treuhaft@gmail.com>
2024-07-12 12:37:50 -0400

    wip

--- runtime/vam/expr/arith.go
+++ runtime/vam/expr/arith.go
@@ -81,8 +81,8 @@ func applyToUnion(zctx *zed.Context, lhs, rhs vector.Any, eval func(lhs, rhs vec
 			// lhsUnion.Values. Second has a corresponding view of
 			// rhs.
 			reverse := lhsUnion.TagMap.Reverse
-			// lhsViewIndexes[k] will hold the view indexes for lhsUnion.Values[k].
-			lhsViewIndexes := make([][]uint32, len(lhsUnion.Values))
+			// viewIndexes[k] will hold the view indexes for XXX.
+			viewIndexes := make([][]uint32, len(lhsUnion.Values))
 			// resultTags[k] is the union tag for the k-th element of the result vector.
 			resultTags := make([]uint32, lhsView.Len())
 			for k, index := range lhsView.Index {
@@ -92,12 +92,12 @@ func applyToUnion(zctx *zed.Context, lhs, rhs vector.Any, eval func(lhs, rhs vec
 				if !ok {
 					panic("index not in reverse")
 				}
-				lhsViewIndexes[tag] = append(lhsViewIndexes[tag], uint32(unionValuesIndex))
+				viewIndexes[tag] = append(viewIndexes[tag], uint32(unionValuesIndex))
 			}
 			// lhsViews[k] will hold the view for lhsUnion.Values[k].
 			lhsViews := make([]vector.Any, len(lhsUnion.Values))
 			for k := range lhsViews {
-				lhsViews[k] = vector.NewView2(lhsViewIndexes[k], lhsUnion.Values[k])
+				lhsViews[k] = vector.NewView2(viewIndexes[k], lhsUnion.Values[k])
 			}
 			// No need to allocate another slice for results.
 			results := lhsViews
@@ -113,7 +113,7 @@ func applyToUnion(zctx *zed.Context, lhs, rhs vector.Any, eval func(lhs, rhs vec
 	if rhsView, ok := rhs.(*vector.View); ok {
 		if rhsUnion, ok := rhsView.Any.(*vector.Union); ok {
 			reverse := rhsUnion.TagMap.Reverse
-			rhsViewIndexes := make([][]uint32, len(rhsUnion.Values))
+			viewIndexes := make([][]uint32, len(rhsUnion.Values))
 			resultTags := make([]uint32, rhsView.Len())
 			for k, index := range rhsView.Index {
 				tag := rhsUnion.Tags[index]
@@ -122,16 +122,16 @@ func applyToUnion(zctx *zed.Context, lhs, rhs vector.Any, eval func(lhs, rhs vec
 				if !ok {
 					panic("index not in reverse")
 				}
-				rhsViewIndexes[tag] = append(rhsViewIndexes[tag], uint32(unionValuesIndex))
+				viewIndexes[tag] = append(viewIndexes[tag], uint32(unionValuesIndex))
 			}
 			rhsViews := make([]vector.Any, len(rhsUnion.Values))
 			for k := range rhsViews {
-				rhsViews[k] = vector.NewView2(rhsViewIndexes[k], rhsUnion.Values[k])
+				rhsViews[k] = vector.NewView2(viewIndexes[k], rhsUnion.Values[k])
 			}
 			// No need to allocate another slice for results.
 			results := rhsViews
 			for tag, rhsView := range rhsViews {
-				lhsView := vector.NewView2(reverse[tag], lhs)
+				lhsView := vector.NewView2(viewIndexes[tag], lhs)
 				results[tag] = eval(lhsView, rhsView)
 			}
 			// XXX Need to merge elements of results that have the same type.
