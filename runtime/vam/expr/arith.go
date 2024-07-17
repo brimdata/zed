9f6c35fed37c3d7ebe244140196b4b0f54998226
Noah Treuhaft <noah.treuhaft@gmail.com>
2024-07-17 13:16:13 -0400

    wip

--- runtime/vam/expr/arith.go
+++ runtime/vam/expr/arith.go
@@ -28,7 +28,7 @@ func (a *Arith) Eval(val vector.Any) vector.Any {
 func (a *Arith) eval(lhs, rhs vector.Any) vector.Any {
 	lhs = vector.Under(lhs)
 	rhs = vector.Under(rhs)
-	if val, ok := applyToUnionOrVariant(lhs, rhs, a.eval); ok {
+	if val, ok := evalForUnionOrVariant(lhs, rhs, a.eval); ok {
 		return val
 	}
 	lhs, rhs, errVal := coerceVals(a.zctx, lhs, rhs)
@@ -54,22 +54,22 @@ func (a *Arith) eval(lhs, rhs vector.Any) vector.Any {
 	return f(lhs, rhs)
 }
 
-func applyToUnionOrVariant(lhs, rhs vector.Any, eval func(lhs, rhs vector.Any) vector.Any) (vector.Any, bool) {
+func evalForUnionOrVariant(lhs, rhs vector.Any, eval func(lhs, rhs vector.Any) vector.Any) (vector.Any, bool) {
 	if lhs.Len() != rhs.Len() {
 		panic(fmt.Sprintf("mismatched vector lengths: %d vs %d", lhs.Len(), rhs.Len()))
 	}
 
 	switch lhs := lhs.(type) {
 	case *vector.Variant:
-		return applyWithTagMap(lhs.Tags, lhs.TagMap.Reverse, lhs.Values, rhs, eval), true
+		return evalForUnionOrVariantLHS(lhs.Tags, lhs.TagMap.Reverse, lhs.Values, rhs, eval), true
 	case *vector.Union:
-		return applyWithTagMap(lhs.Tags, lhs.TagMap.Reverse, lhs.Values, rhs, eval), true
+		return evalForUnionOrVariantLHS(lhs.Tags, lhs.TagMap.Reverse, lhs.Values, rhs, eval), true
 	case *vector.View:
 		if lhsVariant, ok := lhs.Any.(*vector.Variant); ok {
-			return applyToViewOfUnionOrVariant(lhs.Index, lhsVariant.Tags, lhsVariant.TagMap.Forward, lhsVariant.Values, rhs, eval), true
+			return evalForUnionOrVariantViewLHS(lhs.Index, lhsVariant.Tags, lhsVariant.TagMap.Forward, lhsVariant.Values, rhs, eval), true
 		}
 		if lhsUnion, ok := lhs.Any.(*vector.Union); ok {
-			return applyToViewOfUnionOrVariant(lhs.Index, lhsUnion.Tags, lhsUnion.TagMap.Forward, lhsUnion.Values, rhs, eval), true
+			return evalForUnionOrVariantViewLHS(lhs.Index, lhsUnion.Tags, lhsUnion.TagMap.Forward, lhsUnion.Values, rhs, eval), true
 		}
 	}
 
@@ -78,22 +78,22 @@ func applyToUnionOrVariant(lhs, rhs vector.Any, eval func(lhs, rhs vector.Any) v
 	}
 	switch rhs := rhs.(type) {
 	case *vector.Variant:
-		return applyWithTagMap(rhs.Tags, rhs.TagMap.Reverse, rhs.Values, lhs, swapAndEval), true
+		return evalForUnionOrVariantLHS(rhs.Tags, rhs.TagMap.Reverse, rhs.Values, lhs, swapAndEval), true
 	case *vector.Union:
-		return applyWithTagMap(rhs.Tags, rhs.TagMap.Reverse, rhs.Values, lhs, swapAndEval), true
+		return evalForUnionOrVariantLHS(rhs.Tags, rhs.TagMap.Reverse, rhs.Values, lhs, swapAndEval), true
 	case *vector.View:
 		if rhsVariant, ok := rhs.Any.(*vector.Variant); ok {
-			return applyToViewOfUnionOrVariant(rhs.Index, rhsVariant.Tags, rhsVariant.TagMap.Forward, rhsVariant.Values, lhs, swapAndEval), true
+			return evalForUnionOrVariantViewLHS(rhs.Index, rhsVariant.Tags, rhsVariant.TagMap.Forward, rhsVariant.Values, lhs, swapAndEval), true
 		}
 		if rhsUnion, ok := rhs.Any.(*vector.Union); ok {
-			return applyToViewOfUnionOrVariant(rhs.Index, rhsUnion.Tags, rhsUnion.TagMap.Forward, rhsUnion.Values, lhs, swapAndEval), true
+			return evalForUnionOrVariantViewLHS(rhs.Index, rhsUnion.Tags, rhsUnion.TagMap.Forward, rhsUnion.Values, lhs, swapAndEval), true
 		}
 	}
 
 	return nil, false
 }
 
-func applyWithTagMap(lhsTags []uint32, lhsReverse [][]uint32, lhsValues []vector.Any, rhs vector.Any, eval func(lhs, rhs vector.Any) vector.Any) vector.Any {
+func evalForUnionOrVariantLHS(lhsTags []uint32, lhsReverse [][]uint32, lhsValues []vector.Any, rhs vector.Any, eval func(lhs, rhs vector.Any) vector.Any) vector.Any {
 	results := make([]vector.Any, len(lhsValues))
 	for tag, view := range vector.Unstitch(lhsReverse, rhs) {
 		results[tag] = eval(lhsValues[tag], view)
@@ -101,11 +101,12 @@ func applyWithTagMap(lhsTags []uint32, lhsReverse [][]uint32, lhsValues []vector
 	return vector.NewVariant(lhsTags, results)
 }
 
-func applyToViewOfUnionOrVariant(lhsViewIndex []uint32, lhsTags []uint32, lhsForward []uint32, lhsValues []vector.Any, rhs vector.Any, eval func(lhs, rhs vector.Any) vector.Any) vector.Any {
+func evalForUnionOrVariantViewLHS(lhsViewIndex []uint32, lhsTags []uint32, lhsForward []uint32, lhsValues []vector.Any, rhs vector.Any, eval func(lhs, rhs vector.Any) vector.Any) vector.Any {
 	// Have a view on lhs. Need to convert that to two sets of views. First
-	// is a (possibly empty) view per element of lhsValues. Second has a
-	// corresponding views of rhs. lhsIndexes[k] will hold the view indexes
-	// for lhsValues[k].
+	// is a (possibly empty) view per element of lhsValues and second is a
+	// corresponding views of rhs. lhsIndexes[k] holds the indexes for the
+	// view of lhsValues[k] while rhsIndexes[k] holds indexes for the
+	// corresponding view of rhs.
 	lhsIndexes := make([][]uint32, len(lhsValues))
 	rhsIndexes := make([][]uint32, len(lhsValues))
 	// resultTags[k] is the tag for results[k].
