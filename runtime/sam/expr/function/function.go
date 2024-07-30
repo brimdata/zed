2c35ba2e472aec99c108e95e725dcf5ea929cf3e
Matthew Nibecker <hello@mattnibecker.com>
2024-07-30 13:07:22 -0700

    Add strftime function

--- runtime/sam/expr/function/function.go
+++ runtime/sam/expr/function/function.go
@@ -161,6 +161,9 @@ func New(zctx *zed.Context, name string, narg int) (expr.Function, field.Path, e
 	case "regexp_replace":
 		argmin, argmax = 3, 3
 		f = &RegexpReplace{zctx: zctx}
+	case "strftime":
+		argmin, argmax = 2, 2
+		f = &Strftime{zctx: zctx}
 	case "under":
 		f = &Under{zctx: zctx}
 	case "unflatten":
