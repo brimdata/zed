451ccfc0492595332903925328667e313e944647
Matthew Nibecker <hello@mattnibecker.com>
2024-09-03 10:50:47 -0700

    vam: len() function (#5247)

--- vector/ip.go
+++ vector/ip.go
@@ -33,3 +33,26 @@ func (i *IP) Serialize(b *zcode.Builder, slot uint32) {
 		b.Append(zed.EncodeIP(i.Values[slot]))
 	}
 }
+
+func IPValue(val Any, slot uint32) (netip.Addr, bool) {
+	switch val := val.(type) {
+	case *IP:
+		return val.Values[slot], val.Nulls.Value(slot)
+	case *Const:
+		if val.Nulls.Value(slot) {
+			return netip.Addr{}, true
+		}
+		b, _ := val.AsBytes()
+		return zed.DecodeIP(b), false
+	case *Dict:
+		if val.Nulls.Value(slot) {
+			return netip.Addr{}, true
+		}
+		slot = uint32(val.Index[slot])
+		return val.Any.(*IP).Values[slot], false
+	case *View:
+		slot = val.Index[slot]
+		return IPValue(val.Any, slot)
+	}
+	panic(val)
+}
