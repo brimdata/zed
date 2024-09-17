4f75e628e26deda33c29699ec132b8d679ba012a
Noah Treuhaft <noah.treuhaft@gmail.com>
2024-09-17 18:29:15 -0400

    Implement vector pass operator (#5289)

--- compiler/kernel/vop.go
+++ compiler/kernel/vop.go
@@ -82,6 +82,8 @@ func (b *Builder) compileVamLeaf(o dag.Op, parent vector.Puller) (vector.Puller,
 		} else {
 			return nil, fmt.Errorf("internal error: unhandled dag.Summarize: %#v", o)
 		}
+	case *dag.Pass:
+		return parent, nil
 	case *dag.Put:
 		return b.compileVamPut(o, parent)
 	case *dag.Yield:
