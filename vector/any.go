86329205a7c29b75fe1379b6ad49fb4d71512cdb
Matthew Nibecker <hello@mattnibecker.com>
2024-09-25 16:35:55 -0400

    vam: Add network_of() func

--- vector/any.go
+++ vector/any.go
@@ -21,28 +21,3 @@ type Puller interface {
 }
 
 type Builder func(*zcode.Builder) bool
-
-func Combine(vec Any, index []uint32, add Any) Any {
-	var vecs []Any
-	tags := make([]uint32, int(vec.Len())+len(index))
-	if d, ok := vec.(*Dynamic); ok {
-		vecs = d.Values
-		varTags := d.Tags
-		n := uint32(len(vecs))
-		for i := uint32(0); i < uint32(len(tags)); i++ {
-			if len(index) > 0 && i == index[0] {
-				tags[i] = n
-				index = index[1:]
-			} else {
-				tags[i] = varTags[0]
-				varTags = varTags[1:]
-			}
-		}
-	} else {
-		vecs = []Any{vec}
-		for _, k := range index {
-			tags[k] = 1
-		}
-	}
-	return NewDynamic(tags, append(vecs, add))
-}
